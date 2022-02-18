5912769ff05cff195d2df77fe2abd61c32d41676
Alex Gergel <alexgergel@gmail.com>
2022-02-18 13:41:24 +0200

    "Solution"

--- app/game_mechanics/battle.py
+++ app/game_mechanics/battle.py
@@ -1,5 +1,3 @@
-# from app.game_mechanics.dice import modifying
-# from app.game_mechanics.dice import rolling
 from app.npc.knights import fighters
 
 lancelot = fighters["lancelot"]
@@ -9,30 +7,18 @@ red_knight = fighters["red_knight"]
 
 
 def battle(*args):
-    # 1 Lancelot vs Mordred:
-
-    # lancelot["power"] = modifying(rolling(), lancelot["power"])
-    # mordred["power"] = modifying(rolling(), mordred["power"])
-
     lancelot["hp"] -= mordred["power"] - lancelot["protection"]
     mordred["hp"] -= lancelot["power"] - mordred["protection"]
 
-    # check if someone fell in battle
     if lancelot["hp"] <= 0:
         lancelot["hp"] = 0
 
     if mordred["hp"] <= 0:
         mordred["hp"] = 0
 
-    # 2 Arthur vs Red Knight:
-
-    # arthur["power"] = modifying(rolling(), arthur["power"])
-    # red_knight["power"] = modifying(rolling(), red_knight["power"])
-
     arthur["hp"] -= red_knight["power"] - arthur["protection"]
     red_knight["hp"] -= arthur["power"] - red_knight["protection"]
 
-    # check if someone fell in battle
     if arthur["hp"] <= 0:
         arthur["hp"] = 0
 
