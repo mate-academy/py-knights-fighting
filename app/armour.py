dfe917b50200fa9de9bb31626706015bd4639302
Katiukha Oleksandr <1109katiuha@gmail.com>
2022-03-26 20:03:18 +0200

    Solution check. Refactor code . Katiukha

--- app/armour.py
+++
@@ -1,5 +0,0 @@
-class Armour():
-    """ Class describe armour"""
-    def __init__(self, part: str, protection: int):
-        self.part = part
-        self.protection = protection
--- app/potion.py
+++
@@ -1,5 +0,0 @@
-class Potion():
-
-    def __init__(self, name: str, effect: dict):
-        self.name = name
-        self.effect = effect
