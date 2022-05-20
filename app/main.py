21e43698dbecb3fbe8d74ec8c48644644afc1584
Flashmobber <ufinaid@gmail.com>
2022-05-20 17:37:51 +0300

    Remove 0 from range

--- app/main.py
+++ app/main.py
@@ -7,13 +7,13 @@ def battle(knights_config):
     knights = [Knight(knights_config[knight])
                for knight in knights_config.keys()]
 
-    for k in range(0, len(knights) - 2):
+    for k in range(len(knights) - 2):
         battle_result = Battle(knights[k], knights[k + 2])
         battle_result.attack()
 
     battle_results = {}
 
-    for k in range(0, len(knights) - 2):
+    for k in range(len(knights) - 2):
         battle_results.update({knights[k].name: knights[k].hp})
         battle_results.update({knights[k + 2].name: knights[k + 2].hp})
 
