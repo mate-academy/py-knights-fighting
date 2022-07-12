927a155e4b3e2aac7c438eee4d46a260f6ecf251
Slava Seredenko <vrungelsp@gmail.com>
2022-07-12 18:00:01 +0300

    Worлшking version

--- app/battle/battle_logic.py
+++ app/battle/battle_logic.py
@@ -1,6 +1,14 @@
-def battle(battle_list):
-    knight1 = battle_list[0]
-    knight2 = battle_list[1]
+from app.knights_info.characters_list import create_char
+
+
+def battle(knight_dict):
+    knights = create_char(knight_dict)
+
+    return sparring(knights["lancelot"], knights["mordred"]) | \
+        sparring(knights["arthur"], knights["red_knight"])
+
+
+def sparring(knight1, knight2):
     result1 = knight1.hp - (knight2.power - knight1.protection)
     result2 = knight2.hp - (knight1.power - knight2.protection)
     return {knight1.name: (result1 if result1 > 0 else 0),
--- app/main.py
+++ app/main.py
@@ -1,11 +1,4 @@
-from app.knights_info.characters_list import create_char, KNIGHTS
+from app.knights_info.characters_list import KNIGHTS
 from app.battle.battle_logic import battle
 
-# Create class instances
-knights = create_char(KNIGHTS)
-
-# Start battle
-first_battle = battle([knights["lancelot"], knights["mordred"]])
-second_battle = battle([knights["arthur"], knights["red_knight"]])
-
-print(first_battle | second_battle)
+print(battle(KNIGHTS))
