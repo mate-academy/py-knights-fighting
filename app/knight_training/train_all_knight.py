8516e65ae0688443cd3c4dbd6a5b83672d611ab4
Vlad Chernoborodiuk <45739207+Marsianin007@users.noreply.github.com>
2022-07-26 21:28:37 +0300

    Solution_0.2

---
+++ app/knight_training/train_all_knight.py
@@ -0,0 +1,27 @@
+def apply_armour(knight):
+    knight["protection"] = 0
+    for a in knight["armour"]:
+        knight["protection"] += a["protection"]
+
+
+def apply_weapon(knight):
+    knight["power"] += knight["weapon"]["power"]
+
+
+def apply_potion_if_exist(knight):
+    if knight["potion"] is not None:
+        if "power" in knight["potion"]["effect"]:
+            knight["power"] += knight["potion"]["effect"]["power"]
+
+        if "protection" in knight["potion"]["effect"]:
+            knight["protection"] += knight["potion"]["effect"]["protection"]
+
+        if "hp" in knight["potion"]["effect"]:
+            knight["hp"] += knight["potion"]["effect"]["hp"]
+
+
+def train(*args):
+    for knight in args:
+        apply_armour(knight)
+        apply_weapon(knight)
+        apply_potion_if_exist(knight)
