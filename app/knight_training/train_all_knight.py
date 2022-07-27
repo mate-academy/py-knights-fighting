4e5379011fef3d16a4dc3838d7119c4c13230e8a
Vlad Chernoborodiuk <45739207+Marsianin007@users.noreply.github.com>
2022-07-27 10:20:19 +0300

    Fixed duplications

--- app/knight_training/train_all_knight.py
+++ app/knight_training/train_all_knight.py
@@ -10,15 +10,9 @@ def apply_weapon(knight):
 
 def apply_potion_if_exist(knight):
     if knight["potion"] is not None:
-        if "power" in knight["potion"]["effect"]:
-            knight["power"] += knight["potion"]["effect"]["power"]
-
-        if "protection" in knight["potion"]["effect"]:
-            knight["protection"] += knight["potion"]["effect"]["protection"]
-
-        if "hp" in knight["potion"]["effect"]:
-            knight["hp"] += knight["potion"]["effect"]["hp"]
-
+        for magic in ("power", "protection", "hp"):
+            if magic in knight["potion"]["effect"]:
+                knight[magic] += knight["potion"]["effect"][magic]
 
 def train(*args):
     for knight in args:
