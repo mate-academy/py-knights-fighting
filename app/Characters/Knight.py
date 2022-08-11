26ed3af5ef4b1b2f25bbdc2ec4fa324f11372027
Dmitriy Kostiuk <kostiuk.dmitriy@gmail.com>
2022-08-11 12:24:01 +0300

    'Solution'

--- app/Characters/Knight.py
+++ app/Characters/Knight.py
@@ -9,27 +9,28 @@ class Knight:
         self.__class__.knights[name] = self
 
     @staticmethod
-    def knight_from_dict(val: dict):
-        knight = Knight(val["name"], val["power"], val["hp"])
-        knight.add_protection_from_armor(val)
-        knight.add_power_from_weapon(val)
-        if val["potion"] is not None:
-            if "power" in val["potion"]["effect"]:
-                knight.power += val["potion"]["effect"]["power"]
-
-            if "protection" in val["potion"]["effect"]:
-                knight.protection += val["potion"]["effect"]["protection"]
-
-            if "hp" in val["potion"]["effect"]:
-                knight.hp += val["potion"]["effect"]["hp"]
+    def knight_from_dict(knight_dict: dict):
+        knight = Knight(knight_dict["name"],
+                        knight_dict["power"],
+                        knight_dict["hp"])
+        knight.add_protection_from_armor(knight_dict)
+        knight.add_power_from_weapon(knight_dict)
+        if knight_dict["potion"] is not None:
+            stats = ("protection", "power", "hp")
+            effect = knight_dict["potion"]["effect"]
+            for stat in stats:
+                if stat in effect:
+                    knight.__setattr__(
+                        stat,
+                        knight.__getattribute__(stat) + effect[stat])
         return knight
 
-    def add_protection_from_armor(self, val):
-        for armour in val["armour"]:
+    def add_protection_from_armor(self, armor_list):
+        for armour in armor_list["armour"]:
             self.protection += armour["protection"]
 
-    def add_power_from_weapon(self, val):
-        self.power += val["weapon"]["power"]
+    def add_power_from_weapon(self, weapon_data):
+        self.power += weapon_data["weapon"]["power"]
 
     def change_knights_hp(self, other):
         self.hp -= min(self.hp, other.power - self.protection)
