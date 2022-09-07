bd7a37c542731d8fad3e1d5eb49e961cea6e4931
Vadim Kosnovskiy <kvmltkv@gmail.com>
2022-09-07 11:58:00 +0300

    py_knights_fighting_Solution

---
+++ app/knights.py
@@ -0,0 +1,33 @@
+class KnightClass:
+    def __init__(self, knight: dict):
+        self.knight = knight
+
+    def name(self):
+        return self.knight["name"]
+
+    # apply armour
+    def protection(self):
+        self.knight["protection"] = 0
+        for a in self.knight["armour"]:
+            self.knight["protection"] += a["protection"]
+        if self.knight["potion"] is not None:
+            if "protection" in self.knight["potion"]["effect"]:
+                self.knight["protection"] += \
+                    self.knight["potion"]["effect"]["protection"]
+        return self.knight["protection"]
+
+    # apply weapon
+    def power(self):
+        self.knight["power"] += self.knight["weapon"]["power"]
+        if self.knight["potion"] is not None:
+            if "power" in self.knight["potion"]["effect"]:
+                self.knight["power"] += \
+                    self.knight["potion"]["effect"]["power"]
+        return self.knight["power"]
+
+    # apply potion if exist
+    def hp(self):
+        if self.knight["potion"] is not None:
+            if "hp" in self.knight["potion"]["effect"]:
+                self.knight["hp"] += self.knight["potion"]["effect"]["hp"]
+        return self.knight["hp"]
