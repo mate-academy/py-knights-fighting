ed546fbbae6f67029a3fc2301e6cfe1135db88ef
Kyryll Katash <kyryllkatash@gmail.com>
2022-09-08 16:42:49 +0300

    Added 3 new files: - duel.py with function of duel - knight_stats.py calculates knight states - knights.py which contains KNIGHTS dict

---
+++ app/duel.py
@@ -0,0 +1,10 @@
+def duel(first, second):
+    first["hp"] -= second["power"] - first["protection"]
+    second["hp"] -= first["power"] - second["protection"]
+
+    if first["hp"] <= 0:
+        first["hp"] = 0
+
+    if second["hp"] <= 0:
+        second["hp"] = 0
+# this function make a duel between knights
