31eadfcec74b148c17aaa280e1f520bee04184ad
LitvinOleksandr <sanek.litvin@gmail.com>
2022-09-09 16:28:38 +0300

    change method print

--- app/print_result.py
+++ app/print_result.py
@@ -1,7 +1,2 @@
-def print_result(heroy_1, heroy_2, heroy_3, heroy_4):
-    return {
-        heroy_1.name: heroy_1.hp,
-        heroy_2.name: heroy_2.hp,
-        heroy_3.name: heroy_3.hp,
-        heroy_4.name: heroy_4.hp
-    }
+def print_result(*args):
+    return {heroy.name: heroy.hp for heroy in args}
