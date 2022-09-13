5824d9d5fc09dc2369a741289016b5c624d66262
Soroka Igor <copokaigor@gmail.com>
2022-09-13 12:32:57 +0300

    Solution1

--- app/main.py
+++ app/main.py
@@ -1,91 +1,3 @@
-KNIGHTS = {
-    "lancelot": {
-        "name": "Lancelot",
-        "power": 35,
-        "hp": 100,
-        "armour": [],
-        "weapon": {
-            "name": "Metal Sword",
-            "power": 50,
-        },
-        "potion": None,
-    },
-    "arthur": {
-        "name": "Artur",
-        "power": 45,
-        "hp": 75,
-        "armour": [
-            {
-                "part": "helmet",
-                "protection": 15,
-            },
-            {
-                "part": "breastplate",
-                "protection": 20,
-            },
-            {
-                "part": "boots",
-                "protection": 10,
-            }
-        ],
-        "weapon": {
-            "name": "Two-handed Sword",
-            "power": 55,
-        },
-        "potion": None,
-    },
-    "mordred": {
-        "name": "Mordred",
-        "power": 30,
-        "hp": 90,
-        "armour": [
-            {
-                "part": "breastplate",
-                "protection": 15,
-            },
-            {
-                "part": "boots",
-                "protection": 10,
-            }
-        ],
-        "weapon": {
-            "name": "Poisoned Sword",
-            "power": 60,
-        },
-        "potion": {
-            "name": "Berserk",
-            "effect": {
-                "power": +15,
-                "hp": -5,
-                "protection": +10,
-            }
-        }
-    },
-    "red_knight": {
-        "name": "Red Knight",
-        "power": 40,
-        "hp": 70,
-        "armour": [
-            {
-                "part": "breastplate",
-                "protection": 25,
-            }
-        ],
-        "weapon": {
-            "name": "Sword",
-            "power": 45
-        },
-        "potion": {
-            "name": "Blessing",
-            "effect": {
-                "hp": +10,
-                "power": +5,
-            }
-        }
-    }
-}
-
-
 class Knights:
     def __init__(self, configurations):
         # for i in configurations.values():
@@ -106,12 +18,13 @@ class Knights:
 
     def apply_potions(self, potion):
         pot_effect = potion["effect"]
-        if "hp" in pot_effect:
-            self.hp += pot_effect["hp"]
-        if "protection" in pot_effect:
-            self.hp += pot_effect["protection"]
-        if "power" in pot_effect:
-            self.power += pot_effect["power"]
+        for item in pot_effect:
+            if item == "hp":
+                self.hp += pot_effect["hp"]
+            elif item == "protection":
+                self.hp += pot_effect["protection"]
+            else:
+                self.power += pot_effect["power"]
 
     def damage(self, amount_of_damage):
         if amount_of_damage > self.hp:
