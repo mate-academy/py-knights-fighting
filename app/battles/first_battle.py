03cfa326ddc52c7df5fb0b3df3bbdea599174213
Maks Danylchuk <maxitosm00@gmail.com>
2022-09-16 01:49:51 +0300

    I think completed

---
+++ app/battles/first_battle.py
@@ -0,0 +1,13 @@
+def lancelot_with_mordred(lancelot, mordred):
+    # 1 Lancelot vs Mordred:
+    lancelot["hp"] -= mordred["power"] - lancelot["protection"]
+    mordred["hp"] -= lancelot["power"] - mordred["protection"]
+
+    # check if someone fell in battle
+    if lancelot["hp"] <= 0:
+        lancelot["hp"] = 0
+
+    if mordred["hp"] <= 0:
+        mordred["hp"] = 0
+
+    return lancelot, mordred
---
+++ app/battles/second_battle.py
@@ -0,0 +1,13 @@
+def arthur_with_red_knight(arthur, red_knight):
+    # 1 Lancelot vs Mordred:
+    arthur["hp"] -= red_knight["power"] - arthur["protection"]
+    red_knight["hp"] -= arthur["power"] - red_knight["protection"]
+
+    # check if someone fell in battle
+    if arthur["hp"] <= 0:
+        arthur["hp"] = 0
+
+    if red_knight["hp"] <= 0:
+        red_knight["hp"] = 0
+
+    return arthur, red_knight
