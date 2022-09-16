c9f75fdc116edc8611e9cc52ccd553c0cce3e45f
Maks Danylchuk <maxitosm00@gmail.com>
2022-09-16 16:34:49 +0300

    Ready

--- app/knights/main_stats.py
+++ app/knights/main_stats.py
@@ -7,13 +7,10 @@ def knight_stat(all_knights, knight_name):
         # apply weapon
     knight["power"] += knight["weapon"]["power"]
     # apply potion if exist
+
     if knight["potion"] is not None:
-        if "power" in knight["potion"]["effect"]:
-            knight["power"] += knight["potion"]["effect"]["power"]
-
-        if "protection" in knight["potion"]["effect"]:
-            knight["protection"] += knight["potion"]["effect"]["protection"]
-
-        if "hp" in knight["potion"]["effect"]:
-            knight["hp"] += knight["potion"]["effect"]["hp"]
+        stats = ("hp", "protection", "power")
+        for stat in stats:
+            if stat in knight["potion"]["effect"]:
+                knight[stat] += knight["potion"]["effect"][stat]
     return knight
