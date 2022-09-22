d5448e533e1c9743ad2d0d2c5b4aafca27de82ed
Alex Parkhomenko <105493988+wQuelS@users.noreply.github.com>
2022-09-22 15:22:27 +0000

    Update potion.py
    removed annotation to hero due to flake8 error

--- app/equip/potion.py
+++ app/equip/potion.py
@@ -2,7 +2,7 @@ class Potion:
     def __init__(self, potion: dict):
         self.potion = potion
 
-    def get_potion(self, hero: "Hero") -> None:
+    def get_potion(self, hero) -> None:
         if self.potion is not None:
             for stats in ["hp", "protection", "power"]:
                 if stats in self.potion["effect"]:
