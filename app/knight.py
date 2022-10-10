87d5ede0639eef0a9bf79b92b429ad4d8d4b19a2
Nataliia <amirammina@gmail.com>
2022-10-10 13:29:33 +0300

    Solution

---
+++ app/knight.py
@@ -0,0 +1,39 @@
+class Knight:
+
+    def __init__(self, name: str, info: dict) -> None:
+        self.name = name
+        self.info = info
+
+    def get_protection(self) -> int:
+        count = 0
+        for i in self.info["armour"]:
+            count += i["protection"]
+        return count
+
+    def get_power(self) -> int:
+        count = self.info["power"]
+        count += self.info["weapon"]["power"]
+        return count
+
+    def characteristics(self) -> dict:
+        power = self.get_power()
+        protection = self.get_protection()
+        hp = self.info["hp"]
+
+        if self.info["potion"]:
+            if "power" in self.info["potion"]["effect"]:
+                power += self.info["potion"]["effect"]["power"]
+            if "protection" in self.info["potion"]["effect"]:
+                protection += self.info["potion"]["effect"]["protection"]
+            if "hp" in self.info["potion"]["effect"]:
+                hp += self.info["potion"]["effect"]["hp"]
+
+        return {"name": self.name,
+                "power": power,
+                "protection": protection,
+                "hp": hp}
+
+
+def get_characteristics(info: dict) -> list:
+    knights = [Knight(value["name"], value) for value in info.values()]
+    return [knights[i].characteristics() for i in range(len(knights))]
