d348cf0fd00b74ff29311242d32126c408ca3d57
Oleksandr Tkach <110629074+wanmitsuha@users.noreply.github.com>
2022-10-10 21:01:11 +0300

    'Solution'

--- app/knights/class_of_knigts.py
+++ app/knights/class_of_knigts.py
@@ -8,7 +8,7 @@ class Knights:
         self.weapon = lord["weapon"]
         self.potion = lord["potion"]
 
-    def preparations(self):
+    def preparations(self) -> type(None):
         # apply armour
         for armo in self.armour:
             self.protection += armo["protection"]
@@ -26,51 +26,3 @@ class Knights:
 
             if "hp" in self.potion["effect"]:
                 self.hp += self.potion["effect"]["hp"]
-
-    def main_battle(self, another):
-        self.hp -= another.power - self.protection
-        another.hp -= self.power - another.protection
-
-        if self.hp <= 0:
-            self.hp = 0
-
-        if another.hp <= 0:
-            another.hp = 0
-
-    def battle_to_death(self, another):
-        deaths = 0
-        while deaths == 0:
-            self.hp -= another.power - self.protection
-            another.hp -= self.power - another.protection
-
-            if self.hp <= 0:
-                self.hp = 0
-                deaths += 1
-
-            if another.hp <= 0:
-                another.hp = 0
-                deaths += 1
-        if deaths == 2:
-            print(f"{self.name} and {another.name} are defeated!")
-            return "Draw"
-        if another.hp == 0:
-            print(f"{another.name} is defeated in a fair fight!")
-            print(f"{self.name} wins")
-            return self
-        if self.hp == 0:
-            print(f"{self.name} is defeated in a fair fight!")
-            print(f"{another.name} wins")
-            return another
-
-    @staticmethod
-    def championship(members: list):
-        finalists = [
-            members[i].battle_to_death(members[i + 1])
-            for i in range(0, len(members), 2)
-        ]
-        winner = finalists[0].battle_to_death(finalists[1])
-        if winner == "Draw":
-            print("All knights is dead")
-            print("It was beautiful and spectacular championship")
-        else:
-            print(f"winner of championship is {winner.name}!")
--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 from app.knights.class_of_knigts import Knights
+from app.knights import fight_rules
 
 
-def battle(knights_cof: dict):
+def battle(knights_cof: dict) -> dict:
     lancelot = Knights(knights_cof["lancelot"])
     arthur = Knights(knights_cof["arthur"])
     mordred = Knights(knights_cof["mordred"])
@@ -11,8 +12,8 @@ def battle(knights_cof: dict):
     for knight in knights_list:
         knight.preparations()
     # BATTLE:
-    lancelot.main_battle(mordred)
-    arthur.main_battle(red_knight)
+    fight_rules.main_battle(lancelot, mordred)
+    fight_rules.main_battle(arthur, red_knight)
     # Return battle results:
     return {
         lord.name: lord.hp
