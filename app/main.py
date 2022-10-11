029b9794d819a661ee3373227626b3bb65c2abd3
Vadym Shkarbul <vadim.shkarbul@gmail.com>
2022-10-11 18:24:38 +0300

    max instead function

--- app/main.py
+++ app/main.py
@@ -1,13 +1,6 @@
 from app.knight import Knight
 
 
-def health_check(knights_hp: int) -> int:
-    if knights_hp <= 0:
-        return 0
-
-    return knights_hp
-
-
 def battle(knights_config: dict) -> dict:
 
     lancelot = Knight(knights_config["lancelot"])
@@ -23,14 +16,14 @@ def battle(knights_config: dict) -> dict:
     lancelot.hp -= mordred.power - lancelot.protection
     mordred.hp -= lancelot.power - mordred.protection
 
-    lancelot.hp = health_check(lancelot.hp)
-    mordred.hp = health_check(mordred.hp)
+    lancelot.hp = max(lancelot.hp, 0)
+    mordred.hp = max(mordred.hp, 0)
 
     arthur.hp -= red_knight.power - arthur.protection
     red_knight.hp -= arthur.power - red_knight.protection
 
-    arthur.hp = health_check(arthur.hp)
-    red_knight.hp = health_check(red_knight.hp)
+    arthur.hp = max(arthur.hp, 0)
+    red_knight.hp = max(red_knight.hp, 0)
 
     return {
         lancelot.name: lancelot.hp,
