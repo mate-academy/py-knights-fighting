8d17f084b1c1e5cbd9be2649f3f38765b56ff6d8
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-10-20 17:13:46 +0300

    version 2.0

---
+++ app/knights.py
@@ -0,0 +1,38 @@
+class Knights:
+
+    def __init__(self,
+                 name: str = "",
+                 hp: int= 0,
+                 power: int=0,
+                 protection: int = 0):
+        self.name = name
+        self.hp = hp
+        self.power = power
+        self.protection = protection
+
+    @staticmethod
+    def knight_config(knights_dict: dict):
+        dict_of_knights = {}
+        for name, atribute in knights_dict.items():
+            knight = Knights(name=atribute["name"],
+                                      hp=atribute["hp"],
+                                      power=atribute["power"])
+            for armour in atribute["armour"]:
+                knight.protection += armour["protection"]
+            knight.power += atribute["weapon"]["power"]
+
+            if atribute["potion"] is not None:
+                stats = {"protection": knight.protection,
+                         "power": knight.power,
+                         "hp": knight.hp}
+
+                for option in stats:
+                    if option in atribute["potion"]["effect"]:
+                        stats[option] += atribute["potion"]["effect"][option]
+
+                knight.protection = stats["protection"]
+                knight.power = stats["power"]
+                knight.hp = stats["hp"]
+
+            dict_of_knights[atribute["name"]] = knight
+        return dict_of_knights
---
+++ app/main.py
@@ -0,0 +1,21 @@
+from app.knights import Knights
+
+
+def battle(dict_of_knights) -> dict:
+    list_of_knights = Knights.knight_config(dict_of_knights)
+
+    lancelot = list_of_knights["Lancelot"]
+    arthur = list_of_knights["Artur"]
+    mordred = list_of_knights["Mordred"]
+    red_knight = list_of_knights["Red Knight"]
+
+    lancelot.hp -= mordred.power - lancelot.protection
+    mordred.hp -= lancelot.power - mordred.protection
+    arthur.hp -= red_knight.power - arthur.protection
+    red_knight.hp -= arthur.power - red_knight.protectio
+
+    for warrior, stats in list_of_knights.items():
+        if stats.hp <= 0:
+            stats.hp = 0
+        list_of_knights[warrior] = stats.hp
+    return list_of_knights
