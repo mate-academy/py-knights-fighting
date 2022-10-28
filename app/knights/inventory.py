e6b0a1e83a5be9be6c23556ea3f890cdfffd484b
Dede <specialized8393@gmail.com>
2022-10-28 16:34:00 +0300

    Done

--- app/knights/inventory.py
+++ app/knights/inventory.py
@@ -13,14 +13,12 @@ class Inventory:
             self.weapon = Weapon(weapon["name"], weapon["power"])
         else:
             self.weapon = None
-
         if armours:
             self.armour = [
                 Armour(arm["part"],
                        arm["protection"]) for arm in armours]
         else:
             self.armour = []
-
         if potion:
             self.potion = Potion(potion["name"], potion["effect"])
         else:
