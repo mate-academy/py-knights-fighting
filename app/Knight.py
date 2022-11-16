b4df82329542df3ca7e0cff6cbb7df977f9070c9
Paul Maslov <maslovp67@gmail.com>
2022-11-16 16:47:05 +0200

    Solution corrected

--- app/Knight.py
+++ app/Knight.py
@@ -14,14 +14,14 @@ class Knight:
         knight["power"] += knight["weapon"]["power"]
 
         if knight["potion"] is not None:
-            if "power" in knight["potion"]["effect"]:
-                knight["power"] += knight["potion"]["effect"]["power"]
-
-            if "protection" in knight["potion"]["effect"]:
+            stats = knight["potion"]["effect"]
+            if "power" in stats:
+                knight["power"] += stats["power"]
+            if "protection" in stats:
                 knight["protection"]\
-                    += knight["potion"]["effect"]["protection"]
-            if "hp" in knight["potion"]["effect"]:
-                knight["hp"] += knight["potion"]["effect"]["hp"]
+                    += stats["protection"]
+            if "hp" in stats:
+                knight["hp"] += stats["hp"]
 
         self.name = knight["name"]
         self.hp = knight["hp"]
