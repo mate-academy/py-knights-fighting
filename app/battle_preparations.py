2628bcd9b751d7dd61cf61c7962c605971e16810
Maksym Maksimov <maksymmaksimov88@gmail.com>
2022-11-23 23:09:44 +0800

    Solution 2.0

--- app/battle_preparations.py
+++ app/battle_preparations.py
@@ -8,38 +8,35 @@ class Knights:
         self.hp = self.info["hp"]
         self.power = self.info["power"] + self.info["weapon"]["power"]
         self.protection = 0
-        self.hp_total()
-        self.power_total()
+        self.potion_effect()
         self.protection_total()
 
-    def hp_total(self) -> int:
+    def potion_effect(self) -> None:
         if self.info["potion"] is not None:
-            if "hp" in self.info["potion"]["effect"]:
-                self.hp += self.info["potion"]["effect"]["hp"]
-        return self.hp
+            for effect in self.info["potion"]["effect"]:
+                if effect == "hp":
+                    self.hp += self.info["potion"]["effect"]["hp"]
 
-    def power_total(self) -> int:
-        if self.info["potion"] is not None:
-            if "power" in self.info["potion"]["effect"]:
-                self.power += self.info["potion"]["effect"]["power"]
-        return self.power
+                if effect == "power":
+                    self.power += self.info["potion"]["effect"]["power"]
+
+                if effect == "protection":
+                    self.protection += \
+                        self.info["potion"]["effect"]["protection"]
 
     def protection_total(self) -> int:
-        if self.info["potion"] is not None:
-            if "protection" in self.info["potion"]["effect"]:
-                self.protection += self.info["potion"]["effect"]["protection"]
-
         if len(self.info["armour"]) > 0:
             for armour in self.info["armour"]:
                 self.protection += armour["protection"]
         return self.protection
 
+    def hp_check(self) -> None:
+        if self.hp <= 0:
+            self.hp = 0
+
     def holding_a_battle(self, other: Knights) -> None:
         self.hp -= other.power - self.protection
         other.hp -= self.power - other.protection
 
-        if self.hp <= 0:
-            self.hp = 0
-
-        if other.hp <= 0:
-            other.hp = 0
+        self.hp_check()
+        other.hp_check()
