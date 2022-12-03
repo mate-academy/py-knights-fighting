c9874f39081985663bc8bbda6bc15805683f21f8
Roman Harmatiuk <roman.harmatiuk@gmail.com>
2022-12-03 12:18:24 +0200

    List delited

--- app/equipment/armour.py
+++ app/equipment/armour.py
@@ -3,9 +3,9 @@ from app.keys import KeysArmour
 
 class Armour:
     def __init__(self, list_armour: list) -> None:
-        protect = []
+        sum_protect = 0
         for dictionary in list_armour:
             self.part = dictionary[KeysArmour.PART.value]
             self.protection = dictionary[KeysArmour.PROTECTION.value]
-            protect.append(self.protection)
-        self.sum_protect = sum(protect)
+            sum_protect += self.protection
+        self.sum_protect = sum_protect
