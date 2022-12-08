4b6ceb62a2e556de5f23f86a8611f0bfd33960a0
Yevhen Martyniuk <evgenijmartynuk07@gmail.com>
2022-12-08 20:01:43 +0300

    'one_method'

--- app/knights/apply_potion.py
+++ app/knights/apply_potion.py
@@ -5,23 +5,17 @@ class Potion:
     def apply_potion(self) -> dict:
         for hero in self.knight:
             if self.knight[hero]["potion"] is not None:
-                Potion(self.knight[hero]).power()
-                Potion(self.knight[hero]).protection()
-                Potion(self.knight[hero]).hp()
+                Potion(self.knight[hero]).add_parameters()
         return self.knight
 
-    def power(self) -> dict:
+    def add_parameters(self) -> dict:
         if "power" in self.knight["potion"]["effect"]:
             self.knight["power"] += self.knight["potion"]["effect"]["power"]
-            return self.knight
 
-    def protection(self) -> dict:
         if "protection" in self.knight["potion"]["effect"]:
             self.knight["protection"] += \
                 self.knight["potion"]["effect"]["protection"]
-            return self.knight
 
-    def hp(self) -> dict:
         if "hp" in self.knight["potion"]["effect"]:
             self.knight["hp"] += self.knight["potion"]["effect"]["hp"]
-            return self.knight
+        return self.knight
