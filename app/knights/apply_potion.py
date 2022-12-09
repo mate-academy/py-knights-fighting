3c26f9ea2769aa08ace3d5d3d6d26e75e7113e48
Yevhen Martyniuk <evgenijmartynuk07@gmail.com>
2022-12-09 21:29:03 +0300

    'fixed_loop'

--- app/knights/apply_potion.py
+++ app/knights/apply_potion.py
@@ -9,13 +9,7 @@ class Potion:
         return self.knight
 
     def add_parameters(self) -> dict:
-        if "power" in self.knight["potion"]["effect"]:
-            self.knight["power"] += self.knight["potion"]["effect"]["power"]
-
-        if "protection" in self.knight["potion"]["effect"]:
-            self.knight["protection"] += \
-                self.knight["potion"]["effect"]["protection"]
-
-        if "hp" in self.knight["potion"]["effect"]:
-            self.knight["hp"] += self.knight["potion"]["effect"]["hp"]
+        for add in self.knight["potion"]["effect"]:
+            if add in self.knight["potion"]["effect"]:
+                self.knight[add] += self.knight["potion"]["effect"][add]
         return self.knight
