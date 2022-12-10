f3db098372a2b9cf9565b82446b5bc6ddc0e3902
Alex Isaev <lesisv@gmail.com>
2022-12-10 10:15:06 +0200

    Solution_v5

--- app/class_knight/knight.py
+++ app/class_knight/knight.py
@@ -3,8 +3,11 @@ from __future__ import annotations
 
 class Knight:
 
-    def __init__(self, name: str, power: int, hp: int, protection: int) -> \
-            None:
+    def __init__(
+            self, name: str,
+            power: int, hp: int,
+            protection: int
+    ) -> None:
         self.name = name
         self.power = power
         self.hp = hp
