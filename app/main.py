d7f48225f383c66de7347c73eea8120f51f51fc4
Yehor Kornev <dmekkindom00@gmail.com>
2022-12-14 17:39:42 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -7,32 +7,11 @@ def battle(knights: dict) -> dict:
     preparations(knights)
 
     # 1 Lancelot vs Mordred:
-
-    fighting("lancelot", "mordred", KNIGHTS)
-
-    # knights["lancelot"]["hp"] -= knights["mordred"]["power"] \
-    #     - knights["lancelot"]["protection"]
-    # knights["mordred"]["hp"] -= knights["lancelot"]["power"] \
-    #     - knights["mordred"]["protection"]
-    #
-    # # check if someone fell in battle
-    # knights["lancelot"]["hp"] = check_hp_knight(knights["lancelot"]["hp"])
-    # knights["mordred"]["hp"] = check_hp_knight(knights["mordred"]["hp"])
+    fighting("lancelot", "mordred", knights)
 
     # 2 Arthur vs Red Knight:
+    fighting("arthur", "red_knight", knights)
 
-    fighting("arthur", "red_knight", KNIGHTS)
-
-    # knights["arthur"]["hp"] -= knights["red_knight"]["power"] \
-    #     - knights["arthur"]["protection"]
-    # knights["red_knight"]["hp"] -= knights["arthur"]["power"] \
-    #     - knights["red_knight"]["protection"]
-    #
-    # # check if someone fell in battle
-    # knights["arthur"]["hp"] = check_hp_knight(knights["arthur"]["hp"])
-    # knights["red_knight"]["hp"] = check_hp_knight(knights["red_knight"]["hp"])
-    print(knights)
-    # Return battle results:
     return {knights[name]["name"]: knights[name]["hp"] for name in knights}
 
 
--- app/pkg/fighting.py
+++ app/pkg/fighting.py
@@ -1,18 +1,15 @@
-# from app.pkg.knight_config import KNIGHTS
-
-
 def check_hp_knight(hp: int) -> int:
     if hp <= 0:
         hp = 0
     return hp
 
 
-def fighting(first_knight: str, second_knight: str, knights: dict) -> None:
+def fighting(first: str, second: str, knights: dict) -> None:
 
-    knights[first_knight]["hp"] -= knights[second_knight]["power"] \
-        - knights[first_knight]["protection"]
-    knights[second_knight]["hp"] -= knights[first_knight]["power"] \
-        - knights[second_knight]["protection"]
+    knights[first]["hp"] -= knights[second]["power"] \
+        - knights[first]["protection"]
+    knights[second]["hp"] -= knights[first]["power"] \
+        - knights[second]["protection"]
 
-    knights[first_knight]["hp"] = check_hp_knight(knights[first_knight]["hp"])
-    knights[second_knight]["hp"] = check_hp_knight(knights[second_knight]["hp"])
+    knights[first]["hp"] = check_hp_knight(knights[first]["hp"])
+    knights[second]["hp"] = check_hp_knight(knights[second]["hp"])
