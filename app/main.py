f5db9474be4909c95b8c99968654fff4a27f2654
Dmytro Prokofiev <forbuffa@gmail.com>
2022-12-17 11:09:31 +0200

    Changed instances with a loop

--- app/main.py
+++ app/main.py
@@ -4,23 +4,23 @@ from knights.knight import Knight
 
 
 def battle(knightsconfig: dict) -> dict:
-    knights_name_lst = []
-    for name, knight_info in knightsconfig.items():
-        name = Knight(name, knight_info["power"], knight_info["hp"])
-        name.using_armour(knight_info["armour"],
-                          knight_info["weapon"]["power"])
-        name.using_potion(knight_info["potion"])
-        knights_name_lst.append(name)
+    knights_name_dct = {}
+    for knight, knight_info in knightsconfig.items():
+        knight = Knight(knight, knight_info["power"], knight_info["hp"])
+        knight.using_armour(knight_info["armour"],
+                            knight_info["weapon"]["power"])
+        knight.using_potion(knight_info["potion"])
+        knights_name_dct[knight.name] = knight
 
-    first_battle = knights_battle(knights_name_lst[0], knights_name_lst[2])
-    second_battle = knights_battle(knights_name_lst[1], knights_name_lst[3])
+    first_battle = knights_battle(knights_name_dct["lancelot"], knights_name_dct["mordred"])
+    second_battle = knights_battle(knights_name_dct["arthur"], knights_name_dct["red_knight"])
     result = {
         "Lancelot": first_battle["lancelot"],
         "Artur": second_battle["arthur"],
         "Mordred": first_battle["mordred"],
         "Red Knight": second_battle["red_knight"],
     }
-    print(knights_name_lst.append)
+    print(knights_name_dct)
     return result
 
 
