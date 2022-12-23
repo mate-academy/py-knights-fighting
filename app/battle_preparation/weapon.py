5cefa9d6ca2fca1143252755711a0803af6d37b4
Vitalii Khmura <vitalikkhmura13@gmail.com>
2022-12-23 23:21:37 +0200

    Solution without if and enumarating knight_config

--- app/battle_preparation/weapon.py
+++ app/battle_preparation/weapon.py
@@ -2,7 +2,6 @@ from app.knights_dict import LIST_KNIGHT
 
 
 def apply_weapon(knight_config: dict) -> None:
-    for i, knight in enumerate(knight_config):
-        if knight == LIST_KNIGHT[i]:
-            knight_config[knight]["power"] += \
-                knight_config[knight]["weapon"]["power"]
+    for knight in knight_config:
+        knight_config[knight]["power"] += \
+            knight_config[knight]["weapon"]["power"]
