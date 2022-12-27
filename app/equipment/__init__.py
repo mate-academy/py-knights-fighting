2246c8d28deafe0b3f26be9e2e35b9cd6c134ab1
Andrew.H <john.riderr9@gmail.com>
2022-12-27 12:37:27 +0300

    fix flake8

--- app/equipment/__init__.py
+++ app/equipment/__init__.py
@@ -1,3 +1,5 @@
 from .armour import Armour
 from .weapon import Weapon
 from .potion import Potion
+
+__all__ = ["Armour", "Weapon", "Potion"]
--- app/knight/__init__.py
+++ app/knight/__init__.py
@@ -1,1 +1,3 @@
 from .knight import Knight
+
+__all__ = ["Knight"]
