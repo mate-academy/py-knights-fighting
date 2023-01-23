b7d63edc6cea4510812cbd3c5cf441516a221532
Alina Boichenko <ilub1108@gmail.com>
2023-01-23 11:36:27 +0200

    Solution

---
+++ app/knights.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+
+class Knight:
+    def __init__(self, knight: dict) -> None:
+        self.name = knight["name"]
+        self.power = knight["power"]
+        self.hp = knight["hp"]
+        self.protection = 0
+
+        self.power += knight["weapon"]["power"]
+
+        for armour in knight["armour"]:
+            self.protection += armour["protection"]
+
+        if knight["potion"]:
+            if "power" in knight["potion"]["effect"]:
+                self.power += knight["potion"]["effect"]["power"]
+
+            if "protection" in knight["potion"]["effect"]:
+                self.protection += knight["potion"]["effect"]["protection"]
+
+            if "hp" in knight["potion"]["effect"]:
+                self.hp += knight["potion"]["effect"]["hp"]
+
+    def battle_knight(self, other: Knight) -> None:
+        self.hp -= other.power - self.protection
+        other.hp -= self.power - other.protection
+
+        if self.hp <= 0:
+            self.hp = 0
+
+        if other.hp <= 0:
+            other.hp = 0
