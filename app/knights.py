7ab93eab5e66cdd4bb1c674e8249b8c349288980
Tatasha <NatashaLeonidovna319@gmail.com>
2023-01-24 00:43:26 +0200

    'Changes'

--- app/knights.py
+++ app/knights.py
@@ -1,41 +1,53 @@
+from __future__ import annotations
+
+
 class Knights:
 
     def __init__(
             self, name: str,
             power: int, hp: int,
-            protection: int
     ) -> None:
         self.name = name
         self.power = power
         self.hp = hp
-        self.protection = protection
+        self.protection = 0
+
+    def use_potion(self, potions: dict) -> None:
+        if potions is not None:
+            if "hp" in potions["effect"]:
+                self.hp += potions["effect"]["hp"]
+            if "power" in potions["effect"]:
+                self.power += potions["effect"]["power"]
+            if "protection" in potions["effect"]:
+                self.protection += potions["effect"]["protection"]
+
+    def use_armour(self, armours: dict) -> None:
+        if armours is not None:
+            for armor in armours:
+                self.protection += armor["protection"]
+
+    def use_weapon(self, weapon: dict) -> None:
+        if weapon is not None:
+            self.power += weapon["power"]
+
+    def correct_hp(self) -> None:
+        if self.hp < 0:
+            self.hp = 0
+
+    def fight(self, other: Knights) -> None:
+        self.hp -= other.power - self.protection
+        other.hp -= self.power - other.protection
+        self.correct_hp()
+        other.correct_hp()
 
 
-def make_instances(knight: dict) -> list:
-    all_knights = []
-    for knights in knight.values():
-        all_power = knights["power"]
-        all_hp = knights["hp"]
-        all_protection = 0
-
-        if knights["armour"] is not None:
-            for armor in knights["armour"]:
-                all_protection += armor["protection"]
-        if knights["potion"] is not None:
-            for key in knights["potion"]["effect"]:
-                value = knights["potion"]["effect"][key]
-                if key == "power":
-                    all_power += value
-                if key == "hp":
-                    all_hp += value
-                if key == "protection":
-                    all_protection += value
-        if knights["weapon"] is not None:
-            all_power += knights["weapon"]["power"]
-
-        all_knights.append(Knights(
-            name=knights["name"],
-            hp=all_hp,
-            power=all_power,
-            protection=all_protection))
-    return all_knights
+def ready_to_battle(knight_param: dict) -> Knights:
+    knight = Knights(
+        name=knight_param["name"],
+        hp=knight_param["hp"],
+        power=knight_param["power"]
+    )
+    knight.use_weapon(knight_param["weapon"])
+    knight.use_armour(knight_param["armour"])
+    knight.use_potion(knight_param["potion"])
+    return knight
--- app/main.py
+++ app/main.py
@@ -1,45 +1,16 @@
 from __future__ import annotations
-from app.knights import make_instances
+from app.knights import ready_to_battle
 
 
 def battle(knight: dict) -> dict:
-    all_knights = make_instances(knight)
-    lancelot = 0
-    mordred = 0
-    artur = 0
-    red_knight = 0
-    for knights in all_knights:
-        if knights.name == "Lancelot":
-            lancelot = knights
-        if knights.name == "Mordred":
-            mordred = knights
-        if knights.name == "Artur":
-            artur = knights
-        if knights.name == "Red Knight":
-            red_knight = knights
+    lancelot = ready_to_battle(knight_param=knight["lancelot"])
+    mordred = ready_to_battle(knight_param=knight["mordred"])
+    artur = ready_to_battle(knight_param=knight["arthur"])
+    red_knight = ready_to_battle(knight_param=knight["red_knight"])
 
-    # battle lancelot vs mordred
-    lancelot.hp -= mordred.power - lancelot.protection
-    mordred.hp -= lancelot.power - mordred.protection
+    lancelot.fight(other=mordred)
+    artur.fight(other=red_knight)
 
-    # check if someone fell in battle
-    if lancelot.hp <= 0:
-        lancelot.hp = 0
-    if mordred.hp <= 0:
-        mordred.hp = 0
-
-    # battle artur vs red_knight
-    artur.hp -= red_knight.power - artur.protection
-    red_knight.hp -= artur.power - red_knight.protection
-
-    # check if someone fell in battle
-    if artur.hp <= 0:
-        artur.hp = 0
-
-    if red_knight.hp <= 0:
-        red_knight.hp = 0
-
-    # Return battle results:
     return {
         lancelot.name: lancelot.hp,
         artur.name: artur.hp,
