2b214f27218726fc01a1e665a9913b112d9b19a2
Oleksii_Syrota <Oleksiisyrota@gmail.com>
2023-01-25 15:09:53 +0200

    Solution

--- app/knights_config/character_configuration.py
+++ app/knights_config/character_configuration.py
@@ -1,19 +1,15 @@
 class Character:
+    knight_list = {}
 
-    def __init__(self, name_knight: str, knight: dict) -> None:
-        self.name = knight[name_knight]["name"]
-        self.power = knight[name_knight]["power"]
-        self.hp = knight[name_knight]["hp"]
-        self.protection = 0
-        for armour in knight[name_knight]["armour"]:
-            self.protection += armour["protection"]
-        self.power += knight[name_knight]["weapon"]["power"]
-
-        if knight[name_knight]["potion"] is not None:
-            if "power" in knight[name_knight]["potion"]["effect"]:
-                self.power += knight[name_knight]["potion"]["effect"]["power"]
-            if "protection" in knight[name_knight]["potion"]["effect"]:
-                self.protection += \
-                    knight[name_knight]["potion"]["effect"]["protection"]
-            if "hp" in knight[name_knight]["potion"]["effect"]:
-                self.hp += knight[name_knight]["potion"]["effect"]["hp"]
+    def __init__(
+            self,
+            name: str,
+            power: int,
+            hp: int,
+            protection: int
+    ) -> None:
+        self.name = name
+        self.power = power
+        self.hp = hp
+        self.protection = protection
+        Character.knight_list[self.name] = self
--- app/main.py
+++ app/main.py
@@ -4,13 +4,37 @@ from app.battle.battle_knights import Battle
 
 
 def battle(knights_config: dict) -> dict:
-    lancelot = Character(name_knight="lancelot", knight=knights_config)
-    arthur = Character(name_knight="arthur", knight=knights_config)
-    mordred = Character(name_knight="mordred", knight=knights_config)
-    red_knight = Character(name_knight="red_knight", knight=knights_config)
+    for knight in knights_config.values():
+        power = knight["power"]
+        hp = knight["hp"]
+        protection = 0
+        for armour in knight["armour"]:
+            protection += armour["protection"]
+        power += knight["weapon"]["power"]
 
-    Battle(lancelot, mordred).battle_knight()
-    Battle(arthur, red_knight).battle_knight()
+        if knight["potion"] is not None:
+            if "power" in knight["potion"]["effect"]:
+                power += knight["potion"]["effect"]["power"]
+            if "protection" in knight["potion"]["effect"]:
+                protection += knight["potion"]["effect"]["protection"]
+            if "hp" in knight["potion"]["effect"]:
+                hp += knight["potion"]["effect"]["hp"]
+
+        Character(
+            name=knight["name"],
+            power=power,
+            hp=hp,
+            protection=protection
+        )
+
+    Battle(
+        Character.knight_list["Lancelot"],
+        Character.knight_list["Mordred"]
+    ).battle_knight()
+    Battle(
+        Character.knight_list["Artur"],
+        Character.knight_list["Red Knight"]
+    ).battle_knight()
 
     return Battle.result
 
