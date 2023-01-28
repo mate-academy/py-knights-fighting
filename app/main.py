8bdb13b30cc47ecf192ab77cd68167ffc26b8deb
Oleksandr Lysanets <oleksandrlysanets@gmail.com>
2023-01-28 14:50:16 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,8 +2,6 @@ from app.knights.data_knights import knights
 from app.knights.ready_knights import create_knight
 
 
-# задля збереження даних вихідного словника створимо його копію:
-
 ready_knights = knights.copy()
 
 
@@ -11,8 +9,6 @@ def battle(ready_knights: dict) -> dict:
 
     for each in ready_knights:
         ready_knights[each] = create_knight(each, ready_knights)
-
-    # проведемо двобої та виведемо їх результати:
 
     ready_knights["lancelot"].fight(ready_knights["mordred"])
     ready_knights["arthur"].fight(ready_knights["red_knight"])
