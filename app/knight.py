41cc20c4bc0982000b6b3fe8a28f36ae5bec36e3
Bohdan2001007 <bogdan1991007@gmail.com>
2023-02-06 12:37:21 +0100

    Solution

---
+++ app/knight.py
@@ -0,0 +1,26 @@
+class Knight:
+    def __init__(self, name: str, power: int, hp: int) -> None:
+        self.name = name
+        self.power = power
+        self.hp = hp
+        self.protection = 0
+
+    def armour_defence(self, armour: list) -> int:
+        for part in armour:
+            self.protection += part["protection"]
+        return self.protection
+
+    def use_of_weapons(self, weapon: dict) -> int:
+        self.power += weapon["power"]
+        return self.power
+
+    def apply_potion(self, potion: dict) -> None:
+        if potion:
+            potion_effect = potion["effect"]
+            for key in potion_effect:
+                if key == "hp":
+                    self.hp += potion_effect["hp"]
+                if key == "power":
+                    self.power += potion_effect["power"]
+                if key == "protection":
+                    self.protection += potion_effect["protection"]
