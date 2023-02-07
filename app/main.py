9fe433c375417b215a6c5f04eb0d75cd54068d23
Roman Senyshyn <senjaskorp@gmail.com>
2023-02-07 19:06:07 +0200

    flake8 fixed

--- app/main.py
+++ app/main.py
@@ -6,18 +6,12 @@ from app.buttle.preparation_functions import list_of_knights_to_dict,\
 
 def battle(knights_config: dict) -> dict:
     # BATTLE PREPARATIONS:
-
     knights_instances = list_of_knight_instances_from_dict(knights_config)
-
     # BATTLE:
-
     # 1 Lancelot vs Mordred:
-
     start_fight([knights_instances[0], knights_instances[2]])
-
     # 2 Arthur vs Red Knight:
     start_fight([knights_instances[1], knights_instances[3]])
-
     return list_of_knights_to_dict(knights_instances)
 
 
