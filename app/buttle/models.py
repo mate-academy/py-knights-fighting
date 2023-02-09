759dfd57ee678bdc8f8c348b691ee040c743c60b
Roman Senyshyn <senjaskorp@gmail.com>
2023-02-09 22:24:06 +0200

    Moved the logics to classes.

--- app/buttle/models.py
+++ app/buttle/models.py
@@ -36,11 +36,63 @@ class Knight:
         if self.hp <= 0:
             self.hp = 0
 
+    @staticmethod
+    def knights_to_dict(knights_instances: list[Knight]) -> dict:
+        return {
+            knights_instances[0].name: knights_instances[0].hp,
+            knights_instances[1].name: knights_instances[1].hp,
+            knights_instances[2].name: knights_instances[2].hp,
+            knights_instances[3].name: knights_instances[3].hp,
+        }
+
+    @staticmethod
+    def make_list_of_knights(knights_config: dict) -> list[Knight]:
+        knights_instances = []
+        for knight_name in knights_config:
+            knight_dict = knights_config[knight_name]
+
+            armours_list = []
+            if knight_dict.get("armour"):
+                armours_list = Armour.make_armour(knight_dict.get("armour"))
+
+            weapon = Weapon(name=knight_dict["weapon"]["name"],
+                            power=knight_dict["weapon"]["power"])
+
+            potion = None
+            if knight_dict.get("potion"):
+                potion = Potion.make_potion(knight_dict.get("potion"))
+
+            new_knight = Knight(name=knight_dict["name"],
+                                power=knight_dict["power"],
+                                hp=knight_dict["hp"],
+                                armour=armours_list,
+                                weapon=weapon,
+                                potion=potion)
+            new_knight.protection = 0
+
+            if potion:
+                new_knight.potion_bonus(potion)
+
+            new_knight.calculate_protection()
+            new_knight.calculate_power()
+            knights_instances.append(new_knight)
+
+        return knights_instances
+
 
 class Armour:
     def __init__(self, part: str, protection: int) -> None:
         self.part = part
         self.protection = protection
+
+    @staticmethod
+    def make_armour(armour_for_knight: list[dict]) -> list[Armour]:
+        armours_list = []
+        for armour in armour_for_knight:
+            new_armour = Armour(part=armour["part"],
+                                protection=armour["protection"])
+            armours_list.append(new_armour)
+        return armours_list
 
 
 class Weapon:
@@ -60,3 +112,14 @@ class Potion:
     def __init__(self, name: str, effect: Effect) -> None:
         self.name = name
         self.effect = effect
+
+    @staticmethod
+    def make_potion(potion_dict: dict) -> Potion:
+        effect = Effect(hp=potion_dict["effect"]["hp"],
+                        power=potion_dict["effect"]["power"],
+                        protection=potion_dict["effect"].get("protection"))
+
+        potion = Potion(name=potion_dict["name"],
+                        effect=effect)
+
+        return potion
--- app/main.py
+++ app/main.py
@@ -1,16 +1,16 @@
-from app.buttle.start import start_fight
+from app.buttle.action import make_knights
 from app.buttle.knights_data import knights
-from app.buttle.action import knights_to_dict, make_knights
+from app.buttle.models import Knight
+from app.buttle.start import start_fight
 
 
 def battle(knights_config: dict) -> dict:
-
     knights_instances = make_knights(knights_config)
 
     start_fight([knights_instances[0], knights_instances[2]])
     start_fight([knights_instances[1], knights_instances[3]])
 
-    return knights_to_dict(knights_instances)
+    return Knight.knights_to_dict(knights_instances)
 
 
 print(battle(knights))
