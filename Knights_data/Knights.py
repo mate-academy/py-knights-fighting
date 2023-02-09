ceec6db1bc3dbd19d3052dee4f4b14c30dd152e6
Vladyslav Demydenko <bublikyo3@gmail.com>
2023-02-09 20:45:53 +0100

    Removed Knightstats Solution

--- Knights_data/Knights.py
+++ Knights_data/Knights.py
@@ -1,17 +1,38 @@
-from Knights_data.Stats import KnightStats
+from __future__ import annotations
 
 
 class Knight:
-    def __init__(self, knight: KnightStats) -> None:
-        self.hp = knight.hp
-        self.power = knight.power
-        self.protection = knight.protection
+    def __init__(self, name: str, power: int, hp: int, armour: list, weapon: dict, potions: dict) -> None:
+        self.name = name
+        self.power = power + weapon["power"]
+        self.hp = hp
+        self.protection = self.wearing_armour(armour)
+        self.potion = potions
+        self.potions_apply()
 
-    def __sub__(self, other) -> None:
+    @staticmethod
+    def wearing_armour(armours: list) -> int:
+        if armours:
+            return sum(armour["protection"] for armour in armours)
+        return 0
+
+    def potions_apply(self) -> None:
+        if self.potion:
+            if self.potion["effect"]["hp"] is not None:
+                self.hp += self.potion["effect"]["hp"]
+            if self.potion["effect"]["power"] is not None:
+                self.power += self.potion["effect"]["power"]
+            if self.potion["effect"].get("protection") is not None:
+                self.protection += self.potion["effect"]["protection"]
+
+    def __sub__(self, other: Knight) -> None:
         self.hp -= other.power - self.protection
+        other.hp -= self.power - other.protection
+        self.hp_check(other)
+
+    def hp_check(self, other: Knight) -> None:
         if self.hp < 0:
             self.hp = 0
-        other.hp -= self.power - other.protection
         if other.hp < 0:
             other.hp = 0
 
--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-from Knights_data.Stats import KnightStats
 from Knights_data.Knights import Knight
 from Knights_data.unformated import KNIGHTS
 
@@ -6,12 +5,11 @@ from Knights_data.unformated import KNIGHTS
 def battle(knightsconfig: dict) -> dict:
     knights = {}
     for stats in knightsconfig.values():
-        knight = KnightStats(stats["name"], stats["power"], stats["hp"],
-                             stats["armour"], stats["weapon"], stats["potion"])
-        knights[stats["name"]] = Knight(knight)
+        knights[stats["name"]] = Knight(stats["name"], stats["power"],
+                                        stats["hp"], stats["armour"],
+                                        stats["weapon"], stats["potion"])
     knights["Lancelot"] - knights["Mordred"]
     knights["Artur"] - knights["Red Knight"]
-    print(knights)
     return {name: stats.hp for name, stats in knights.items()}
 
 
