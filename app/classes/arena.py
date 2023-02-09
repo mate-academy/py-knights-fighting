3d79c286e1ba4b2b64494fdaf33acd7b1b21c216
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-02-09 08:28:12 +0200

    Change arena file, data package added

--- app/classes/arena.py
+++ app/classes/arena.py
@@ -6,12 +6,27 @@ class Arena:
     fighting_result = {}
 
     @classmethod
-    def fight_in_arena(cls, knight1: Knight, knight2: Knight) -> None:
-        knight1.hp -= knight2.power
-        if knight1.hp <= 0:
-            knight1.hp = 0
-        cls.fighting_result[knight1.name] = knight1.hp
-        knight2.hp -= knight1.power
-        if knight2.hp <= 0:
-            knight2.hp = 0
-        cls.fighting_result[knight2.name] = knight2.hp
+    def fight_in_arena(
+            cls,
+            participants: str,
+            instances: dict[Knight]
+    ) -> None:
+        participant_a, participant_b = participants.split("_VS_")
+
+        cls.fighting_result[participant_a.title().replace("h", "")] = \
+            (
+                instances[participant_a].hp
+                - instances[participant_b].power
+            )
+
+        cls.fighting_result[participant_b.title().replace("_", " ")] = \
+            (
+                instances[participant_b].hp
+                - instances[participant_a].power
+            )
+
+    @classmethod
+    def death_check(cls) -> None:
+        for name, hp in cls.fighting_result.items():
+            if hp <= 0:
+                cls.fighting_result[name] = 0
--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
-import app.classes
+from app.data.knights_data import KNIGHTS
 from app.classes.knights import Knight
 from app.classes.arena import Arena
-
-
 
 
 def battle(knights_dict: dict) -> dict:
@@ -11,17 +9,13 @@ def battle(knights_dict: dict) -> dict:
         knights_instances[name] = Knight(**knights_dict.get(name))
         knights_instances[name].gear_converter()
 
-    Arena.fight_in_arena(
-        knights_instances["lancelot"],
-        knights_instances["mordred"]
-    )
-    Arena.fight_in_arena(
-        knights_instances["arthur"],
-        knights_instances["red_knight"]
-    )
+    Arena.fight_in_arena("lancelot_VS_mordred", knights_instances)
+    Arena.fight_in_arena("arthur_VS_red_knight", knights_instances)
+
+    Arena.death_check()
 
     return Arena.fighting_result
 
 
-if __name__ == '__main__':
-    print(battle(app.classes.KNIGHTS))
+if __name__ == "__main__":
+    print(battle(KNIGHTS))
