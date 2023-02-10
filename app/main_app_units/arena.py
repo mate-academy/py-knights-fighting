3a307b97b9cd413db50560ed9ff9595511abf297
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-02-10 19:49:19 +0200

    fixed indentation at fight_in_arena

--- app/main_app_units/arena.py
+++ app/main_app_units/arena.py
@@ -13,15 +13,19 @@ class Arena:
     ) -> None:
         participant_a, participant_b = participants.split("_VS_")
 
-        cls.fighting_result[participant_a.title().replace("h", "")] = \
-            (
+        cls.fighting_result[
+            participant_a.title().replace("h", "")
+        ] = (
                 instances[participant_a].hp
-                - instances[participant_b].power)
+                - instances[participant_b].power
+        )
 
-        cls.fighting_result[participant_b.title().replace("_", " ")] = \
-            (
+        cls.fighting_result[
+            participant_b.title().replace("_", " ")
+        ] = (
                 instances[participant_b].hp
-                - instances[participant_a].power)
+                - instances[participant_a].power
+        )
 
     @classmethod
     def death_check(cls) -> None:
