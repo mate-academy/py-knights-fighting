a8c2964d4376f66424483a4642763b9e18671f76
Jorjio <Jorjio22@gmail.com>
2023-02-12 16:25:10 +0000

    'Solution'

---
+++ app/preparation.py
@@ -0,0 +1,25 @@
+def preparation(func):
+    def inner(knights: dict):
+        for knight, attributes in knights.items():
+
+            # apply weapon
+            attributes["power"] += attributes["weapon"]["power"]
+
+            # apply armour
+            attributes["protection"] = 0
+            if attributes["armour"] != []:
+                for a in attributes["armour"]:
+                    attributes["protection"] += a["protection"]
+
+            # apply potion if exist
+            if attributes["potion"] is not None:
+                if "power" in attributes["potion"]["effect"]:
+                    attributes["power"] += attributes["potion"]["effect"]["power"]
+
+                if "protection" in attributes["potion"]["effect"]:
+                    attributes["protection"] += attributes["potion"]["effect"]["protection"]
+
+                if "hp" in attributes["potion"]["effect"]:
+                    attributes["hp"] += attributes["potion"]["effect"]["hp"]
+        return func(knights)
+    return inner
