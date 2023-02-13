aca56e675cb6587e6973b368d2a20979c2ddd6c9
ZaikaDaria <corplinebusiness@gmail.com>
2023-02-13 19:44:44 +0200

    Add annotations

--- app/characters.py
+++ app/characters.py
@@ -1,19 +1,22 @@
+from __future__ import annotations
+
+
 class Character:
 
-    def __init__(self, name: str, power: int, hp: int) -> None:
+    def __init__(self, name: str, power: int, hp: int) -> Character:
         self.name = name
         self.power = power
         self.hp = hp
 
-    def use_weapon(self, power: int) -> None:
+    def use_weapon(self, power: int) -> Character:
         self.power += power
 
-    def use_armour(self, armour: list) -> None:
+    def use_armour(self, armour: list) -> Character:
         if armour is not None:
             for item in armour:
                 self.hp += item["protection"]
 
-    def use_potion(self, potion: dict):
+    def use_potion(self, potion: dict) -> Character:
         if potion is not None:
             for key, value in potion["effect"].items():
                 if key == "power":
