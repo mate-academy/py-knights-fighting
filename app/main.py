f85e46eaad50aa66a65a4c6a664d27f66c1c897a
Zenon <zenion@ukr.net>
2023-02-17 17:24:31 +0200

    Changes

--- app/main.py
+++ app/main.py
@@ -99,12 +99,7 @@ def battle(knights_config: dict) -> dict:
     knights["lancelot"].duel(knights["mordred"])
     knights["arthur"].duel(knights["red_knight"])
 
-    return {
-        knights["lancelot"].name: knights["lancelot"].hp,
-        knights["arthur"].name: knights["arthur"].hp,
-        knights["mordred"].name: knights["mordred"].hp,
-        knights["red_knight"].name: knights["red_knight"].hp,
-    }
+    return {knight.name: knight.hp for knight in knights.values()}
 
 
 print(battle(KNIGHTS))
