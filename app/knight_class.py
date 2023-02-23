6899348f21b8de52d0adf1151b2c2a47f9bce068
Dmitry Zheltukhin <bloodviel@gmail.com>
2023-02-23 15:34:47 +0200

    solution

---
+++ app/knight_class.py
@@ -0,0 +1,19 @@
+class Knight:
+    def __init__(self, info: dict) -> None:
+        self.name = info["name"]
+        self.power = info["power"]
+        self.hp = info["hp"]
+        self.protection = 0
+        self.preparing(info)
+
+    def preparing(self, info: dict) -> None:
+        self.power += info["weapon"]["power"]
+
+        if info["armour"]:
+            for part in info["armour"]:
+                self.protection += part["protection"]
+
+        if info["potion"] is not None:
+            for effect in info["potion"]["effect"]:
+                self.__dict__[effect] += \
+                    info["potion"]["effect"].get(effect, 0)
