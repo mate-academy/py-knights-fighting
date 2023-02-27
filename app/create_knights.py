8e1100acefacba19af354c516797187cd6dfaa27
kostya-kononenko <kkononenko3@gmail.com>
2023-02-27 16:44:18 +0200

    Solution1

--- app/create_knights.py
+++ app/create_knights.py
@@ -1,15 +1,13 @@
-from typing import Any
-
 from app.knights import CreateKnights
 
 
 def create_knights(dict_of_knights: dict) -> dict[str, CreateKnights]:
-    knights: dict[Any, Any] = dict()
-    new_dict = {}
+    knights: dict[int] = dict()
+    knights_value = {}
     for value_knights in dict_of_knights.values():
-        total_power: list[Any] = []
-        total_protection: list[Any] = []
-        total_hp: list[Any] = []
+        total_power: list[int] = []
+        total_protection: list[int] = []
+        total_hp: list[int] = []
 
         total_power.append(value_knights["power"])
         total_power.append(value_knights["weapon"].get("power"))
@@ -21,12 +19,14 @@ def create_knights(dict_of_knights: dict) -> dict[str, CreateKnights]:
             total_power.append(value_knights["potion"]["effect"]["power"])
             total_protection.append(value_knights["potion"]
                                     ["effect"].get("protection", 0))
-        new_dict["power"] = sum(total_power)
-        new_dict["protection"] = sum(total_protection)
-        new_dict["hp"] = sum(total_hp)
-        knights[value_knights["name"]] = CreateKnights(value_knights["name"],
-                                                       new_dict["power"],
-                                                       new_dict["protection"],
-                                                       new_dict["hp"])
+        knights_value["power"] = sum(total_power)
+        knights_value["protection"] = sum(total_protection)
+        knights_value["hp"] = sum(total_hp)
+        knights[value_knights["name"]] = CreateKnights(
+            value_knights["name"],
+            knights_value["power"],
+            knights_value["protection"],
+            knights_value["hp"]
+        )
 
     return knights
