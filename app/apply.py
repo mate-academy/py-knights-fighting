e5e4653865d0804b17ff9c55ab26d434aa871a77
Arsen Makovei <makoveyarsen@gmail.com>
2023-03-01 22:59:57 +0200

    'Solution'

---
+++ app/apply.py
@@ -0,0 +1,19 @@
+def apply(knight: dict) -> None:
+    # apply armour
+    knight["protection"] = 0
+    for armour in knight["armour"]:
+        knight["protection"] += armour["protection"]
+
+    # apply weapon
+    knight["power"] += knight["weapon"]["power"]
+
+    # apply potion if exist
+    if knight["potion"] is not None:
+        if "power" in knight["potion"]["effect"]:
+            knight["power"] += knight["potion"]["effect"]["power"]
+
+        if "protection" in knight["potion"]["effect"]:
+            knight["protection"] += knight["potion"]["effect"]["protection"]
+
+        if "hp" in knight["potion"]["effect"]:
+            knight["hp"] += knight["potion"]["effect"]["hp"]
