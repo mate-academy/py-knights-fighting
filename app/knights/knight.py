bce1ace11f58e6ed208da95a7c5f42fca1d739ff
Vitalii Shulepa <vitaliy.shulep@gmail.com>
2023-03-09 00:46:31 +0200

    Solution

---
+++ app/knights/knight.py
@@ -0,0 +1,30 @@
+class CamelotKnights:
+    def __init__(self,
+                 name: str,
+                 power: int,
+                 hp: int,
+                 armour: list,
+                 weapon: dict,
+                 potion: dict) -> None:
+        self.name = name
+        self.power = power
+        self.hp = hp
+        self.armour = armour
+        self.weapon = weapon
+        self.potion = potion
+        self.protection = 0
+
+    def battle_preparation(self) -> None:
+        for elem in self.armour:
+            self.protection += elem["protection"]
+
+        self.power += self.weapon["power"]
+        if self.potion is not None:
+            if "power" in self.potion["effect"]:
+                self.power += self.potion["effect"]["power"]
+
+            if "protection" in self.potion["effect"]:
+                self.protection += self.potion["effect"]["protection"]
+
+            if "hp" in self.potion["effect"]:
+                self.hp += self.potion["effect"]["hp"]
