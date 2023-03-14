f2eadfe58a2fe8d1728171d8d84514269f623c65
oleg-potichnyi <potichnyi.oleg@gmail.com>
2023-03-14 18:24:35 +0200

    Fix flake8

--- app/basic/main.py
+++ app/basic/main.py
@@ -20,7 +20,7 @@ KNIGHTS = {
         "power": 35,
         "hp": 100,
         "armour": [],
-        "weapon": {"name": "Metal Sword", "power": 50,},
+        "weapon": {"name": "Metal Sword", "power": 50},
         "potion": None,
     },
     "arthur": {
@@ -28,11 +28,11 @@ KNIGHTS = {
         "power": 45,
         "hp": 75,
         "armour": [
-            {"part": "helmet", "protection": 15,},
-            {"part": "breastplate", "protection": 20,},
-            {"part": "boots", "protection": 10,},
+            {"part": "helmet", "protection": 15},
+            {"part": "breastplate", "protection": 20},
+            {"part": "boots", "protection": 10},
         ],
-        "weapon": {"name": "Two-handed Sword", "power": 55,},
+        "weapon": {"name": "Two-handed Sword", "power": 55},
         "potion": None,
     },
     "mordred": {
@@ -40,21 +40,21 @@ KNIGHTS = {
         "power": 30,
         "hp": 90,
         "armour": [
-            {"part": "breastplate", "protection": 15,},
-            {"part": "boots", "protection": 10,},
+            {"part": "breastplate", "protection": 15},
+            {"part": "boots", "protection": 10},
         ],
-        "weapon": {"name": "Poisoned Sword", "power": 60,},
+        "weapon": {"name": "Poisoned Sword", "power": 60},
         "potion": {
             "name": "Berserk",
-            "effect": {"power": +15, "hp": -5, "protection": +10,},
+            "effect": {"power": +15, "hp": -5, "protection": +10},
         },
     },
     "red_knight": {
         "name": "Red Knight",
         "power": 40,
         "hp": 70,
-        "armour": [{"part": "breastplate", "protection": 25,}],
+        "armour": [{"part": "breastplate", "protection": 25}],
         "weapon": {"name": "Sword", "power": 45},
-        "potion": {"name": "Blessing", "effect": {"hp": +10, "power": +5,}},
+        "potion": {"name": "Blessing", "effect": {"hp": +10, "power": +5}},
     },
 }
