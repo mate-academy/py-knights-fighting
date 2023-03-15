d0df9eaeaedf2938ce98d314550a43280d8a2f94
oleg-potichnyi <potichnyi.oleg@gmail.com>
2023-03-15 14:29:40 +0200

    Solution, version 3

---
+++ app/main.py
@@ -0,0 +1,70 @@
+from app.stats.knight import Knight
+
+
+def knight_fight(knight1: Knight, knight2: Knight) -> dict:
+    knight2.hp -= knight1.power - knight2.protection
+    knight1.hp -= knight2.power - knight1.protection
+    if knight1.hp < 0:
+        knight1.hp = 0
+    if knight2.hp < 0:
+        knight2.hp = 0
+    return {knight1.name: knight1.hp, knight2.name: knight2.hp}
+
+
+def battle(knight_config: dict) -> Knight:
+    dict_with_knight = {}
+    for knight_name, knight in knight_config.items():
+        dict_with_knight[knight_name] = knight.Knight(**knight)
+    knight_fight(dict_with_knight["lancelot"], dict_with_knight["mordred"])
+    knight_fight(dict_with_knight["arthur"], dict_with_knight["red_knight"])
+    return dict_with_knight
+
+
+
+KNIGHTS = {
+    "lancelot": {
+        "name": "Lancelot",
+        "power": 35,
+        "hp": 100,
+        "armour": [],
+        "weapon": {"name": "Metal Sword", "power": 50},
+        "potion": None,
+    },
+    "arthur": {
+        "name": "Arthur",
+        "power": 45,
+        "hp": 75,
+        "armour": [
+            {"part": "helmet", "protection": 15},
+            {"part": "breastplate", "protection": 20},
+            {"part": "boots", "protection": 10},
+        ],
+        "weapon": {"name": "Two-handed Sword", "power": 55},
+        "potion": None,
+    },
+    "mordred": {
+        "name": "Mordred",
+        "power": 30,
+        "hp": 90,
+        "armour": [
+            {"part": "breastplate", "protection": 15},
+            {"part": "boots", "protection": 10},
+        ],
+        "weapon": {"name": "Poisoned Sword", "power": 60},
+        "potion": {
+            "name": "Berserk",
+            "effect": {"power": +15, "hp": -5, "protection": +10},
+        },
+    },
+    "red_knight": {
+        "name": "Red Knight",
+        "power": 40,
+        "hp": 70,
+        "armour": [{"part": "breastplate", "protection": 25}],
+        "weapon": {"name": "Sword", "power": 45},
+        "potion": {"name": "Blessing", "effect": {"hp": +10, "power": +5}},
+    },
+}
+print(battle(KNIGHTS))
+
+
