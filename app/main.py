1bf01cccc63a2e0272345eaa80962d0705034684
Serhii <85514036+volodinsy@users.noreply.github.com>
2023-03-25 21:07:47 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,24 +2,24 @@ from app.knights import knights
 from app.preparation import preparation
 
 
-
 @preparation
-def battle(knights):
-
+def battle(knights: dict) -> dict:
     # 1 Lancelot vs Mordred:
-    knights["lancelot"]["hp"] -= knights["mordred"]["power"] - knights["lancelot"]["protection"]
-    knights["mordred"]["hp"] -= knights["lancelot"]["power"] - knights["mordred"]["protection"]
 
+    knights["lancelot"]["hp"] -= (knights["mordred"]["power"] -
+                                  knights["lancelot"]["protection"])
+    knights["mordred"]["hp"] -= (knights["lancelot"]["power"] -
+                                 knights["mordred"]["protection"])
     # 2 Arthur vs Red Knight:
-    knights["arthur"]["hp"] -= knights["red_knight"]["power"] - knights["arthur"]["protection"]
-    knights["red_knight"]["hp"] -= knights["arthur"]["power"] - knights["red_knight"]["protection"]
-
+    knights["arthur"]["hp"] -= (knights["red_knight"]["power"] -
+                                knights["arthur"]["protection"])
+    knights["red_knight"]["hp"] -= (knights["arthur"]["power"] -
+                                    knights["red_knight"]["protection"])
     # check if someone fell in battle
     for knight, attributes in knights.items():
         if attributes["hp"] <= 0:
             attributes["hp"] = 0
-
     # Return battle results:
-    return {attributes["name"]: attributes["hp"] for _, attributes in knights.items()}
-
+    return {attributes["name"]: attributes["hp"]
+            for _, attributes in knights.items()}
 print(battle(knights))
