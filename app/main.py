698f1247026311ca0b07eae4fb30bdf140259145
Serhii <85514036+volodinsy@users.noreply.github.com>
2023-03-25 21:14:09 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,17 +3,19 @@ from app.preparation import preparation
 
 
 @preparation
+
+
 def battle(knights: dict) -> dict:
     # 1 Lancelot vs Mordred:
 
     knights["lancelot"]["hp"] -= (knights["mordred"]["power"]
                                   - knights["lancelot"]["protection"])
-    knights["mordred"]["hp"] -= (knights["lancelot"]["power"] 
+    knights["mordred"]["hp"] -= (knights["lancelot"]["power"]
                                  - knights["mordred"]["protection"])
     # 2 Arthur vs Red Knight:
-    knights["arthur"]["hp"] -= (knights["red_knight"]["power"] 
+    knights["arthur"]["hp"] -= (knights["red_knight"]["power"]
                                 - knights["arthur"]["protection"])
-    knights["red_knight"]["hp"] -= (knights["arthur"]["power"] 
+    knights["red_knight"]["hp"] -= (knights["arthur"]["power"]
                                     - knights["red_knight"]["protection"])
     # check if someone fell in battle
     for knight, attributes in knights.items():
