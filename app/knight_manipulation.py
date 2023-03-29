67a860b1cc55e2fdde0965b6ca66a1a8158d64fb
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-03-29 15:37:02 +0300

    func -> class; func from class -> static

---
+++ app/knight_manipulation.py
@@ -0,0 +1,99 @@
+import random
+
+import libraries
+
+knights_obj_list = []
+
+
+def knight_dict_creation():
+    knights_instances_dict = {}
+    for name in libraries.names_of_knights:
+        current_knight = {name: {
+            "name": name.capitalize() if "_" not in name
+            else name.replace("_", " ").title(),
+            "power": random.randint(10, 15),
+            "hp": random.randint(80, 100),
+            "armour": armour_pick(),
+            "weapon": weapon_pick(),
+            "potion": random.choice(libraries.potions)}}
+
+        knights_instances_dict.update(current_knight)
+    return knights_instances_dict
+
+
+def armour_pick():
+    knight_gear = []
+
+    for part in libraries.gear:
+        perk = random.choice(libraries.perks)
+        if perk is not None:
+            knight_gear.append({
+                "part": f"{part} of {perk[0]}",
+                "protection": perk[1]
+            })
+        else:
+            knight_gear.append({
+                "part": f"{part}",
+                "protection": 5
+            })
+
+    return knight_gear
+
+
+def weapon_pick():
+    knight_weapon = {}
+    perk = random.choice(libraries.perks)
+    if perk is not None:
+        knight_weapon["name"] = f"{random.choice(libraries.weapons)} of {perk[0]}"
+        knight_weapon["power"] = 10 + perk[1]
+    else:
+        knight_weapon["name"] = f"{random.choice(libraries.weapons)}"
+        knight_weapon["power"] = 10
+    return knight_weapon
+
+
+class Knight:
+    def __init__(self,
+                 name: str,
+                 power: int,
+                 hp: int,
+                 armour: list[dict],
+                 weapon: dict,
+                 potion: dict):
+        self.name = name
+        self.power = power
+        self.hp = hp
+        self.armour = armour
+        self.weapon = weapon
+        self.potion = potion
+
+    def stats_calculation():
+        for knight_dict in knights_obj_list:
+
+
+            for characteristics in knight_dict.values():
+
+                for equipment in characteristics.armour:
+                    if equipment is not None:
+                        characteristics.hp += equipment["protection"]
+                characteristics.power += characteristics.weapon["power"]
+                if characteristics.potion is not None:
+                    characteristics.potion["effect"]["power"]
+                    characteristics.power += characteristics.potion["effect"]["power"]
+                    characteristics.hp += characteristics.potion["effect"]["hp"]
+                    characteristics.hp += characteristics.potion["effect"]["protection"]
+
+    def knight_obj_creation(participants_dict):
+        global knights_obj_list
+
+        for participant in participants_dict.values():
+            prepared_knight = Knight(name=participant["name"],
+                                     power=participant["power"],
+                                     hp=participant["hp"],
+                                     armour=participant["armour"],
+                                     weapon=participant["weapon"],
+                                     potion=participant["potion"])
+
+            knights_obj_list.append(
+                {participant["name"]: prepared_knight})
+        return knights_obj_list
--- app/main.py
+++ app/main.py
@@ -1,62 +1,52 @@
-from event_master import event_start
-from knights_creation import (
-    Knight,
-    knight_dict_creation,
-    knight_obj_creation,
-    knights_obj_list
-    )
+from knight_manipulation import (
+    Knight, knight_dict_creation,knights_obj_list
+
+)
 
 participants_dict = knight_dict_creation()
+
+
 # print(participants_dict)
 def battle(participants_dict):
-    #Lancelot is battling versus Mordred and Arthur versus Red Knight
-    knight_obj_creation(participants_dict)
+    # Lancelot is battling versus Mordred and Arthur versus Red Knight
+    Knight.knight_obj_creation(participants_dict)
     Knight.stats_calculation()
+    print(knights_obj_list)
+    # !!!!!!!(knights_obj_list[0]["Lancelot"].hp)
 
-    print(knights_obj_list[0]["lancelot"].power)
-    print(knights_obj_list[0]["arthur"].power)
-    print(knights_obj_list[0]["mordred"].power)
-    print(knights_obj_list[0]["red_knight"].power)
-    # knights_obj_list[0]["Lancelot"].hp -= knights_obj_list[0]["Mordred"].power
-    # knights_obj_list[0]["Mordred"].hp -= knights_obj_list[0]["Lancelot"].power
-    # knights_obj_list[0]["Arthur"].hp -= knights_obj_list[0]["Red knight"].power
-    # knights_obj_list[0]["Red knight"].hp -= knights_obj_list[0]["Arthur"].power
-
-    #!!!!!!!(knights_obj_list[0]["Lancelot"].hp)
-
-
-
-    # -------------------------------------------------------------------------------
-#     # BATTLE:
-#
-#     # 1 Lancelot vs Mordred:
-#     lancelot["hp"] -= mordred["power"] - lancelot["protection"]
-#     mordred["hp"] -= lancelot["power"] - mordred["protection"]
-#
-#     # check if someone fell in battle
-#     if lancelot["hp"] <= 0:
-#         lancelot["hp"] = 0
-#
-#     if mordred["hp"] <= 0:
-#         mordred["hp"] = 0
-#
-#     # 2 Arthur vs Red Knight:
-#     arthur["hp"] -= red_knight["power"] - arthur["protection"]
-#     red_knight["hp"] -= arthur["power"] - red_knight["protection"]
-#
-#     # check if someone fell in battle
-#     if arthur["hp"] <= 0:
-#         arthur["hp"] = 0
-#
-#     if red_knight["hp"] <= 0:
-#         red_knight["hp"] = 0
-#
-#     # Return battle results:
-#     return {
-#         lancelot["name"]: lancelot["hp"],
-#         arthur["name"]: arthur["hp"],
-#         mordred["name"]: mordred["hp"],
-#         red_knight["name"]: red_knight["hp"],
-#     }
+    # -------------------       ------------------------------------------------------------
+    #     # BATTLE:
+    #
+    #     # 1 Lancelot vs Mordred:
+    #     lancelot["hp"] -= mordred["power"] - lancelot["protection"]
+    #     mordred["hp"] -= lancelot["power"] - mordred["protection"]
+    #
+    #     # check if someone fell in battle
+    #     if lancelot["hp"] <= 0:
+    #         lancelot["hp"] = 0
+    #
+    #     if mordred["hp"] <= 0:
+    #         mordred["hp"] = 0
+    #
+    #     # 2 Arthur vs Red Knight:
+    #     arthur["hp"] -= red_knight["power"] - arthur["protection"]
+    #     red_knight["hp"] -= arthur["power"] - red_knight["protection"]
+    #
+    #     # check if someone fell in battle
+    #     if arthur["hp"] <= 0:
+    #         arthur["hp"] = 0
+    #
+    #     if red_knight["hp"] <= 0:
+    #         red_knight["hp"] = 0
+    #
+    #     # Return battle results:
+    #     return {
+    #         lancelot["name"]: lancelot["hp"],
+    #         arthur["name"]: arthur["hp"],
+    #         mordred["name"]: mordred["hp"],
+    #         red_knight["name"]: red_knight["hp"],
+    #     }
     return "RESULT"
+
+
 print(battle(participants_dict))
