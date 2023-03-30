b6345c9a3af955671622bedb9ad89cbce2224844
Vika <viktorykoroliova@gmail.com>
2023-03-30 13:31:38 +0300

    solution

---
+++ app/battle/fight.py
@@ -0,0 +1,6 @@
+class Fight:
+    def __init__(self, knights_config: dict, other: dict) -> None:
+        self.knights_config = knights_config
+        self.other = other
+        self.knights_config["hp"] -= \
+            self.other["power"] - self.knights_config["protection"]
