dfaf29fdc01f620b720881f9dde1912d04be5005
vito <vitalytyagunov@gmail.com>
2023-04-09 21:05:38 +0200

    Solution

--- app/characters/knight.py
+++ app/characters/knight.py
@@ -15,9 +15,10 @@ class Knight:
         self.power += self.weapon["power"]
 
         if self.potion is not None:
-            if "power" in self.potion["effect"]:
-                self.power += self.potion["effect"]["power"]
-            if "protection" in self.potion["effect"]:
-                self.protection += self.potion["effect"]["protection"]
-            if "hp" in self.potion["effect"]:
-                self.hp += self.potion["effect"]["hp"]
+            for key in self.potion["effect"]:
+                if key == "power":
+                    self.power += self.potion["effect"][key]
+                if key == "protection":
+                    self.protection += self.potion["effect"][key]
+                if key == "hp":
+                    self.hp += self.potion["effect"][key]
