018687934025e2196572ffb7a55abb573b767dba
Serhiienko Bohdan <serhiienko.b.i@gmail.com>
2023-04-12 15:36:04 +0300

    fresh

--- app/main.py
+++ app/main.py
@@ -1,12 +1,2 @@
-from app.knight_items.knight_manipulation import (for_pytest_func,
-                                                  knight_dict_creation)
-
-
-from typing import Dict
-
-
-def battle(participants_dict: Dict[str, Dict]) -> Dict[str, int]:
-    return for_pytest_func(participants_dict)
-
-
-battle(knight_dict_creation())
+def battle():
+    return None
