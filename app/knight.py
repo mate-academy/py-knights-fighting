4ddb17d1e17e9a0d3fb27cc9e41ad93f6437f3f7
Igor <igornev83@gmail.com>
2023-04-12 19:19:11 +0300

    Tested2

--- app/knight.py
+++ app/knight.py
@@ -4,7 +4,7 @@ class Knight:
         self.power = power
         self.hp = hp
 
-    def using_armour(self, armour: list) -> int:
+    def using_armour(self, armour: list[dict[str, int]]) -> int:
         for part in armour:
             self.hp += part["protection"]
         return self.hp
@@ -13,12 +13,12 @@ class Knight:
         self.power += weapon_power
         return self.power
 
-    def using_potion(self, potion: dict) -> callable:
+    def using_potion(self, potion: dict) -> tuple[int, int]:
         if potion is not None:
             if potion["effect"].get("protection"):
                 self.hp += potion["effect"]["protection"]
-            if potion["effect"]["power"] is not None:
+            if potion["effect"].get("power"):
                 self.power += potion["effect"]["power"]
-            if potion["effect"]["hp"] is not None:
+            if potion["effect"].get("hp"):
                 self.hp += potion["effect"]["hp"]
         return self.hp, self.power
