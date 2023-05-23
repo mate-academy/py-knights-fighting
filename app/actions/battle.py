3405b235a9ad37bef659827b717f0f8356c6bbfb
Dmytro Kolodii <dimakolodiy.ua@gmail.com>
2023-05-23 15:27:03 +0300

    Updated solution

--- app/actions/battle.py
+++ app/actions/battle.py
@@ -1,6 +1,19 @@
 from app.people.knight import Knight
 
 
+def attack(knight1: "Knight", knight2: "Knight") -> int:
+    if knight2.battle_stats["power"] <= knight1.battle_stats["protection"]:
+        return knight1.battle_stats["hp"]
+    elif knight1.battle_stats["hp"] \
+            - knight2.battle_stats["power"] \
+            + knight1.battle_stats["protection"] <= 0:
+        return 0
+    else:
+        return knight1.battle_stats["hp"] \
+            - knight2.battle_stats["power"] \
+            + knight1.battle_stats["protection"]
+
+
 # preset battle
 def battle(dict_of_knights: dict) -> dict:
     list_of_knight_instances = []
@@ -13,27 +26,11 @@ def battle(dict_of_knights: dict) -> dict:
                         knight_data["potion"])
         list_of_knight_instances.append(knight)
     lancelot = list_of_knight_instances[0]
-    lancelot.update_battle_stats()
     arthur = list_of_knight_instances[1]
-    lancelot.update_battle_stats()
     mordred = list_of_knight_instances[2]
-    lancelot.update_battle_stats()
     red_knight = list_of_knight_instances[3]
-    lancelot.update_battle_stats()
 
     # returns hp of knight1 after hit from knight2
-    def attack(knight1: "Knight", knight2: "Knight") -> int:
-
-        if knight2.battle_stats["power"] <= knight1.battle_stats["protection"]:
-            return knight1.battle_stats["hp"]
-        elif knight1.battle_stats["hp"] \
-                - knight2.battle_stats["power"] \
-                + knight1.battle_stats["protection"] <= 0:
-            return 0
-        else:
-            return knight1.battle_stats["hp"] \
-                - knight2.battle_stats["power"] \
-                + knight1.battle_stats["protection"]
 
     result_of_battle = {
         lancelot.name: attack(lancelot, mordred),
