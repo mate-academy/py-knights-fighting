9f79fb6bd5d07179f9b55b363138a13d802871d7
Oknametra <oknametra@gmail.com>
2023-05-24 14:26:58 +0200

    refactored knights fighting

---
+++ app/inventory/apply_inventory.py
@@ -0,0 +1,25 @@
+from app.knights.knights import KNIGHTS
+from typing import List
+
+
+class Inventory:
+    @staticmethod
+    def inventory_application(knights: List[KNIGHTS]) -> None:
+        for knight in knights:
+
+            knight["protection"] = 0
+            for armour_piece in knight["armour"]:
+                knight["protection"] += armour_piece["protection"]
+
+            knight["power"] += knight["weapon"]["power"]
+
+            if knight["potion"] is not None:
+                if "power" in knight["potion"]["effect"]:
+                    knight["power"] += knight["potion"]["effect"]["power"]
+
+                if "protection" in knight["potion"]["effect"]:
+                    knight["protection"] += \
+                        knight["potion"]["effect"]["protection"]
+
+                if "hp" in knight["potion"]["effect"]:
+                    knight["hp"] += knight["potion"]["effect"]["hp"]
