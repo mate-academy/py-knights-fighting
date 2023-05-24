8e6e2e4d8421919fbae7b826444ecbd57b53a992
Oknametra <oknametra@gmail.com>
2023-05-24 17:07:10 +0200

    refactored knights fighting

--- app/main.py
+++ app/main.py
@@ -6,31 +6,23 @@ from app.inventory.apply_inventory import Inventory
 
 
 def battle(knights_config: Dict[str, dict]) -> dict:
-    # BATTLE PREPARATIONS:
 
-    # lancelot
     lancelot = knights_config["lancelot"]
     Inventory.inventory_application([lancelot])
 
-    # arthur
     arthur = knights_config["arthur"]
     Inventory.inventory_application([arthur])
 
-    # mordred
     mordred = knights_config["mordred"]
     Inventory.inventory_application([mordred])
 
-    # red_knight
     red_knight = knights_config["red_knight"]
     Inventory.inventory_application([red_knight])
 
-    # -------------------------------------------------------------------------------
-    # BATTLE:
     Battle.battle(lancelot, mordred)
 
     Battle.battle(arthur, red_knight)
 
-    # Return battle results:
     return {
         lancelot["name"]: lancelot["hp"],
         arthur["name"]: arthur["hp"],
