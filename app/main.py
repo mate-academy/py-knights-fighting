3dce6b26602c744c5b266bc7adb6c1d05b8b647a
Valeriia Tymchenko <maksimova.tymchenko@gmail.com>
2023-06-08 08:21:24 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -4,18 +4,13 @@ from app.knights_info import KNIGHTS
 
 def battle(knights_config: dict) -> dict:
     list_of_instances = []
-    for key in knights_config:
-        knight_instance = Knight(knights_config[key]["name"],
-                                 knights_config[key]["power"],
-                                 knights_config[key]["hp"],
-                                 knights_config[key]["armour"],
-                                 knights_config[key]["weapon"],
-                                 knights_config[key]["potion"])
+    for hero in knights_config:
+        knight_instance = Knight(*knights_config[hero].values())
         knight_instance.preparations()
         list_of_instances.append(knight_instance)
 
-    return list_of_instances[0].battle_result(list_of_instances[2]) | \
-        list_of_instances[1].battle_result(list_of_instances[3])
+    return (list_of_instances[0].battle_result(list_of_instances[2])
+            | list_of_instances[1].battle_result(list_of_instances[3]))
 
 
 print(battle(KNIGHTS))
