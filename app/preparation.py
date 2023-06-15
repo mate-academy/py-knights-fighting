a23b1647b061c387e545ec8367067331f149b3d4
KirillMelanich <kirillmelanich@gmail.com>
2023-06-15 15:21:43 +0300

    'Solution'

---
+++ app/preparation.py
@@ -0,0 +1,24 @@
+class Knight:
+
+    def __init__(self, name: str, power: int, hp: int,
+                 armour: list, weapon: dict, potion: dict) -> None:
+
+        self.name = name
+        self.power = power
+        self.hp = hp
+        self.protection = 0
+        self.armour = armour
+        self.weapon = weapon
+        self.potion = potion
+
+    def preparation(self) -> None:
+        self.protection = sum(item["protection"] for item in self.armour)
+        self.power += self.weapon["power"]
+        if self.potion is not None:
+            if "power" in self.potion["effect"]:
+                self.power += self.potion["effect"]["power"]
+            if "protection" in self.potion["effect"]:
+                self.protection += self.potion["effect"]["protection"]
+            if "hp" in self.potion["effect"]:
+                self.hp += self.potion["effect"]["hp"]
+        return self
