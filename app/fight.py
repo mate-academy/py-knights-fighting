de1b31490f06418f4d0d041b9f911ad92e976491
nailia-mamiedova <nailia.mamiedova.work@gmail.com>
2023-07-03 14:55:42 +0300

    Solution

---
+++ app/fight.py
@@ -0,0 +1,8 @@
+def fight(knight1: dict, knight2: dict) -> tuple:
+    knight1["hp"] -= max(0, knight2["power"] - knight1["protection"])
+    knight2["hp"] -= max(0, knight1["power"] - knight2["protection"])
+
+    knight1["hp"] = max(0, knight1["hp"])
+    knight2["hp"] = max(0, knight2["hp"])
+
+    return knight1, knight2
