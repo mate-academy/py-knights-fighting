212d49a330b9c3f92510b55b0e3af82516a3e456
Dimo-n <dimon.chmara@gmail.com>
2023-07-06 12:39:01 +0300

    Fixed project structure, logic and functions naming

--- app/knight/knight.py
+++ app/knight/knight.py
@@ -1,45 +1,61 @@
 from __future__ import annotations
+from app.ammunition.ammunition import Weapon, Armour, Potion
 
 
 class Knight:
     def __init__(
             self,
-            knight_dict: dict
+            name: str,
+            power: int,
+            hp: int,
+            armour: [Armour],
+            weapon: Weapon,
+            potion: Potion
     ) -> None:
-        self.name = knight_dict.get("name")
-        self.power = knight_dict.get("power")
-        self.hp = knight_dict.get("hp")
-        self.weapon = knight_dict.get("weapon")
-        self.armour = knight_dict.get("armour")
-        self.potion = knight_dict.get("potion")
-        self.battle_ready_stats = self.battle_ready_stats()
+        self.name = name
+        self.power = power
+        self.hp = hp
+        self.armour = armour
+        self.weapon = weapon
+        self.potion = potion
 
-    # calculates battle ready knight's stats
-    def battle_ready_stats(self) -> dict:
+        self.apply_ammunition()
+
+    @classmethod
+    def from_dict(cls, knight_data: dict) -> Knight:
+        armour = [
+            Armour(**armour_data) for armour_data in knight_data.pop("armour")
+        ]
+        weapon = Weapon(**knight_data.pop("weapon"))
+        potion = None if (potion_data := knight_data.pop("potion")
+                          ) is None else Potion(**potion_data)
+
+        return cls(
+            **knight_data,
+            armour=armour,
+            weapon=weapon,
+            potion=potion
+        )
+
+    def apply_ammunition(self) -> int:
         if self.potion is not None:
-            if "power" in self.potion["effect"]:
-                self.power += self.potion["effect"]["power"]
-            if "hp" in self.potion["effect"]:
-                self.hp += self.potion["effect"]["hp"]
-            if "protection" in self.potion["effect"]:
-                self.hp += self.potion["effect"]["protection"]
+            if "power" in self.potion.effect:
+                self.power += self.potion.effect["power"]
+            if "hp" in self.potion.effect:
+                self.hp += self.potion.effect["hp"]
+            if "protection" in self.potion.effect:
+                self.hp += self.potion.effect["protection"]
 
-        # battle_hp -> sum of hp, armour protection ...
-        # ... and(if) potion hp or/and protection
-        battle_hp = self.hp + \
-            sum([armour["protection"] for armour in self.armour])
+        for armour in self.armour:
+            self.hp += armour.protection
 
-        # battle_power -> sum of knight power, weapon power ...
-        # ...and (if) potion power
-        battle_power = self.power + self.weapon["power"]
-        return {"battle_hp": battle_hp, "battle_power": battle_power}
+        self.power += self.weapon.power
+        return self.hp | self.power
 
     def fight(self, other: Knight) -> dict:
-        hp_left = self.battle_ready_stats["battle_hp"] \
-            - other.battle_ready_stats["battle_power"]
+        hp_left = self.hp - other.power
 
-        villain_hp_left = other.battle_ready_stats["battle_hp"] \
-            - self.battle_ready_stats["battle_power"]
+        villain_hp_left = other.hp - self.power
 
         hp_left = max(0, hp_left)
         villain_hp_left = max(0, villain_hp_left)
--- app/main.py
+++ app/main.py
@@ -4,10 +4,10 @@ from app.knight.knight import Knight
 
 
 def battle(knights_config: dict) -> dict:
-    lancelot = Knight(knights_config["lancelot"])
-    mordred = Knight(knights_config["mordred"])
-    arthur = Knight(knights_config["arthur"])
-    red_knight = Knight(knights_config["red_knight"])
+    lancelot = Knight.from_dict(knights_config["lancelot"])
+    mordred = Knight.from_dict(knights_config["mordred"])
+    arthur = Knight.from_dict(knights_config["arthur"])
+    red_knight = Knight.from_dict(knights_config["red_knight"])
 
     return lancelot.fight(mordred) | arthur.fight(red_knight)
 
