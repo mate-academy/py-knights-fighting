149035cd43c14efcb1076c21ae0800aadfdff545
lubart2517 <lubomirvihvatniuk@gmail.com>
2023-07-07 18:30:40 +0300

    implemented function battle and corresponding classes, modules and packages

---
+++ app/logic/belongings.py
@@ -0,0 +1,111 @@
+from __future__ import annotations
+
+
+class Armor:
+    def __init__(
+            self,
+            part: str,
+            protection: int
+    ) -> None:
+        self.part = part
+        self.protection = protection
+
+
+class Weapon:
+    def __init__(
+            self,
+            name: str,
+            power: int
+    ) -> None:
+        self.name = name
+        self.power = power
+
+    @staticmethod
+    def initialize(
+            weapon: dict
+    ) -> Weapon:
+        return Weapon(weapon["name"], weapon["power"])
+
+
+class Potion:
+    def __init__(
+            self,
+            name: str,
+            effect: Effect
+    ) -> None:
+        self.name = name
+        self.effect = effect
+
+    @staticmethod
+    def check_effect(effect_dict: dict) -> Effect:
+        result = Effect.find_effect(Effect.make_key(effect_dict))
+        if result:
+            return result
+        return Effect(
+            hp=effect_dict.get("hp") or 0,
+            power=effect_dict.get("power") or 0,
+            protection=effect_dict.get("protection") or 0
+        )
+
+    @staticmethod
+    def initialize(
+            potion: dict | list
+    ) -> Potion | list[Potion]:
+        if isinstance(potion, dict):
+            return [
+                Potion(
+                    name=potion["name"],
+                    effect=Potion.check_effect(potion["effect"])
+                )
+            ]
+        elif isinstance(potion, list):
+            return [
+                Potion(
+                    name=elem["name"],
+                    effect=Potion.check_effect(elem["effect"]))
+                for elem in potion
+            ]
+        elif potion is None:
+            return []
+
+
+class Effect:
+    effects = []
+
+    def __init__(
+            self,
+            hp: int = 0,
+            power: int = 0,
+            protection: int = 0
+    ) -> None:
+        self.hp = hp
+        self.power = power
+        self.protection = protection
+        self.key = str(hp) + str(power) + str(protection)
+        self.effects.append(self)
+
+    @staticmethod
+    def make_key(
+            effect_dict: dict
+    ) -> str:
+        hp = "0"
+        power = "0"
+        protection = "0"
+        if effect_dict.get("hp"):
+            hp = str(effect_dict.get("hp"))
+        if effect_dict.get("power"):
+            power = str(effect_dict.get("power"))
+        if effect_dict.get("protection"):
+            protection = str(effect_dict.get("protection"))
+        return hp + power + protection
+
+    @classmethod
+    def find_effect(
+            cls,
+            key: str
+    ) -> Effect | False:
+        if cls.effects:
+            for effect in cls.effects:
+                if effect.key == key:
+                    return effect
+        return False
