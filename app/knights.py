3a8556507f989ba8b53bf2f7db6790c2aed7a552
Alla Kuksa <alla.kuksa92@gmail.com>
2023-07-19 08:48:51 +0300

    Solution

--- app/knights.py
+++ app/knights.py
@@ -7,9 +7,8 @@ def apply_armor_and_weapon(knight: dict) -> None:
 def apply_potion(knight: dict) -> None:
     if knight["potion"] is not None:
         effect = knight["potion"]["effect"]
-        knight["power"] += effect.get("power", 0)
-        knight["protection"] += effect.get("protection", 0)
-        knight["hp"] += effect.get("hp", 0)
+        for key in ["power", "protection", "hp"]:
+            knight[key] += effect.get(key, 0)
 
 
 def prepare_knight(knight: dict) -> None:
