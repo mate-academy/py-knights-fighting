362b2348426a57b0f7a73d4c153fffb85bd2f108
Roman Sachavskyi <roman.sachavskyi.ki.2019@lpnu.ua>
2023-07-20 18:27:16 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -3,38 +3,10 @@ from app.config import KNIGHTS
 
 
 def battle(knights: dict) -> dict:
-    knight1 = Knight(
-        knights.get("lancelot").get("name"),
-        knights.get("lancelot").get("power"),
-        knights.get("lancelot").get("hp"),
-        knights.get("lancelot").get("armour"),
-        knights.get("lancelot").get("weapon"),
-        knights.get("lancelot").get("potion")
-    )
-    knight2 = Knight(
-        knights.get("arthur").get("name"),
-        knights.get("arthur").get("power"),
-        knights.get("arthur").get("hp"),
-        knights.get("arthur").get("armour"),
-        knights.get("arthur").get("weapon"),
-        knights.get("arthur").get("potion")
-    )
-    knight3 = Knight(
-        knights.get("mordred").get("name"),
-        knights.get("mordred").get("power"),
-        knights.get("mordred").get("hp"),
-        knights.get("mordred").get("armour"),
-        knights.get("mordred").get("weapon"),
-        knights.get("mordred").get("potion")
-    )
-    knight4 = Knight(
-        knights.get("red_knight").get("name"),
-        knights.get("red_knight").get("power"),
-        knights.get("red_knight").get("hp"),
-        knights.get("red_knight").get("armour"),
-        knights.get("red_knight").get("weapon"),
-        knights.get("red_knight").get("potion")
-    )
+    knight1 = Knight(*[value for value in knights.get("lancelot").values()])
+    knight2 = Knight(*[value for value in knights.get("arthur").values()])
+    knight3 = Knight(*[value for value in knights.get("mordred").values()])
+    knight4 = Knight(*[value for value in knights.get("red_knight").values()])
 
     def fight(first_knight: Knight, second_knight: Knight) -> None:
         first_knight.hp -= second_knight.power - first_knight.protection
