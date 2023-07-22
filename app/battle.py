d2120483f07fe5c9728c099a4512434439a80453
Oleksandr Plaksa <plaksa.sasha@gmail.com>
2023-07-22 22:24:19 +0300

    Solution fixed

--- app/battle.py
+++ app/battle.py
@@ -1,9 +1,13 @@
+from app.battle_preparations.armor import Armour
+# from app.main import KNIGHTS
+
+
 class Fight:
-    def __init__(self, knight_1: dict, knight_2: dict) -> None:
+    def __init__(self, knight_1: Armour, knight_2: Armour) -> None:
         self.knight_1 = knight_1
         self.knight_2 = knight_2
 
-    def battle(self) -> dict:
+    def fight(self) -> dict:
 
         self.knight_1.hp -= self.knight_2.power - self.knight_1.protection
         self.knight_2.hp -= self.knight_1.power - self.knight_2.protection
@@ -13,5 +17,7 @@ class Fight:
         if self.knight_2.hp < 0:
             self.knight_2.hp = 0
 
-        return {self.knight_1.name: self.knight_1.hp,
-                self.knight_2.name: self.knight_2.hp}
+        # return {self.knight_1.name: self.knight_1.hp,
+        #         self.knight_2.name: self.knight_2.hp}
+        # print(self.knight_1.name, self.knight_1.hp)
+        # return self.knight_1, self.knight_2
