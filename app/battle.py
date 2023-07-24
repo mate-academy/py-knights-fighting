a5b377895cc6433c978813907b966bd2acc9c4a1
Oleksandr Plaksa <plaksa.sasha@gmail.com>
2023-07-24 11:08:22 +0300

    add forloop

--- app/battle.py
+++ app/battle.py
@@ -1,5 +1,4 @@
 from app.battle_preparations.armor import Armour
-# from app.main import KNIGHTS
 
 
 class Fight:
@@ -7,7 +6,7 @@ class Fight:
         self.knight_1 = knight_1
         self.knight_2 = knight_2
 
-    def fight(self) -> dict:
+    def fight(self) -> None:
 
         self.knight_1.hp -= self.knight_2.power - self.knight_1.protection
         self.knight_2.hp -= self.knight_1.power - self.knight_2.protection
@@ -16,8 +15,3 @@ class Fight:
             self.knight_1.hp = 0
         if self.knight_2.hp < 0:
             self.knight_2.hp = 0
-
-        # return {self.knight_1.name: self.knight_1.hp,
-        #         self.knight_2.name: self.knight_2.hp}
-        # print(self.knight_1.name, self.knight_1.hp)
-        # return self.knight_1, self.knight_2
