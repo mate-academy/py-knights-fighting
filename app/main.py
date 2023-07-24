dd677e89548318232097dcd2560ce795a4d311b2
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-07-24 18:03:33 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,72 +1,7 @@
-from app.Knights.knight import KNIGHTS
-from app.Battle.fight_red import fights_red
-from app.Battle.fight_mordred import fights_mordred
-from app.Battle.fight_arthur import fights_arthur
+# main.py
+from .battles import battle
+from .knights import KNIGHTS
 
-
-def battle(knightsConfig, red_knight):
-    # BATTLE PREPARATIONS:
-
-    # lancelot
-    lancelot = knightsConfig["lancelot"]
-
-    # apply armour
-    lancelot["protection"] = 0
-    for a in lancelot["armour"]:
-        lancelot["protection"] += a["protection"]
-
-    # apply weapon
-    lancelot["power"] += lancelot["weapon"]["power"]
-
-    # apply potion if exist
-    if lancelot["potion"] is not None:
-        if "power" in lancelot["potion"]["effect"]:
-            lancelot["power"] += lancelot["potion"]["effect"]["power"]
-
-        if "protection" in lancelot["potion"]["effect"]:
-            lancelot["protection"] += lancelot["potion"]["effect"]["protection"]
-
-        if "hp" in lancelot["potion"]["effect"]:
-            lancelot["hp"] += lancelot["potion"]["effect"]["hp"]
-
-    fights_red(knightsConfig)
-    fights_mordred(knightsConfig)
-    fights_arthur(knightsConfig)
-
-    mordred = knightsConfig["mordred"]
-    lancelot["hp"] -= mordred["power"] - lancelot["protection"]
-    mordred["hp"] -= lancelot["power"] - mordred["protection"]
-
-    # check if someone fell in battle
-    if lancelot["hp"] <= 0:
-        lancelot["hp"] = 0
-
-    if mordred["hp"] <= 0:
-        mordred["hp"] = 0
-
-    # arthur
-    arthur = knightsConfig["arthur"]
-
-    arthur["hp"] -= red_knight["power"] - arthur["protection"]
-    red_knight["hp"] -= arthur["power"] - red_knight["protection"]
-
-    # check if someone fell in battle
-    if arthur["hp"] <= 0:
-        arthur["hp"] = 0
-
-    if red_knight["hp"] <= 0:
-        red_knight["hp"] = 0
-
-    red_knight = knightsConfig["red_knight"]
-
-    # Return battle results:
-    return {
-        lancelot["name"]: lancelot["hp"],
-        arthur["name"]: arthur["hp"],
-        mordred["name"]: mordred["hp"],
-        red_knight["name"]: red_knight["hp"],
-    }
-
-
-print(battle(KNIGHTS))
-
+if __name__ == "__main__":
+    battle_results = battle(KNIGHTS)
+    print(battle_results)
