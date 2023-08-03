9267b73414038c96c4e28dc569a07797fb319c59
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-08-03 20:00:34 +0200

    Update Knights.py

--- app/Knights.py
+++ app/Knights.py
@@ -1,4 +1,4 @@
-def apply_effects(knight):
+def apply_effects(knight: dict) -> None:
     # Apply armour
     knight["protection"] = sum(a["protection"] for a in knight["armour"])
 
@@ -14,3 +14,28 @@ def apply_effects(knight):
             knight["protection"] += potion_effects["protection"]
         if "hp" in potion_effects:
             knight["hp"] += potion_effects["hp"]
+
+
+def knight_battle(knight1: dict, knight2: dict) -> None:
+    # Knight1 attacks Knight2
+    knight2["hp"] -= max(0, knight1["power"] - knight2["protection"])
+
+    # Knight2 attacks Knight1
+    knight1["hp"] -= max(0, knight2["power"] - knight1["protection"])
+
+    # Make sure HP is non-negative
+    knight1["hp"] = max(0, knight1["hp"])
+    knight2["hp"] = max(0, knight2["hp"])
+
+
+def battle(knights_config: dict) -> dict:
+    # Apply effects for each knight
+    for knight in knights_config.values():
+        apply_effects(knight)
+
+    # Knight battles
+    knight_battle(knights_config["lancelot"], knights_config["mordred"])
+    knight_battle(knights_config["arthur"], knights_config["red_knight"])
+
+    # Return battle results:
+    return {knight["name"]: knight["hp"] for knight in knights_config.values()}
