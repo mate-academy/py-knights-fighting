a6a98c4570ff66f4daf83a45f3952b3516e809ff
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-08-03 18:11:54 +0200

    Update Knights.py

--- app/Knights.py
+++ app/Knights.py
@@ -1,4 +1,5 @@
 from typing import Dict, Any
+import json
 
 
 def apply_potion(knight: Dict[str, Any]) -> Dict[str, Any]:
@@ -19,3 +20,91 @@ def apply_weapon(knight: Dict[str, Any]) -> Dict[str, Any]:
 
     return knight
     
+KNIGHTS = {
+    "lancelot": {
+        "name": "Lancelot",
+        "power": 35,
+        "hp": 100,
+        "armour": [],
+        "weapon": {
+            "name": "Metal Sword",
+            "power": 50,
+        },
+        "potion": None,
+    },
+    "arthur": {
+        "name": "Arthur",
+        "power": 45,
+        "hp": 75,
+        "armour": [
+            {
+                "part": "helmet",
+                "protection": 15,
+            },
+            {
+                "part": "breastplate",
+                "protection": 20,
+            },
+            {
+                "part": "boots",
+                "protection": 10,
+            }
+        ],
+        "weapon": {
+            "name": "Two-handed Sword",
+            "power": 55,
+        },
+        "potion": None,
+    },
+    "mordred": {
+        "name": "Mordred",
+        "power": 30,
+        "hp": 90,
+        "armour": [
+            {
+                "part": "breastplate",
+                "protection": 15,
+            },
+            {
+                "part": "boots",
+                "protection": 10,
+            }
+        ],
+        "weapon": {
+            "name": "Poisoned Sword",
+            "power": 60,
+        },
+        "potion": {
+            "name": "Berserk",
+            "effect": {
+                "power": 15,
+                "hp": -5,
+                "protection": 10,
+            }
+        }
+    },
+    "red_knight": {
+        "name": "Red Knight",
+        "power": 40,
+        "hp": 70,
+        "armour": [
+            {
+                "part": "breastplate",
+                "protection": 25,
+            }
+        ],
+        "weapon": {
+            "name": "Sword",
+            "power": 45
+        },
+        "potion": {
+            "name": "Blessing",
+            "effect": {
+                "hp": 10,
+                "power": 5,
+            }
+        }
+    }
+}
+
+file_name = "knights.json"
