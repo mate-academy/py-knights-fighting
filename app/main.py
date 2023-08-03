1e21dd6f58e81e184ad2ac388441b442e9d22b34
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-08-03 20:19:57 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
-from knights_data import KNIGHTS
-from Battles import battle
+from app.knights_data import KNIGHTS
+from app.Battles import battle
 
 if __name__ == "__main__":
     print(battle(KNIGHTS))
