db57e0ebafe6cee556c52956d17fa389e0d988a2
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-08-04 20:13:24 +0200

    Update knights.py

--- app/knights.py
+++ app/knights.py
@@ -1,5 +1,5 @@
 def apply_effects(knight: dict) -> None:
-    knight["protection"] = sum(a["protection"] for a in knight["armour"])
+    knight["protection"] = sum(arm["protection"] for arm in knight["armour"])
     knight["power"] += knight["weapon"]["power"]
 
     if knight["potion"] is not None:
@@ -10,14 +10,6 @@ def apply_effects(knight: dict) -> None:
             knight["protection"] += potion_effects["protection"]
         if "hp" in potion_effects:
             knight["hp"] += potion_effects["hp"]
-
-
-def knight_battle(knight1: dict, knight2: dict) -> None:
-    knight2["hp"] -= max(0, knight1["power"] - knight2["protection"])
-    knight1["hp"] -= max(0, knight2["power"] - knight1["protection"])
-
-    knight1["hp"] = max(0, knight1["hp"])
-    knight2["hp"] = max(0, knight2["hp"])
 
 
 def battle(knights_config: dict) -> dict:
