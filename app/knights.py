289aba945edc01b01ae43301a3c96f9dd594b9de
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-08-04 20:30:22 +0200

    Update knights.py

--- app/knights.py
+++ app/knights.py
@@ -16,8 +16,9 @@ def knight_battle(knight1: dict, knight2: dict) -> None:
     knight2["hp"] -= max(0, knight1["power"] - knight2["protection"])
     knight1["hp"] -= max(0, knight2["power"] - knight1["protection"])
 
-    knight1["hp"] -= knight1
-    knight2["hp"] -= knight2
+    knight1["hp"] = max(0, knight1["hp"])
+    knight2["hp"] = max(0, knight2["hp"])
+
 
 
 def battle(knights_config: dict) -> dict:
