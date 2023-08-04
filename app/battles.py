2898554901eeaa2ed72aeb550062ccf329def87b
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-08-04 17:49:26 +0200

    Update battles.py

--- app/battles.py
+++ app/battles.py
@@ -2,13 +2,9 @@ from app.knights import apply_effects
 
 
 def knight_battle(knight1: dict, knight2: dict) -> None:
-    # Knight1 attacks Knight2
     knight2["hp"] -= max(0, knight1["power"] - knight2["protection"])
-
-    # Knight2 attacks Knight1
     knight1["hp"] -= max(0, knight2["power"] - knight1["protection"])
 
-    # Make sure HP is non-negative
     knight1["hp"] = max(0, knight1["hp"])
     knight2["hp"] = max(0, knight2["hp"])
 
@@ -18,9 +14,7 @@ def battle(knights_config: dict) -> dict:
     for knight in knights_config.values():
         apply_effects(knight)
 
-    # Knight battles
     knight_battle(knights_config["lancelot"], knights_config["mordred"])
     knight_battle(knights_config["arthur"], knights_config["red_knight"])
 
-    # Return battle results:
     return {knight["name"]: knight["hp"] for knight in knights_config.values()}
