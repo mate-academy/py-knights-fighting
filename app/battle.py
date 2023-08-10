389f844867a51020cb95dbda7d719e4c39263025
Max Katkalov <makskatkalov@gmail.com>
2023-08-10 21:02:14 +0300

    method of Battle class cnanged + fucn battle updated

--- app/battle.py
+++ app/battle.py
@@ -6,35 +6,18 @@ from app.Knights import Knight
 
 
 class Battle:
-    @staticmethod
-    def make_ready_2_battle(
-            knights_dic: Dict[str, Knight]
-    ) -> None:
-        for knight in knights_dic.values():
-            if knight.potion:
-                knight.potion_effect()
-            knight.apply_armour()
-            knight.add_weapon(knight.weapon)
-            knight.apply_weapon()
 
     @staticmethod
     def make_fight(
-            knights: Dict[str, Knight]
+            knights: Dict[str, Knight],
+            fighter1: str,
+            fighter2: str,
     ) -> None:
-        # 1 Lancelot vs Mordred:
-        knights["Lancelot"].hp -= (
-            knights["Mordred"].power - knights["Lancelot"].protection
+        knights[fighter1].hp -= (
+            knights[fighter2].power - knights[fighter1].protection
         )
-        knights["Mordred"].hp -= (
-            knights["Lancelot"].power - knights["Mordred"].protection
-        )
-
-        # 2 Arthur vs Red Knight:
-        knights["Arthur"].hp -= (
-            knights["Red Knight"].power - knights["Arthur"].protection
-        )
-        knights["Red Knight"].hp -= (
-            knights["Arthur"].power - knights["Red Knight"].protection
+        knights[fighter2].hp -= (
+                knights[fighter1].power - knights[fighter2].protection
         )
 
     @staticmethod
@@ -50,8 +33,6 @@ class Battle:
             knights: Dict[str, Knight]
     ) -> Dict[str, int]:
         return {
-            "Lancelot": knights["Lancelot"].hp,
-            "Arthur": knights["Arthur"].hp,
-            "Mordred": knights["Mordred"].hp,
-            "Red Knight": knights["Red Knight"].hp,
+            fighter.name: fighter.hp
+            for fighter in knights.values()
         }
--- app/main.py
+++ app/main.py
@@ -3,10 +3,17 @@ from app.battle import Battle
 from app.knights_data import KNIGHTS
 
 
-def battle(knigts: dict) -> dict:
+def battle(
+        knigts: dict[str, Knight],
+        knight1: str = "Lancelot",
+        knight2: str = "Mordred",
+        knight3: str = "Arthur",
+        knight4: str = "Red Knight",
+) -> dict:
     knigts_fighters: dict[str, Knight] = Knight.create_dict_of_knghtes(knigts)
 
-    Battle.make_fight(knigts_fighters)
+    Battle.make_fight(knigts_fighters, knight1, knight2)
+    Battle.make_fight(knigts_fighters, knight3, knight4)
     Battle.is_anyone_alive(knigts_fighters)
 
     return Battle.return_result(knigts_fighters)
