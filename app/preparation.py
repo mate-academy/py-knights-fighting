3231292430c92c9d58919e25b1ef2f5c97f97308
OlegatorLE <oleh.travin@gmail.com>
2023-08-10 14:25:15 +0300

    Solution

---
+++ app/preparation.py
@@ -0,0 +1,22 @@
+def preparations(knight: dict) -> dict:
+
+    knight["protection"] = 0
+    for arm in knight["armour"]:
+        knight["protection"] += arm["protection"]
+
+    knight["power"] += knight["weapon"]["power"]
+
+    if knight["potion"] is not None:
+        if "power" in knight["potion"]["effect"]:
+            knight["power"] += knight["potion"]["effect"]["power"]
+        if "protection" in knight["potion"]["effect"]:
+            knight["protection"] += knight["potion"]["effect"]["protection"]
+        if "hp" in knight["potion"]["effect"]:
+            knight["hp"] += knight["potion"]["effect"]["hp"]
+
+    return {
+        "name": knight["name"],
+        "power": knight["power"],
+        "protection": knight["protection"],
+        "hp": knight["hp"]
+    }
