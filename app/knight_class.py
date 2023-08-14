9a4df2973e105bb215de15fcfcac548b6b1d1e44
Olgierrd <oolgierrd@gmail.com>
2023-08-14 16:34:03 +0200

    Solution after changes

--- app/knight_class.py
+++ app/knight_class.py
@@ -1,12 +1,13 @@
 class Knight:
-    def __init__(self,
-                 name: str,
-                 power: int,
-                 hp: int,
-                 armour: list,
-                 weapon: dict,
-                 potion: None | dict,
-                 ) -> None:
+    def __init__(
+            self,
+            name: str,
+            power: int,
+            hp: int,
+            armour: list,
+            weapon: dict,
+            potion: None | dict,
+            ) -> None:
         self.name = name
         self.power = power
         self.hp = hp
