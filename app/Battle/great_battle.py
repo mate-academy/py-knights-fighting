45cd3f6d08ea94d87b2516cc16b6bb3acda69093
Serhii Loboda <serhiy.loboda@gmail.com>
2023-09-04 00:07:03 +0300

    Alteration: Knights Fighting.

---
+++ app/Battle/great_battle.py
@@ -0,0 +1,14 @@
+from app.Knights.knight_class import Knight
+
+
+def battle_action_phase(knight_1: Knight, knight_2: Knight) -> None:
+
+    damage_for_first_knight = knight_2.power - knight_1.protection
+    damage_for_second_knight = knight_1.power - knight_2.protection
+    knight_1.hp -= max(0, damage_for_first_knight)
+    knight_2.hp -= max(0, damage_for_second_knight)
+
+
+def show_result_battle(knight: Knight) -> None:
+
+    knight.hp = max(0, knight.hp)
--- app/main.py
+++ app/main.py
@@ -1,11 +1,9 @@
 from typing import Dict, Any
 
-from app.legendary_knights import KNIGHTS
-from app.structuring_the_knight_personality import Knight
-from app.preparing_for_the_great_battle import BeforeBattle
-from app.great_battle import BattleOfTwoKnight
-
-# ______________________WELCOME TO THE TOURNAMENT__________________________
+from app.Knights.knights_config import KNIGHTS
+from app.Knights.knight_class import Knight
+from app.Battle import preparation_battle
+from app.Battle.great_battle import battle_action_phase, show_result_battle
 
 
 def battle(knights: Dict[str, Any] = KNIGHTS) -> Dict[str, int]:
@@ -14,23 +12,23 @@ def battle(knights: Dict[str, Any] = KNIGHTS) -> Dict[str, int]:
     arthur = Knight(**knights["arthur"])
     mordred = Knight(**knights["mordred"])
     red_knight = Knight(**knights["red_knight"])
-    # list of registered knights for the duel
-    list_knight = [lancelot, arthur, mordred, red_knight]
 
-    for knight in list_knight:
-        BeforeBattle.apply_armour(knight)
-        BeforeBattle.apply_weapon(knight)
-        BeforeBattle.apply_potion(knight)
+    knights_names = [lancelot, arthur, mordred, red_knight]
 
-    BattleOfTwoKnight.battle_action_phase(lancelot, mordred)
-    BattleOfTwoKnight.battle_action_phase(arthur, red_knight)
+    for knight in knights_names:
+        preparation_battle.apply_armour(knight)
+        preparation_battle.apply_weapon(knight)
+        preparation_battle.apply_potion(knight)
 
-    for knight in list_knight:
-        BattleOfTwoKnight.result_after_battle(knight)
+    battle_action_phase(lancelot, mordred)
+    battle_action_phase(arthur, red_knight)
+
+    for knight in knights_names:
+        show_result_battle(knight)
 
     return {
         knight.name: knight.hp
-        for knight in list_knight
+        for knight in knights_names
     }
 
 
