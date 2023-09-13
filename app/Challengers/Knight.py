4a28f2814352b4dff28144d8fba35443177c5492
Philip Plokhikh <filllick13@gmail.com>
2023-09-13 20:32:29 +0300

    "Solution_v3"

--- app/Challengers/Knight.py
+++ app/Challengers/Knight.py
@@ -8,3 +8,11 @@ class Hero:
         self.power = power
         self.hp = hp
         self.armour = armour
+
+    @property
+    def hp(self) -> int:
+        return self._hp
+
+    @hp.setter
+    def hp(self, value: int) -> None:
+        self._hp = value if value >= 0 else 0
--- app/battle/duel.py
+++ app/battle/duel.py
@@ -2,7 +2,5 @@ from app.Challengers.Knight import Hero
 
 
 def fight(first_fighter: Hero, second_fighter: Hero) -> None:
-    first_fighter_damage = first_fighter.power - second_fighter.armour
-    second_fighter_damage = second_fighter.power - first_fighter.armour
-    first_fighter.hp = max(0, first_fighter.hp - second_fighter_damage)
-    second_fighter.hp = max(0, second_fighter.hp - first_fighter_damage)
+    first_fighter.hp -= second_fighter.power - first_fighter.armour
+    second_fighter.hp -= first_fighter.power - second_fighter.armour
