d0ee1643ece1c352cdbb2270e50b1e3932c88483
naz-iv <nazarivankiv1@gmail.com>
2023-10-03 22:28:58 +0300

    Work in progress

---
+++ app/arena.py
@@ -0,0 +1,27 @@
+from app.knight import Knight
+
+
+class Arena:
+
+    battles_result_on_arena = {}
+
+    def __init__(self) -> None:
+        pass
+
+    @classmethod
+    def knight_battle(cls, knight_one: Knight, knight_two: Knight) -> dict:
+        knight_one.hp -= knight_two.power - knight_one.protection
+        knight_two.hp -= knight_one.power - knight_two.protection
+
+        if knight_one.hp < 0:
+            print(f"{knight_one.name} fall in battle!")
+            knight_one.hp = 0
+
+        if knight_two.hp < 0:
+            print(f"{knight_two.name} fall in battle!")
+            knight_two.hp = 0
+
+        cls.battles_result_on_arena[knight_one.name] = knight_one.hp
+        cls.battles_result_on_arena[knight_two.name] = knight_two.hp
+
+        return cls.battles_result_on_arena
---
+++ app/knight.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+
+class Knight:
+
+    def __init__(self,
+                 name: str,
+                 power: int,
+                 hp: int,
+                 armour: list = [],
+                 weapon: dict = {},
+                 potion: dict = {}) -> None:
+        self.name = name
+        self.power = power
+        self.hp = hp
+        self.armour = armour
+        self.weapon = weapon
+        self.potion = potion
+
+    def wear_armour(self) -> None:
+        self.protection = 0
+        if self.armour is not None:
+            for item in self.armour:
+                self.protection += item["protection"]
+
+    def take_weapon(self) -> None:
+        self.power += self.weapon["power"]
+
+    def drink_potion(self) -> None:
+        if self.potion:
+            if "power" in self.potion["effect"]:
+                self.power += self.potion["effect"]["power"]
+
+            if "hp" in self.potion["effect"]:
+                self.hp += self.potion["effect"]["hp"]
+
+            if "protection" in self.potion["effect"]:
+                self.protection += self.potion["effect"]["protection"]
+
+    def prepare_for_battle(self) -> Knight:
+        self.wear_armour()
+        self.take_weapon()
+        self.drink_potion()
+        return self
