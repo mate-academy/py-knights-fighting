a34703c10cd2c8b7f5097b6a86b0eabf13fbd3f1
Yuliia Dudych <juliadudych@gmail.com>
2023-10-19 20:36:49 +0300

    Add logic to reproduce battles

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from app.data.knights import KNIGHTS
+from app.data.tournament import tournament
 from app.modules.knight import Knight
 
 
@@ -7,12 +8,23 @@ def battle(knights_config: dict):
     # PREPARE KNIGHTS DATA:
     participants = create_participants_list(knights_config)
 
+    # CARRY OUT BATTLES
+    for opponents in tournament:
+        left, right = opponents
+        if left in participants and right in participants:
+            fight(participants[left], participants[right])
+
 
 def create_participants_list(knights_data: dict) -> dict:
     for name in knights_data:
         knight = Knight(*knights_data[name].values())
         knights_data[name] = knight
     return knights_data
+
+
+def fight(left_knight: Knight, right_knight: Knight) -> None:
+    left_knight.fighting(right_knight)
+    right_knight.fighting(left_knight)
 
 
 print(battle(KNIGHTS))
