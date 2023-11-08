3fffc0a3cd815798eac80c31f84e18675bcacdfc
TROT <seotrotpol@gmail.com>
2023-11-08 19:56:02 +0200

    "Solution 2"

--- app/main.py
+++ app/main.py
@@ -1,33 +1,10 @@
 from app.input import KNIGHTS
-from app.knights import Knights
+from app.fight import fight
 
 
 def battle(knights_config: dict) -> dict:
-    knight_instances = []
-    for player, info in knights_config.items():
-        name, power, hp, armours, weapons, potion = info.values()
 
-        knight_instance = Knights(
-            name=name,
-            power=power,
-            hp=hp
-        )
-        if armours:
-            protection = []
-            for armour in armours:
-                protection.append(armour["protection"])
-            knight_instance.get_protection(protection)
-
-        knight_instance.get_power(weapons["power"])
-
-        if potion:
-            if "power" in potion["effect"]:
-                knight_instance.get_power(potion["effect"]["power"])
-            if "hp" in potion["effect"]:
-                knight_instance.get_hp(potion["effect"]["hp"])
-            if "protection" in potion["effect"]:
-                knight_instance.get_protection(potion["effect"]["protection"])
-        knight_instances.append(knight_instance)
+    knight_instances = fight(knights_config)
 
     lancelot = knight_instances[0]
     mordred = knight_instances[2]
