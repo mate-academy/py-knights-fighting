0e4ff1842d9fbf79f6b1b61166214b35c1932384
Oksana Boliziuk <oksanabolizyk23@gmail.com>
2023-11-08 12:06:09 +0200

    Add_getattr_setattr

--- app/knight.py
+++ app/knight.py
@@ -12,25 +12,32 @@ class Knight:
         self.hp = hp
         self.armour = armour or []
         self.weapon = weapon
-        self.total_protection = sum(part["protection"] for part in self.armour)
+        self.protection = sum(part["protection"] for part in self.armour)
 
         if potion:
             self.apply_potion(potion)
 
         self.equip_weapon()
 
-    def apply_potion(self, potion: dict) -> None:
-        if "power" in potion["effect"]:
-            self.power += potion["effect"]["power"]
-        if "protection" in potion["effect"]:
-            self.total_protection += potion["effect"]["protection"]
-        if "hp" in potion["effect"]:
-            self.hp += potion["effect"]["hp"]
+    def apply_potion(self, potion) -> None:
+        if potion is not None:
+            for stat, value in potion["effect"].items():
+                if hasattr(self, stat):
+                    current_value = getattr(self, stat)
+                    setattr(self, stat, current_value + value)
+
+    # def apply_potion(self, potion: dict) -> None:
+    #     if "power" in potion["effect"]:
+    #         self.power += potion["effect"]["power"]
+    #     if "protection" in potion["effect"]:
+    #         self.total_protection += potion["effect"]["protection"]
+    #     if "hp" in potion["effect"]:
+    #         self.hp += potion["effect"]["hp"]
 
     def equip_weapon(self) -> None:
         self.power += self.weapon["power"]
 
     def attack(self, other: "Knight") -> None:
-        other.hp -= self.power - other.total_protection
+        other.hp -= self.power - other.protection
         if other.hp < 0:
             other.hp = 0
