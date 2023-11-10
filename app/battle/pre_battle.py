e6fcc5a8537cc67a9248055f4ab44164b46be92c
Seniv Volodymyr <01156240@pw.edu.pl>
2023-11-10 13:57:42 +0100

    solution

---
+++ app/battle/pre_battle.py
@@ -0,0 +1,18 @@
+from preparation.knights import KNIGHTS
+from preparation.class_knight import Knight
+
+
+def pre_battle(KNIGHTS: dict) -> Knight:
+    lancelot = Knight(KNIGHTS["lancelot"])
+    lancelot.config()
+
+    arthur = Knight(KNIGHTS["arthur"])
+    arthur.config()
+
+    mordred = Knight(KNIGHTS["mordred"])
+    mordred.config()
+
+    red_knight = Knight(KNIGHTS["red_knight"])
+    red_knight.config()
+
+    return lancelot, arthur, mordred, red_knight
