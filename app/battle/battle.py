9060fd1df902a248de1b505d836983a1543bd0f6
Prorochenko Oleksandr <prorochenko.ch@gmail.com>
2023-11-15 19:23:53 +0200

    'Solution'

--- app/battle/battle.py
+++ app/battle/battle.py
@@ -2,21 +2,17 @@ from app.knights.knight import Knight
 
 
 def perform_battle(knight1: Knight, knight2: Knight) -> dict:
-    knight1.apply_armour()
-    knight1.apply_weapon()
-    knight1.apply_potion()
-
-    knight2.apply_armour()
-    knight2.apply_weapon()
-    knight2.apply_potion()
+    knights = [knight1, knight2]
+    for knight in knights:
+        knight.apply_armour()
+        knight.apply_weapon()
+        knight.apply_potion()
 
     knight1_hp = knight1.hp - (knight2.power - knight1.protection)
     knight2_hp = knight2.hp - (knight1.power - knight2.protection)
 
-    if knight1_hp <= 0:
-        knight1_hp = 0
-    if knight2_hp <= 0:
-        knight2_hp = 0
+    knight1_hp = max(0, knight1_hp)
+    knight2_hp = max(0, knight2_hp)
 
     return {
         knight1.name: knight1_hp,
