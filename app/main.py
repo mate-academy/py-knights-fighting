7a7b0e55c9cb24ed9376458183e5c902735351e6
Serhii <serhii.domik@gmail.com>
2023-11-18 16:39:45 +0200

    'Fixed'

--- app/main.py
+++ app/main.py
@@ -8,33 +8,14 @@ def battle(knights_dict: dict) -> dict:
     Knight.make_knight(knights_dict)
     knight = Knight.knights
 
-    knight1 = knight["Lancelot"]
-    knight2 = knight["Arthur"]
-    knight3 = knight["Mordred"]
-    knight4 = knight["Red Knight"]
-
-    knight1.hp -= knight3.power - knight1.protection
-    knight3.hp -= knight1.power - knight3.protection
-    knight2.hp -= knight4.power - knight2.protection
-    knight4.hp -= knight2.power - knight4.protection
-
-    if knight1.hp <= 0:
-        knight1.hp = 0
-
-    if knight2.hp <= 0:
-        knight2.hp = 0
-
-    if knight3.hp <= 0:
-        knight3.hp = 0
-
-    if knight4.hp <= 0:
-        knight4.hp = 0
+    Knight.fight(knight["Lancelot"], knight["Mordred"])
+    Knight.fight(knight["Arthur"], knight["Red Knight"])
 
     result_dict.update({
-        knight1.name: knight1.hp,
-        knight2.name: knight2.hp,
-        knight3.name: knight3.hp,
-        knight4.name: knight4.hp
+        knight["Lancelot"].name: knight["Lancelot"].hp,
+        knight["Arthur"].name: knight["Arthur"].hp,
+        knight["Mordred"].name: knight["Mordred"].hp,
+        knight["Red Knight"].name: knight["Red Knight"].hp
     })
 
     return result_dict
--- app/modules/knights.py
+++ app/modules/knights.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+
+
 class Knight:
     knights = {}
 
@@ -42,3 +45,14 @@ class Knight:
                     hp += effect_from_potion["hp"]
 
             Knight(name, power, hp, protection)
+
+    @staticmethod
+    def fight(knight1: Knight, knight2: Knight) -> None:
+        knight1.hp -= knight2.power - knight1.protection
+        knight2.hp -= knight1.power - knight2.protection
+
+        if knight1.hp <= 0:
+            knight1.hp = 0
+
+        if knight2.hp <= 0:
+            knight2.hp = 0
