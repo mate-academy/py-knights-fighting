0f65cbb1757e41f488942ab2572afd3607e15cf8
Ostap <ostapbojko04@gmail.com>
2023-11-22 21:21:56 +0200

    py-knight-fight(2)

--- app/knight_function.py
+++ app/knight_function.py
@@ -1,26 +1,36 @@
 from typing import Dict, Union
 
 
-def prepare_knight(knight: Dict) -> Dict[str, Union[str, int]]:
+def calculate_power_and_potion(knight: Dict) -> Dict[str, int]:
     protection = 0
     for a_armour in knight["armour"]:
         protection += a_armour["protection"]
 
-        def calculate_power_and_potion(knight: Dict) -> None:
-            protection = 0
-            for a_armour in knight["armour"]:
-                protection += a_armour["protection"]
+    power = knight["power"] + knight["weapon"]["power"]
 
-            power = knight["power"] + knight["weapon"]["power"]
+    potion = knight["potion"]
+    if potion:
+        effects = potion.get("effect", {})
+        power += effects.get("power", 0)
+        protection += effects.get("protection", 0)
+        hp = knight["hp"] + effects.get("hp", 0)
+    else:
+        hp = knight["hp"]
 
-            potion = knight["potion"]
-            if potion:
-                effects = potion.get("effect", {})
-                power += effects.get("power", 0)
-                protection += effects.get("protection", 0)
-                hp = knight["hp"] + effects.get("hp", 0)
-            else:
-                hp = knight["hp"]
+    return {
+        "power": power,
+        "protection": protection,
+        "hp": hp,
+    }
+
+
+def prepare_knight(knight: Dict) -> Dict[str, Union[str, int]]:
+    calculated_values = calculate_power_and_potion(knight)
+
+    return {
+        "name": knight["name"],
+        **calculated_values,
+    }
 
 
 def battle(knights: Dict[str, Dict[str, int]]) -> Dict[str, int]:
@@ -46,6 +56,5 @@ def battle(knights: Dict[str, Dict[str, int]]) -> Dict[str, int]:
 
     return {
         name.replace("_", " ").title(): knight["hp"]
-        for name,
-        knight in prepared_knights.items()
+        for name, knight in prepared_knights.items()
     }
