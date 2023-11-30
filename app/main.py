cc40ab84328ca6b87bf71886a8a3d3d5d1c46913
Pavlo-here <pasha.nasonovhtc@gmail.com>
2023-11-30 22:44:29 +0200

    remove comments

--- app/main.py
+++ app/main.py
@@ -6,7 +6,6 @@ from app.utils import (
 
 
 def battle(knights_config: dict) -> dict:
-    # BATTLE PREPARATIONS:
     lancelot = knights_config["lancelot"]
     arthur = knights_config["arthur"]
     mordred = knights_config["mordred"]
@@ -14,14 +13,12 @@ def battle(knights_config: dict) -> dict:
 
     make_preparations(lancelot, arthur, mordred, red_knight)
 
-    # BATTLE:
     make_fight(lancelot, mordred)
     check_health(lancelot, mordred)
 
     make_fight(arthur, red_knight)
     check_health(arthur, red_knight)
 
-    # Return battle results:
     return {
         lancelot["name"]: lancelot["hp"],
         arthur["name"]: arthur["hp"],
