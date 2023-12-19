8b2b7746bd20f9ea7580b696a8bc3d98268b5a8b
angelzagniy <angelzagniy@gmail.com>
2023-12-19 13:54:14 +0200

    remove all comments, create new file for knights configuration, make loop in method apply

--- app/main.py
+++ app/main.py
@@ -1,122 +1,23 @@
 from participants.change_participant import apply
 from participants.battle import after_battle
-
-
-KNIGHTS = {
-    "lancelot": {
-        "name": "Lancelot",
-        "power": 35,
-        "hp": 100,
-        "armour": [],
-        "weapon": {
-            "name": "Metal Sword",
-            "power": 50,
-        },
-        "potion": None,
-    },
-    "arthur": {
-        "name": "Arthur",
-        "power": 45,
-        "hp": 75,
-        "armour": [
-            {
-                "part": "helmet",
-                "protection": 15,
-            },
-            {
-                "part": "breastplate",
-                "protection": 20,
-            },
-            {
-                "part": "boots",
-                "protection": 10,
-            }
-        ],
-        "weapon": {
-            "name": "Two-handed Sword",
-            "power": 55,
-        },
-        "potion": None,
-    },
-    "mordred": {
-        "name": "Mordred",
-        "power": 30,
-        "hp": 90,
-        "armour": [
-            {
-                "part": "breastplate",
-                "protection": 15,
-            },
-            {
-                "part": "boots",
-                "protection": 10,
-            }
-        ],
-        "weapon": {
-            "name": "Poisoned Sword",
-            "power": 60,
-        },
-        "potion": {
-            "name": "Berserk",
-            "effect": {
-                "power": +15,
-                "hp": -5,
-                "protection": +10,
-            }
-        }
-    },
-    "red_knight": {
-        "name": "Red Knight",
-        "power": 40,
-        "hp": 70,
-        "armour": [
-            {
-                "part": "breastplate",
-                "protection": 25,
-            }
-        ],
-        "weapon": {
-            "name": "Sword",
-            "power": 45
-        },
-        "potion": {
-            "name": "Blessing",
-            "effect": {
-                "hp": +10,
-                "power": +5,
-            }
-        }
-    }
-}
+from participants.knights_config import KNIGHTS
 
 
 def battle(knights_config: dict) -> dict:
-    # BATTLE PREPARATIONS:
-
-    # lancelot
     lancelot = apply(knights_config["lancelot"])
 
-    # arthur
     arthur = apply(knights_config["arthur"])
 
-    # mordred
     mordred = apply(knights_config["mordred"])
 
-    # red_knight
     red_knight = apply(knights_config["red_knight"])
 
-    # -------------------------------------------------------------------------------
-    # BATTLE:
-
-    # 1 Lancelot vs Mordred:
     lancelot = after_battle(lancelot, mordred)
     mordred = after_battle(mordred, lancelot)
 
-    # 2 Arthur vs Red Knight:
     arthur = after_battle(arthur, red_knight)
     red_knight = after_battle(red_knight, arthur)
 
-    # Return battle results:
     return {
         lancelot["name"]: lancelot["hp"],
         arthur["name"]: arthur["hp"],
--- participants/change_participant.py
+++ participants/change_participant.py
@@ -2,21 +2,14 @@ def apply(participant: dict) -> dict:
     changed_participant = participant
 
     changed_participant["protection"] = 0
-    for a in changed_participant["armour"]:
-        changed_participant["protection"] += a["protection"]
+    for armour in changed_participant["armour"]:
+        changed_participant["protection"] += armour["protection"]
 
-    # apply weapon
     changed_participant["power"] += changed_participant["weapon"]["power"]
 
-    # apply potion if exist
     if changed_participant["potion"] is not None:
-        if "power" in changed_participant["potion"]["effect"]:
-            changed_participant["power"] += changed_participant["potion"]["effect"]["power"]
-
-        if "protection" in changed_participant["potion"]["effect"]:
-            changed_participant["protection"] += changed_participant["potion"]["effect"]["protection"]
-
-        if "hp" in changed_participant["potion"]["effect"]:
-            changed_participant["hp"] += changed_participant["potion"]["effect"]["hp"]
+        for skill in ["power", "protection", "hp"]:
+            if skill in changed_participant["potion"]["effect"]:
+                changed_participant[skill] += changed_participant["potion"]["effect"][skill]
 
     return changed_participant
