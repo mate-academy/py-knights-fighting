d8c126c6c5fd4e3244d69e8b62eca6fb980e598f
Sikach Illia <55293120+isikach@users.noreply.github.com>
2024-01-15 09:35:27 +0200

    Update kings_start_position.py
    separated the loop with a blank line before and after it

--- app/Kings_fighting/kings_start_position.py
+++ app/Kings_fighting/kings_start_position.py
@@ -12,10 +12,13 @@ class King:
             input_dict["protection"] = sum(part["protection"]
                                            for part in input_dict["armour"])
             input_dict["power"] += input_dict["weapon"]["power"]
+            
             if input_dict["potion"]:
                 potion = input_dict["potion"]["effect"]
+                
                 for name in potion.keys():
                     input_dict[name] += potion[name]
+                    
             __init__(self, input_dict)
         return wrapper
 
