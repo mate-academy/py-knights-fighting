78d4c5645104d5ca5d9113d0c21e39f756ff8d24
Sikach Illia <sikatch.ilja@gmail.com>
2024-01-15 11:55:32 +0200

    Solution

--- app/Kings_fighting/kings_start_position.py
+++ app/Kings_fighting/kings_start_position.py
@@ -2,22 +2,25 @@ from __future__ import annotations
 from typing import Any
 
 
+def magic_upgrades(__init__: Any) -> Any:
+    def wrapper(self: None, input_dict: dict) -> Any:
+        input_dict["protection"] = sum(part["protection"]
+                                       for part in input_dict["armour"])
+        input_dict["power"] += input_dict["weapon"]["power"]
+        if input_dict["potion"]:
+            potion = input_dict["potion"]["effect"]
+
+            for name in potion.keys():
+                input_dict[name] += potion[name]
+
+        __init__(self, input_dict)
+
+    return wrapper
+
+
 class King:
     # It can be useful to check correct start parameters
     start_position = {}
-
-    @staticmethod
-    def magic_upgrades(__init__: Any) -> Any:
-        def wrapper(self: None, input_dict: dict) -> Any:
-            input_dict["protection"] = sum(part["protection"]
-                                           for part in input_dict["armour"])
-            input_dict["power"] += input_dict["weapon"]["power"]
-            if input_dict["potion"]:
-                potion = input_dict["potion"]["effect"]
-                for name in potion.keys():
-                    input_dict[name] += potion[name]
-            __init__(self, input_dict)
-        return wrapper
 
     @magic_upgrades
     def __init__(self, input_dict: dict) -> None:
