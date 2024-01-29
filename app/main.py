1d075f3b386359dc06b420344c01536e2748be2f
Mariia Sydoruk <marisydoruk12@gmail.com>
2024-01-29 13:45:10 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -2,34 +2,99 @@ from app.knight.knight import Knight
 from app.battle.fight import fight
 
 
-KNIGHTS = [
-    Knight(
-        "Lancelot",
-        35, 100,
-        [], {"name": "Metal Sword", "power": 50}, None),
-    Knight("Arthur", 45, 75, [
-        {"part": "helmet", "protection": 15},
-        {"part": "breastplate", "protection": 20},
-        {"part": "boots", "protection": 10}
-    ], {"name": "Two-handed Sword", "power": 55}, None),
-    Knight("Mordred", 30, 90, [
-        {"part": "breastplate", "protection": 15},
-        {"part": "boots", "protection": 10}
-    ], {
-        "name": "Poisoned Sword", "power": 60},
-        {"name": "Berserk",
-         "effect": {
-             "power": 15, "hp": -5, "protection": 10}}),
-    Knight("Red Knight", 40, 70, [
-        {"part": "breastplate", "protection": 25}
-    ], {"name": "Sword", "power": 45}, {"name": "Blessing",
-                                        "effect": {
-                                            "hp": 10, "power": 5}})
-]
+KNIGHTS = {
+    "lancelot": {
+        "name": "Lancelot",
+        "power": 35,
+        "hp": 100,
+        "armour": [],
+        "weapon": {
+            "name": "Metal Sword",
+            "power": 50,
+        },
+        "potion": None,
+    },
+    "arthur": {
+        "name": "Arthur",
+        "power": 45,
+        "hp": 75,
+        "armour": [
+            {
+                "part": "helmet",
+                "protection": 15,
+            },
+            {
+                "part": "breastplate",
+                "protection": 20,
+            },
+            {
+                "part": "boots",
+                "protection": 10,
+            },
+        ],
+        "weapon": {
+            "name": "Two-handed Sword",
+            "power": 55,
+        },
+        "potion": None,
+    },
+    "mordred": {
+        "name": "Mordred",
+        "power": 30,
+        "hp": 90,
+        "armour": [
+            {
+                "part": "breastplate",
+                "protection": 15,
+            },
+            {
+                "part": "boots",
+                "protection": 10,
+            },
+        ],
+        "weapon": {
+            "name": "Poisoned Sword",
+            "power": 60,
+        },
+        "potion": {
+            "name": "Berserk",
+            "effect": {
+                "power": +15,
+                "hp": -5,
+                "protection": +10,
+            },
+        },
+    },
+    "red_knight": {
+        "name": "Red Knight",
+        "power": 40,
+        "hp": 70,
+        "armour": [
+            {
+                "part": "breastplate",
+                "protection": 25,
+            }
+        ],
+        "weapon": {"name": "Sword", "power": 45},
+        "potion": {
+            "name": "Blessing",
+            "effect": {
+                "hp": +10,
+                "power": +5,
+            },
+        },
+    },
+}
 
 
 def battle(knights_config: list) -> dict:
-    knights = [knight.prepare_knight() for knight in knights_config]
+    knights = [
+        Knight(
+            knight["name"], knight["power"], knight["hp"],
+            knight["armour"], knight["weapon"], knight["potion"]
+        ).prepare_knight()
+        for knight in knights_config.values()
+    ]
     battle_results = fight(knights[0], knights[2])
     battle_results.update(fight(knights[1], knights[3]))
     return battle_results
