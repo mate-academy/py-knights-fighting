53a7e5cff4a857086ae3f3ce3cf48eba82919e15
Kolya Ostrovka <mykolaostrovka27@gmail.com>
2024-02-01 13:37:30 +0200

    Solution_3

--- app/main.py
+++ app/main.py
@@ -11,10 +11,10 @@ def battle(knights_config: dict) -> dict:
     ]
 
     for knight in knights:
-        kn = Knight(knight)
-        kn.apply_point()
-        kn.apply_armour()
-        kn.apply_weapon()
+        new_knight_instance = Knight(knight)
+        new_knight_instance.apply_point()
+        new_knight_instance.apply_armour()
+        new_knight_instance.apply_weapon()
 
     fight(knights[0], knights[2])
 
@@ -27,8 +27,6 @@ def battle(knights_config: dict) -> dict:
     check_hp(knights[3])
 
     return {
-        knights[0]["name"]: knights[0]["hp"],
-        knights[1]["name"]: knights[1]["hp"],
-        knights[2]["name"]: knights[2]["hp"],
-        knights[3]["name"]: knights[3]["hp"],
+        knights[number]["name"]: knights[number]["hp"]
+        for number in range(3 + 1)
     }
