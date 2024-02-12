cf93d8ebeca1eaf27dcd2fbdf349f6959ebea9ab
opminen <155957180+opminen@users.noreply.github.com>
2024-02-12 12:20:23 +0200

    Update preparation.py

--- app/knights/preparation.py
+++ app/knights/preparation.py
@@ -29,18 +29,18 @@ class Knight:
             "power": 0,
             "hp": 0
         }
-    
+
         if self.armour is not None:
             for item in self.armour:
                 attributes_to_update["protection"] += item["protection"]
-    
+
         attributes_to_update["power"] += self.weapon["power"]
-    
+
         if self.potion is not None:
             potion_effect = self.potion["effect"]
             for attribute, value in potion_effect.items():
                 if attribute in attributes_to_update:
                     attributes_to_update[attribute] += value
-    
+
         for attribute, value in attributes_to_update.items():
             setattr(self, attribute, getattr(self, attribute) + value)
