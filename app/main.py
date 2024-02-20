edd16f63ad9de702fdbf3feb31e48158c919ea56
Yaroslav Yavornytskyi <ya.yaroslav27@gmail.com>
2024-02-20 14:18:49 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from battle import battle
+from app.knight import Knight
 
 
 KNIGHTS = {
@@ -87,5 +87,33 @@ KNIGHTS = {
         }
     }
 }
+
+
+def create_knight(config: dict) -> Knight:
+    return Knight(
+        name=config["name"],
+        power=config["power"],
+        hp=config["hp"],
+        armour=config.get("armour", []),
+        weapon=config["weapon"],
+        potion=config.get("potion")
+    )
+
+
+def battle(knightsconfig: dict) -> dict:
+    knights = {
+        name: create_knight(info) for name, info in knightsconfig.items()
+    }
+
+    # Lancelot vs Mordred
+    knights["lancelot"].receive_damage(knights["mordred"].power)
+    knights["mordred"].receive_damage(knights["lancelot"].power)
+
+    # Arthur vs Red Knight
+    knights["arthur"].receive_damage(knights["red_knight"].power)
+    knights["red_knight"].receive_damage(knights["arthur"].power)
+    return {knight.name: knight.hp for knight in knights.values()}
+
+
 results = battle(KNIGHTS)
 print(results)
