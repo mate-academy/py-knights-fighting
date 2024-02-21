17b1c9450d38a582f596db1ebb77f01f2dac2f7d
sergey <38cerg12021979@jmal.com>
2024-02-21 16:34:38 +0200

    21_02_24_01

--- app/battle_players/battle_player.py
+++ app/battle_players/battle_player.py
@@ -8,27 +8,19 @@ class Battle:
         self.player4_status = player4
 
     def battls(self) -> dict:
-        self.player1_status["hp"] \
-            -= (self.player2_status["power"]
-                - self.player1_status["protection"])
-
-        self.player2_status["hp"] \
-            -= (self.player1_status["power"]
-                - self.player2_status["protection"])
-
-        self.player3_status["hp"] \
-            -= (self.player4_status["power"]
-                - self.player3_status["protection"])
-
-        self.player4_status["hp"] \
-            -= (self.player3_status["power"]
-                - self.player4_status["protection"])
-
         players = [self.player1_status, self.player2_status,
                    self.player3_status, self.player4_status]
-        for i in players:
-            if i["hp"] <= 0:
-                i["hp"] = 0
+        for index in range(0, len(players), 2):
+            attacker = players[index]
+            target = players[index + 1]
+            damage = max(0, attacker["power"] - target["protection"])
+            target["hp"] -= damage
+            attacker, target = target, attacker
+            damage = max(0, attacker["power"] - target["protection"])
+            target["hp"] -= damage
+        for player in players:
+            if player["hp"] <= 0:
+                player["hp"] = 0
         return {f"""{self.player1_status.get("name")}""":
                 self.player1_status.get("hp"),
                 f"""{self.player3_status.get("name")}""":
