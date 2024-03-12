5b2e1adcac839a7484030dfe1462e21cabf7cf4a
Vsevoloddeez <143706389+Vsevoloddeez@users.noreply.github.com>
2024-03-12 19:24:36 +0100

    Update check_battle.py
    fix

--- app/check_battle.py
+++ app/check_battle.py
@@ -12,8 +12,8 @@ def check_loser(fighter_hp: int) -> int:
 
 
 def fight(first_fighter: Knight, second_fighter: Knight) -> None:
-    first_fighter.hp -= second_fighter.power - first_fighter.protection
-    second_fighter.hp -= first_fighter.power - second_fighter.protection
+    first_fighter.hp -= (second_fighter.power - first_fighter.protection)
+    second_fighter.hp -= (first_fighter.power - second_fighter.protection)
 
     first_fighter.hp = check_loser(first_fighter.hp)
     second_fighter.hp = check_loser(second_fighter.hp)
