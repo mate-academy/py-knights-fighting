00893ad86f097b44895fe94a4fbcadcfce8fac16
Alexander <oleksandr.tsvirko@gmail.com>
2024-03-12 18:10:19 +0100

    solution3

--- app/battlefield/battle.py
+++ app/battlefield/battle.py
@@ -6,19 +6,15 @@ class Battle:
         self.knight_1 = knight_1
         self.knight_2 = knight_2
 
+    @staticmethod
+    def if_die(knight: Knight) -> None:
+        if knight.hp <= 0:
+            knight.hp = 0
+
     def battle(self) -> None:
+
         self.knight_1.hp -= self.knight_2.power - self.knight_1.protection
+        self.if_die(self.knight_1)
+
         self.knight_2.hp -= self.knight_1.power - self.knight_2.protection
-
-        if self.knight_1.hp <= 0:
-            self.knight_1.hp = 0
-        if self.knight_2.hp <= 0:
-            self.knight_2.hp = 0
-
-
-if __name__ == "__main__":
-    knight_1 = Knight(name="Lancelot", power=80, hp=100)
-    knight_2 = Knight(name="red", power=45, hp=75)
-    battle = Battle(knight_1, knight_2)
-    battle.battle()
-    print(knight_1.hp, knight_2.hp)
+        self.if_die(self.knight_2)
