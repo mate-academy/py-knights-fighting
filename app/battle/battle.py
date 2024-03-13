dc67ebc25bc8f10785a0e1d89dc4fa5dddf82ed5
Yaschenko1 <hectorlitopisec@gmail.com>
2024-03-13 19:53:57 +0200

    Solution

--- app/battle/battle.py
+++ app/battle/battle.py
@@ -10,20 +10,17 @@ def battle(knightsconfig: Dict[str, Dict]) -> Dict[str, int]:
         apply_armor_and_weapon(knight)
         apply_effects(knight)
 
-    # Fight sequences
     lancelot = knightsconfig["lancelot"]
     mordred = knightsconfig["mordred"]
     arthur = knightsconfig["arthur"]
     red_knight = knightsconfig["red_knight"]
 
-    # Battle sequence
     lancelot["hp"] -= mordred["power"] - lancelot["protection"]
     mordred["hp"] -= lancelot["power"] - mordred["protection"]
 
     arthur["hp"] -= red_knight["power"] - arthur["protection"]
     red_knight["hp"] -= arthur["power"] - red_knight["protection"]
 
-    # Ensure HP doesn't go negative
     for knight in knightsconfig.values():
         knight["hp"] = max(0, knight["hp"])
 
