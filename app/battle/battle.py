ea9fdc8423ef6961247abf555c23c1b14c80204c
Yaschenko1 <hectorlitopisec@gmail.com>
2024-03-14 13:05:53 +0200

    Solution

--- app/battle/battle.py
+++ app/battle/battle.py
@@ -4,16 +4,16 @@ from app.knights.effects import apply_effects
 from app.knights.armor_and_weapon import apply_armor_and_weapon
 
 
-def battle(knight_sconfig: Dict[str, Dict]) -> Dict[str, int]:
+def battle(knights_config: Dict[str, Dict]) -> Dict[str, int]:
 
-    for knight in knight_sconfig.values():
+    for knight in knights_config.values():
         apply_armor_and_weapon(knight)
         apply_effects(knight)
 
-    lancelot = knight_sconfig["lancelot"]
-    mordred = knight_sconfig["mordred"]
-    arthur = knight_sconfig["arthur"]
-    red_knight = knight_sconfig["red_knight"]
+    lancelot = knights_config["lancelot"]
+    mordred = knights_config["mordred"]
+    arthur = knights_config["arthur"]
+    red_knight = knights_config["red_knight"]
 
     lancelot["hp"] -= mordred["power"] - lancelot["protection"]
     mordred["hp"] -= lancelot["power"] - mordred["protection"]
@@ -21,7 +21,7 @@ def battle(knight_sconfig: Dict[str, Dict]) -> Dict[str, int]:
     arthur["hp"] -= red_knight["power"] - arthur["protection"]
     red_knight["hp"] -= arthur["power"] - red_knight["protection"]
 
-    for knight in knight_sconfig.values():
+    for knight in knights_config.values():
         knight["hp"] = max(0, knight["hp"])
 
-    return {knight["name"]: knight["hp"] for knight in knight_sconfig.values()}
+    return {knight["name"]: knight["hp"] for knight in knights_config.values()}
