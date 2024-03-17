c8fc5fd3ce29cd473f1967108f4f856b6a9ac93e
Vsevoloddeez <143706389+Vsevoloddeez@users.noreply.github.com>
2024-03-17 14:20:50 +0100

    Update knight_stat.py

--- app/knight_stat.py
+++ app/knight_stat.py
@@ -1,5 +1,9 @@
+from typing import Dict, Any, Optional
+
+
 class Knight:
-    def __init__(self, name, hp, power, armour, weapon, potion=None):
+    def __init__(self, name: str, hp: int, power: int, armour: list,
+                 weapon: dict, potion: Optional[dict] = None) -> None:
         self.name = name
         self.hp = hp
         self.power = power
@@ -8,8 +12,7 @@ class Knight:
         self.potion = potion
         self.protection = 0
 
-    
-    def calculate_knight_stat(self):
+    def calculate_knight_stat(self) -> Dict[str, Any]:
         self.protection = 0
 
         for part in self.armour:
@@ -21,7 +24,8 @@ class Knight:
             stat_list = ["power", "hp", "protection"]
             for stat in stat_list:
                 if stat in self.potion["effect"]:
-                    setattr(self, stat, getattr(self, stat) + self.potion["effect"][stat])
+                    setattr(self, stat,
+                            getattr(self, stat) + self.potion["effect"][stat])
 
         return {
             "name": self.name,
