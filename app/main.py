5eb272049e9786edf9f2c2553b6d41b7c96f9af5
donbas777 <renary34@gmail.com>
2024-03-17 22:14:13 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -12,20 +12,6 @@ def battle(knights_config: dict) -> dict:
     fight(knight1=lancelot, knight2=mordred)
     fight(knight1=arthur, knight2=red_knight)
 
-from app.battle import battle
-from app.KNIGHTS.knights import KnightsConfig
-from app.KNIGHTS.knight import Knight
-
-
-lancelot = Knight(KnightsConfig["lancelot"])
-arthur = Knight(KnightsConfig["arthur"])
-mordred = Knight(KnightsConfig["mordred"])
-red_knight = Knight(KnightsConfig["red_knight"])
-
-
-def main() -> dict:
-    battle(knight1=lancelot, knight2=arthur)
-    battle(knight1=mordred, knight2=red_knight)
     return {
         lancelot.name: lancelot.hp,
         arthur.name: arthur.hp,
@@ -35,7 +21,3 @@ def main() -> dict:
 
 
 print(battle(Knights))
-        red_knight.name: red_knight.hp,
-    }
-
-print(main())
