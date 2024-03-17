868859f836466dc7f6c707bbd2da11d448612dcb
Serhii Kravchenko <serhii.kravchenko.q@gmail.com>
2024-03-17 13:46:56 +0100

    'Solution'

--- app/battle.py
+++ app/battle.py
@@ -1,17 +1,12 @@
-from app.models.knight import Knight
-from typing import Dict
+from app.KNIGHTS.knight import Knight
 
 
-class Battle:
-    @staticmethod
-    def fight(knight1: Knight, knight2: Knight) -> None:
-        knight1.hp -= max(0, knight2.power - knight1.protection)
-        knight2.hp -= max(0, knight1.power - knight2.protection)
-
-    @staticmethod
-    def calculate_result(knights: Dict[str, Knight]) -> Dict[str, int]:
-        for knight in knights.values():
-            knight.prepare_for_battle()
-        Battle.fight(knights["Lancelot"], knights["Mordred"])
-        Battle.fight(knights["Arthur"], knights["Red Knight"])
-        return {k.name: max(0, k.hp) for k in knights.values()}
+def fight(knight1: "Knight", knight2: "Knight") -> None:
+    knight1.potion_activate()
+    knight2.potion_activate()
+    knight1.hp -= knight2.power - knight1.protection
+    knight2.hp -= knight1.power - knight2.protection
+    if knight1.hp < 0:
+        knight1.hp = 0
+    if knight2.hp < 0:
+        knight2.hp = 0
