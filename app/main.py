b8d1f1f4f73e1e8cdcc0849064dd3325a629b550
serhii_chaban <boetz1995@gmail.com>
2024-03-25 10:10:43 +0200

    simplified class Knight

--- app/main.py
+++ app/main.py
@@ -1,24 +1,24 @@
 from app.preparing_knights.create import create_knights
-from app.preparing_knights.knights import Knights
 
 
 def battle(knights: dict) -> dict:
-    create_knights(knights)
+    fighters = {
+        name: create_knights(knight) for name, knight in knights.items()
+    }
 
-    lancelot = Knights.knights_inst["Lancelot"]
-    mordred = Knights.knights_inst["Mordred"]
-    arthur = Knights.knights_inst["Arthur"]
-    red_knight = Knights.knights_inst["Red Knight"]
+    lancelot = fighters.get("lancelot")
+    mordred = fighters.get("mordred")
+    arthur = fighters.get("arthur")
+    red_knight = fighters.get("red_knight")
 
     lancelot.hp -= mordred.power - lancelot.protection
     mordred.hp -= lancelot.power - mordred.protection
     arthur.hp -= red_knight.power - arthur.protection
     red_knight.hp -= arthur.power - red_knight.protection
 
-    Knights.recount_hp()
     return {
-        lancelot.name: lancelot.hp,
-        arthur.name: arthur.hp,
-        mordred.name: mordred.hp,
-        red_knight.name: red_knight.hp,
+        lancelot.name: max(0, lancelot.hp),
+        arthur.name: max(0, arthur.hp),
+        mordred.name: max(0, mordred.hp),
+        red_knight.name: max(0, red_knight.hp),
     }
--- app/preparing_knights/create.py
+++ app/preparing_knights/create.py
@@ -1,7 +1,7 @@
 from app.equipment.armour import Armour
 from app.equipment.potion import Potion
 from app.equipment.weapon import Weapon
-from app.preparing_knights.knights import Knights
+from app.preparing_knights.knight import Knight
 
 
 def create_armour(knight: dict) -> list[Armour]:
@@ -23,14 +23,30 @@ def create_potion(knight: dict) -> Potion | None:
         return Potion(potion["name"], potion["effect"])
 
 
-def create_knights(knights: dict) -> None:
-    for name, knight in knights.items():
-        Knights(
-            knight["name"],
-            knight["power"],
-            knight["hp"],
-            create_armour(knight),
-            create_weapon(knight),
-            create_potion(knight),
+def create_knights(knight: dict) -> Knight:
+    instance = Knight(
+        knight.get("name"),
+        knight.get("power"),
+        knight.get("hp"),
+        create_armour(knight),
+        create_weapon(knight),
+        create_potion(knight),
+    )
+    apply_knights(instance)
+    return instance
+
+
+def apply_knights(knight: Knight) -> None:
+    if knight.armour:
+        knight.protection += sum(
+            [armour.protection for armour in knight.armour]
         )
-    Knights.apply_knights()
+
+    knight.power += knight.weapon.power
+
+    if knight.potion:
+        knight.power += knight.potion.effect.get("power", 0)
+        knight.hp += knight.potion.effect.get("hp", 0)
+        knight.protection += knight.potion.effect.get(
+            "protection", 0
+        )
