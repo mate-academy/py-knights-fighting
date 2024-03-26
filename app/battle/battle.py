a8ce6f7dfd00215ec2e94fff448272b1b434bf80
Мар'яна Тиха <mariana@tikhaya@gmail.com>
2024-03-26 15:15:42 +0200

    fixed without while

--- app/battle/battle.py
+++ app/battle/battle.py
@@ -10,15 +10,18 @@ class Battle:
         print("Fight!")
         print(f"{self.knight1.name} against {self.knight2.name}")
 
-        while self.knight1.hp > 0 and self.knight2.hp > 0:
-            print(f"{self.knight1.name} has {self.knight1.hp} life")
-            print(f"{self.knight2.name} has {self.knight2.hp} life")
+        damage_knight1 = max(self.knight1.power - self.knight2.defense, 0)
+        damage_knight2 = max(self.knight2.power - self.knight1.defense, 0)
 
-            damage_knight1 = max(self.knight1.power - self.knight2.defense, 0)
-            damage_knight2 = max(self.knight2.power - self.knight1.defense, 0)
+        self.knight1.hp -= damage_knight2
+        self.knight2.hp -= damage_knight1
 
-            self.knight1.hp -= damage_knight2
-            self.knight2.hp -= damage_knight1
+        print(f"{self.knight1.name} has {self.knight1.hp} life")
+        print(f"{self.knight2.name} has {self.knight2.hp} life")
 
-            if self.knight1.hp <= 0 or self.knight2.hp <= 0:
-                break
+        if self.knight1.hp <= 0:
+            print(f"{self.knight2.name} wins!")
+        elif self.knight2.hp <= 0:
+            print(f"{self.knight1.name} wins!")
+        else:
+            print("The battle continues!")
