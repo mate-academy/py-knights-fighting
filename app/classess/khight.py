6db7ab0278f97d926e0ee9ab193608651260872f
Sivkovich Danylo <danilsivkovic@gmail.com>
2024-04-24 20:31:34 +0300

    My problem

---
+++ app/classess/khight.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+
+class Knight:
+    knights = []
+
+    def __init__(self, name: str, power: int, protection: int,
+                 hp: int) -> None:
+        self.name = name
+        self. power = power
+        self.hp = hp
+        self.protection = protection
+        Knight.knights.append(self)
+
+    def battle(self, other: Knight) -> None:
+        self.hp -= other.power - self.protection
+        other.hp -= self.power - other.protection
+        if self.hp < 0:
+            self.hp = 0
+        if other.hp < 0:
+            other.hp = 0
