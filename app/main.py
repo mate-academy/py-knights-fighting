62930a7200933db9fe285dae7dbbfbf9ee3ae9fe
Vsevoloddeez <143706389+Vsevoloddeez@users.noreply.github.com>
2024-04-27 23:51:20 +0300

    Update main.py
    fix

--- app/main.py
+++ app/main.py
@@ -8,8 +8,7 @@ def battle(knights_config: dict) -> dict:
     knights = []
 
     for knight_data in knights_config.values():
-        knight = Knight(knight_data["name"], 
-                        knight_data["power"], knight_data["hp"])
+        knight = Knight(knight_data["name"], knight_data["power"], knight_data["hp"])
 
         # Equip armour
         for armour_data in knight_data["armour"]:
@@ -57,44 +56,3 @@ def battle(knights_config: dict) -> dict:
         mordred.name: mordred.hp,
         red_knight.name: red_knight.hp
     }
-
-
-# Call battle function with your knights_config dictionary
-knights_config = {
-    "Lancelot": {
-        "name": "Lancelot",
-        "power": 100,
-        "hp": 200,
-        "armour": [{"part": "Chestplate", "protection": 50}],
-        "weapon": {"name": "Excalibur", "power": 80},
-        "potion": {"name": "Healing Potion", "effect": "heal"}
-    },
-    "Arthur": {
-        "name": "Arthur",
-        "power": 90,
-        "hp": 180,
-        "armour": [{"part": "Helmet", "protection": 30}],
-        "weapon": {"name": "Caliburn", "power": 75},
-        "potion": None
-    },
-    "Mordred": {
-        "name": "Mordred",
-        "power": 95,
-        "hp": 190,
-        "armour": [{"part": "Greaves", "protection": 40}],
-        "weapon": {"name": "Clarent", "power": 85},
-        "potion": None
-    },
-    "Red Knight": {
-        "name": "Red Knight",
-        "power": 85,
-        "hp": 170,
-        "armour": [{"part": "Shield", "protection": 35}],
-        "weapon": {"name": "Crimson Blade", "power": 70},
-        "potion": None
-    }
-}
-
-battle_results = battle(knights_config)
-print(battle_results)
-
