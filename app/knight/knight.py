0543304063c10288a3ae723d21a24cc594c62bc9
Taras Tkachuk <tarastkachuk123@gmail.com>
2024-05-14 21:48:40 +0300

    Solution

---
+++ app/knight/knight.py
@@ -0,0 +1,34 @@
+class Knight:
+    def __init__(self, knights_config: dict) -> None:
+        self.knights_config = knights_config
+        self.name = knights_config["name"]
+        self.hp = self.knights_config["hp"]
+        self.power = (
+            self.knights_config["power"]
+            + self.knights_config["weapon"]["power"]
+        )
+        self.protection = 0
+
+    def calculate_stats(self) -> dict:
+        self.protection = self.apply_armour()
+        self.apply_potion()
+        return {
+            "hp": self.hp,
+            "power": self.power,
+            "protection": self.protection
+        }
+
+    def apply_armour(self) -> int:
+        return sum(
+            armour["protection"] for armour in self.knights_config["armour"]
+        )
+
+    def apply_potion(self) -> None:
+        if self.knights_config["potion"]:
+            if "hp" in self.knights_config["potion"]["effect"]:
+                self.hp += self.knights_config["potion"]["effect"]["hp"]
+            if "power" in self.knights_config["potion"]["effect"]:
+                self.power += self.knights_config["potion"]["effect"]["power"]
+            if "protection" in self.knights_config["potion"]["effect"]:
+                self.protection += (
+                    self.knights_config)["potion"]["effect"]["protection"]
