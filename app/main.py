edad25cfbe3124eb0b2272bb24509909d8c78a8b
Oleksandr Rozvazhaiev <alexrozv@gmail.com>
2024-05-17 14:54:09 +0300

    moved data and added TODO

--- app/main.py
+++ app/main.py
@@ -1,110 +1,25 @@
+from app.data.knights_data import KNIGHTS
 from app.knight import Knight
 
-KNIGHTS = {
-    "lancelot": {
-        "name": "Lancelot",
-        "power": 35,
-        "hp": 100,
-        "armour": [],
-        "weapon": {
-            "name": "Metal Sword",
-            "power": 50,
-        },
-        "potion": None,
-    },
-    "arthur": {
-        "name": "Arthur",
-        "power": 45,
-        "hp": 75,
-        "armour": [
-            {
-                "part": "helmet",
-                "protection": 15,
-            },
-            {
-                "part": "breastplate",
-                "protection": 20,
-            },
-            {
-                "part": "boots",
-                "protection": 10,
-            }
-        ],
-        "weapon": {
-            "name": "Two-handed Sword",
-            "power": 55,
-        },
-        "potion": None,
-    },
-    "mordred": {
-        "name": "Mordred",
-        "power": 30,
-        "hp": 90,
-        "armour": [
-            {
-                "part": "breastplate",
-                "protection": 15,
-            },
-            {
-                "part": "boots",
-                "protection": 10,
-            }
-        ],
-        "weapon": {
-            "name": "Poisoned Sword",
-            "power": 60,
-        },
-        "potion": {
-            "name": "Berserk",
-            "effect": {
-                "power": +15,
-                "hp": -5,
-                "protection": +10,
-            }
-        }
-    },
-    "red_knight": {
-        "name": "Red Knight",
-        "power": 40,
-        "hp": 70,
-        "armour": [
-            {
-                "part": "breastplate",
-                "protection": 25,
-            }
-        ],
-        "weapon": {
-            "name": "Sword",
-            "power": 45
-        },
-        "potion": {
-            "name": "Blessing",
-            "effect": {
-                "hp": +10,
-                "power": +5,
-            }
-        }
-    }
-}
 
-
-def battle(knightsConfig):
+# TODO: refactor battle() method and move to separate module
+def battle(knights_config):
     # BATTLE PREPARATIONS:
 
     # lancelot
-    lancelot = Knight.create_knight(knightsConfig["lancelot"])
+    lancelot = Knight.create_knight(knights_config["lancelot"])
     lancelot.prepare_for_battle()
 
     # arthur
-    arthur = Knight.create_knight(knightsConfig["arthur"])
+    arthur = Knight.create_knight(knights_config["arthur"])
     arthur.prepare_for_battle()
 
     # mordred
-    mordred = Knight.create_knight(knightsConfig["mordred"])
+    mordred = Knight.create_knight(knights_config["mordred"])
     mordred.prepare_for_battle()
 
     # red_knight
-    red_knight = Knight.create_knight(knightsConfig["red_knight"])
+    red_knight = Knight.create_knight(knights_config["red_knight"])
     red_knight.prepare_for_battle()
 
     # -------------------------------------------------------------------------------
