bd6784de90956384ddef360e21406db9b1448722
lilarin <80650911+lilarin@users.noreply.github.com>
2024-05-26 14:24:54 +0300

    Solution refinement (flake8 single quote fix)

--- app/main.py
+++ app/main.py
@@ -4,11 +4,11 @@ from app.battle import battle
 
 
 def load_knights_config(filename: str) -> Dict[str, Dict]:
-    with open(filename, 'r') as file:
+    with open(filename, "r") as file:
         return json.load(file)
 
 
-knights_config = load_knights_config('../app/knights.json')
+knights_config = load_knights_config("../app/knights.json")
 
 battle_results = battle(knights_config)
 print(battle_results)
