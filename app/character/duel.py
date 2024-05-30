61072616f073caa0bec1bc51a4dd756bcaa8503b
AndreiVed <catblues87@gmail.com>
2024-05-30 15:33:20 +0300

    solution

--- app/character/duel.py
+++ app/character/duel.py
@@ -1,4 +1,8 @@
-def duel(knight1: object, knight2: object) -> dict:
+from __future__ import annotations
+from app.character.knight import Knight
+
+
+def duel(knight1: Knight, knight2: Knight) -> dict:
     knight1.hp -= max(knight2.power - knight1.protection, 0)
     knight2.hp -= max(knight1.power - knight2.protection, 0)
 
