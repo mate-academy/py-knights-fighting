b415ebf0246b3620f145648cec4f7fada2ca1b42
AndreiVed <catblues87@gmail.com>
2024-05-30 13:15:26 +0300

    solution

--- app/character/duel.py
+++ app/character/duel.py
@@ -1,4 +1,4 @@
-def battle(knight1: object, knight2: object) -> dict:
+def duel(knight1: object, knight2: object) -> dict:
     knight1.hp -= max(knight2.power - knight1.protection, 0)
     knight2.hp -= max(knight1.power - knight2.protection, 0)
 
--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 from app.character.knight_dict import KNIGHTS
 from app.character.knight import Knight
-from app.character.battle import battle
+from app.character.duel import duel
 
 
-def main(knights: dict[dict]) -> dict:
+def battle(knights: dict[dict]) -> dict:
     lancelot, arthur, mordred, red_knight = Knight.knight_preparation(knights)
-    result1 = battle(lancelot, mordred)
-    result2 = battle(arthur, red_knight)
+    result1 = duel(lancelot, mordred)
+    result2 = duel(arthur, red_knight)
     return {**result1, **result2}
 
 
-print(main(KNIGHTS))
+print(battle(KNIGHTS))
