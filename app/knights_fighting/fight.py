cf9be69ffee73532814c447284ae3a9a831bf236
timur <stukantimur811@gmail.com>
2024-06-07 08:32:16 +0300

    changed knight class and all_fights function

--- app/knights_fighting/fight.py
+++ app/knights_fighting/fight.py
@@ -1,18 +1,19 @@
 from app.knights_fighting.knight import Knight
 
 
-def all_fights(knights: dict) -> tuple:
-    knight1, knight2 = Knight(knights["lancelot"]), Knight(knights["mordred"])
-    knight1.modify_knight()
-    knight2.modify_knight()
-    fight(knight1, knight2)
-    print(knight1.get_hp(), knight1.get_power(), knight1.get_protection())
-    print(knight2.get_hp(), knight2.get_power(), knight2.get_protection())
-    knight3, knight4 = Knight(knights["arthur"]), Knight(knights["red_knight"])
-    knight3.modify_knight()
-    knight4.modify_knight()
-    fight(knight3, knight4)
-    return knight1, knight2, knight3, knight4
+def all_fights(knights: dict) -> list:
+    list_of_knights = [
+        Knight(knights["lancelot"]),
+        Knight(knights["mordred"]),
+        Knight(knights["arthur"]),
+        Knight(knights["red_knight"])
+    ]
+    for knight_fight in range(0, 4, 2):
+        list_of_knights[knight_fight].modify_knight()
+        list_of_knights[knight_fight + 1].modify_knight()
+        fight(list_of_knights[knight_fight], list_of_knights[knight_fight + 1])
+
+    return list_of_knights
 
 
 def fight(fighter1: Knight, fighter2: Knight) -> tuple:
@@ -31,8 +32,8 @@ def check_for_defeat(knight: Knight) -> None:
 def get_result_of_all_fights(knights: dict) -> dict:
     all_knights = all_fights(knights)
     return {
-        "Lancelot": all_knights[0].get_hp(),
-        "Arthur": all_knights[2].get_hp(),
-        "Mordred": all_knights[1].get_hp(),
-        "Red Knight": all_knights[3].get_hp()
+        "Lancelot": all_knights[0].hp,
+        "Arthur": all_knights[2].hp,
+        "Mordred": all_knights[1].hp,
+        "Red Knight": all_knights[3].hp
     }
