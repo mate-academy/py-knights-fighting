843d43df10447366f6444afba19ba7c0d308ef36
Maksym Rozdoba <rozdobamax@gmail.com>
2024-06-20 13:06:39 +0300

    Add solution

---
+++ app/battle.py
@@ -0,0 +1,10 @@
+from app.knights_processing.knights_making import Knight
+
+
+def kombat(knight1: Knight, knight2: Knight) -> None:
+    knight1.hp -= knight2.power - knight1.protection
+    knight2.hp -= knight1.power - knight2.protection
+    if knight1.hp <= 0:
+        knight1.hp = 0
+    if knight2.hp <= 0:
+        knight2.hp = 0
