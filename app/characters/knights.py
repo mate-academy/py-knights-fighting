61e1d5b4d2c11b4d3216ba146d86233b0a1f6082
PythonZem <114096673+PythonZem@users.noreply.github.com>
2024-06-21 18:33:13 +0300

    add class knight

---
+++ app/characters/knights.py
@@ -0,0 +1,16 @@
+class Knight:
+    def __init__(
+            self,
+            name: str,
+            power: int,
+            hp: int,
+            armour: list,
+            weapon: list,
+            potion: list,
+    ):
+        self.name = name
+        self.power = power
+        self.hp = hp
+        self.armour = armour
+        self.weapon = weapon
+        self.potion = potion
