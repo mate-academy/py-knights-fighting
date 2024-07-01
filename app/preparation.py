c9955e049a86b37d0811d903cf207ac56650bb65
bythewaters <bythewaters800@gmail.com>
2024-07-01 15:26:12 +0200

    Oooh, i got it:)

--- app/preparation.py
+++ app/preparation.py
@@ -6,42 +6,15 @@ class Preparation:
         self.knight = knight
 
     def full_preparation(self) -> None:
-        setattr(
-            self.knight, "power",
-            getattr(self.knight, "power")
-            + self.knight.weapon["power"]
-        )
+        self.knight.power += self.knight.weapon["power"]
 
         for armour in self.knight.armour:
-            setattr(
-                self.knight, "protection",
-                getattr(self.knight, "protection")
-                + armour["protection"]
-            )
+            self.knight.protection += armour["protection"]
 
         if self.knight.potion is not None:
             for name, boost in self.knight.potion["effect"].items():
-                if name == "protection":
-                    setattr(
-                        self.knight, "protection",
-                        getattr(self.knight, "protection")
-                        + boost
-                    )
-                if name == "power":
-                    setattr(
-                        self.knight, "power",
-                        getattr(self.knight, "power")
-                        + boost
-                    )
-                if name == "armour":
-                    setattr(
-                        self.knight, "armour",
-                        getattr(self.knight, "armour")
-                        + boost
-                    )
-                if name == "hp":
-                    setattr(
-                        self.knight, "hp",
-                        getattr(self.knight, "hp")
-                        + boost
-                    )
+                setattr(
+                    self.knight, name,
+                    getattr(self.knight, name)
+                    + boost
+                )
