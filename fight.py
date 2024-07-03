ad981b3dc398cc7ea268170a94bab54f52533939
Artem Yatsuta <yatsutaproject@gmail.com>
2024-07-03 21:40:18 +0200

    fix duplication in fight.py

--- fight.py
+++ fight.py
@@ -1,10 +1,11 @@
 # Test your might knight!!!
-def fight(knight1, knight2):
-    knight1["hp"] -= max(0, knight2["power"] - knight1["protection"])
-    knight2["hp"] -= max(0, knight1["power"] - knight2["protection"])
+def apply_damage(attacker, defender):
+    defender["hp"] -= max(0, attacker["power"] - defender["protection"])
+    # ensure that hp isn't below 0
+    if defender["hp"] < 0:
+        defender["hp"] = 0
 
-    # Ensure HP does not go below 0
-    if knight1["hp"] <= 0:
-        knight1["hp"] = 0
-    if knight2["hp"] <= 0:
-        knight2["hp"] = 0
+
+def fight(knight1, knight2):
+    apply_damage(knight2, knight1)
+    apply_damage(knight1, knight2)
