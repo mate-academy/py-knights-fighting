9f45e4c7e873893303513b2e5c13e8ef6e6ae5c4
Artem Shakun <artem.shakun.91@gmail.com>
2024-07-04 19:38:30 +0300

    solutions kinghts fighting

---
+++ app/battle.py
@@ -0,0 +1,21 @@
+from app.knight import Knight
+
+
+def battle(knights_config: dict) -> dict:
+    lancelot = Knight(**knights_config["lancelot"])
+    arthur = Knight(**knights_config["arthur"])
+    mordred = Knight(**knights_config["mordred"])
+    red_knight = Knight(**knights_config["red_knight"])
+
+    lancelot.take_damage(mordred.power)
+    mordred.take_damage(lancelot.power)
+
+    arthur.take_damage(red_knight.power)
+    red_knight.take_damage(arthur.power)
+
+    return {
+        lancelot.name: lancelot.hp,
+        arthur.name: arthur.hp,
+        mordred.name: mordred.hp,
+        red_knight.name: red_knight.hp,
+    }
