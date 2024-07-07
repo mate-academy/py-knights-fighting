66d0f045127c9fafa1e4e524e5f534c6ae393f0d
Vadym Vyshnevskyi <wallian169@gmail.com>
2024-07-07 16:44:20 +0300

    Added class Knight with appropriate сдmethods

--- app/main.py
+++ app/main.py
@@ -1,31 +1,28 @@
-from app.battle_preparations import use_power_ups
+from app.knight import Knight
 from app.knights_config import KNIGHTS
 
 
 def battle(knights_config: dict) -> dict:
-    for knight in knights_config.values():
-        use_power_ups(knight)
+    knights = {
+        name: Knight.from_dict(data)
+        for name, data in knights_config.items()
+    }
+    for knight in knights.values():
+        knight.use_power_ups()
 
-    lancelot = knights_config["lancelot"]
-    arthur = knights_config["arthur"]
-    mordred = knights_config["mordred"]
-    red_knight = knights_config["red_knight"]
+    lancelot = knights["lancelot"]
+    arthur = knights["arthur"]
+    mordred = knights["mordred"]
+    red_knight = knights["red_knight"]
 
-    lancelot["hp"] -= mordred["power"] - lancelot["protection"]
-    mordred["hp"] -= lancelot["power"] - mordred["protection"]
-    arthur["hp"] -= red_knight["power"] - arthur["protection"]
-    red_knight["hp"] -= arthur["power"] - red_knight["protection"]
-
-    lancelot["hp"] = max(0, lancelot["hp"])
-    mordred["hp"] = max(0, mordred["hp"])
-    arthur["hp"] = max(0, arthur["hp"])
-    red_knight["hp"] = max(0, red_knight["hp"])
+    lancelot.fight(mordred)
+    arthur.fight(red_knight)
 
     return {
-        lancelot["name"]: lancelot["hp"],
-        arthur["name"]: arthur["hp"],
-        mordred["name"]: mordred["hp"],
-        red_knight["name"]: red_knight["hp"],
+        lancelot.name: lancelot.hp,
+        arthur.name: arthur.hp,
+        mordred.name: mordred.hp,
+        red_knight.name: red_knight.hp,
     }
 
 
