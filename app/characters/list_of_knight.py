431770858402408615ec74af5f7d0e80d7ebc086
Ihor Utkin <igorutkin2002@gmail.com>
2024-07-26 17:08:27 +0200

    Resolve

--- app/characters/list_of_knight.py
+++
@@ -1,8 +0,0 @@
-from app.characters.lancelot import lancelot
-from app.characters.arthur import arthur
-from app.characters.mordred import mordred
-from app.characters.red_knight import red_knight
-
-list_of_knight = [
-    lancelot, arthur, mordred, red_knight
-]
--- app/main.py
+++ app/main.py
@@ -1,21 +1,14 @@
 from app.characters.knights import KNIGHTS
 from app.characters.knight import Knight
 
-from app.characters.lancelot import lancelot
-from app.characters.arthur import arthur
-from app.characters.mordred import mordred
-from app.characters.red_knight import red_knight
 
-from app.characters.list_of_knight import list_of_knight
-
-
-def battle(knights_config) -> dict:
+def battle(knights_config: dict) -> dict:
     # BATTLE PREPARATIONS:
     knights_list = []
 
     for key, value in knights_config.items():
         knight = Knight(
-            name=key,
+            name=value["name"],
             power=value["power"],
             hp=value["hp"],
             armour=value["armour"],
@@ -24,39 +17,38 @@ def battle(knights_config) -> dict:
         )
         knight.get_ready()
         knights_list.append(knight)
-    print(knights_list[0].power)
 
     # -------------------------------------------------------------------------------
     # BATTLE:
 
     # 1 Lancelot vs Mordred:
-    list_of_knight[0].hp -= list_of_knight[2].power - list_of_knight[0].protection
-    list_of_knight[2].hp -= list_of_knight[0].power - list_of_knight[2].protection
+    knights_list[0].hp -= knights_list[2].power - knights_list[0].protection
+    knights_list[2].hp -= knights_list[0].power - knights_list[2].protection
 
     # check if someone fell in battle
-    if list_of_knight[0].hp <= 0:
-        list_of_knight[0].hp = 0
+    if knights_list[0].hp <= 0:
+        knights_list[0].hp = 0
 
-    if list_of_knight[2].hp <= 0:
-        list_of_knight[2].hp = 0
+    if knights_list[2].hp <= 0:
+        knights_list[2].hp = 0
 
     # 2 Arthur vs Red Knight:
-    list_of_knight[1].hp -= list_of_knight[3].power - list_of_knight[1].protection
-    list_of_knight[3].hp -= list_of_knight[1].power - list_of_knight[3].protection
+    knights_list[1].hp -= knights_list[3].power - knights_list[1].protection
+    knights_list[3].hp -= knights_list[1].power - knights_list[3].protection
 
     # check if someone fell in battle
-    if list_of_knight[1].hp <= 0:
-        list_of_knight[1].hp = 0
+    if knights_list[1].hp <= 0:
+        knights_list[1].hp = 0
 
-    if list_of_knight[3].hp <= 0:
-        list_of_knight[3].hp = 0
+    if knights_list[3].hp <= 0:
+        knights_list[3].hp = 0
 
     # Return battle results:
     return {
-        list_of_knight[0].name: list_of_knight[0].hp,
-        list_of_knight[1].name: list_of_knight[1].hp,
-        list_of_knight[2].name: list_of_knight[2].hp,
-        list_of_knight[3].name: list_of_knight[3].hp,
+        knights_list[0].name: knights_list[0].hp,
+        knights_list[1].name: knights_list[1].hp,
+        knights_list[2].name: knights_list[2].hp,
+        knights_list[3].name: knights_list[3].hp,
     }
 
 
