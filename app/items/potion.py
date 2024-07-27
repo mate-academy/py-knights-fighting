9c475dbe9177868e83d959f310d86ddec99facd0
Bogdan <bogdan.rylov422@gmail.com>
2024-07-27 17:28:48 +0300

    Solution

---
+++ app/items/potion.py
@@ -0,0 +1,18 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from app.knight import Knight
+
+
+class Potion:
+    def __init__(self, name: str, effect: dict) -> None:
+        self.name = name
+        self.effect = effect
+
+    def apply(self, knight: "Knight") -> None:
+        if "power" in self.effect:
+            knight.power += self.effect["power"]
+        if "hp" in self.effect:
+            knight.hp += self.effect["hp"]
+        if "protection" in self.effect:
+            knight.protection += self.effect["protection"]
