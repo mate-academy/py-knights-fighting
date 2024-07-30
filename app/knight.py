0f7e114f1fa2084802acb070820392fe0ef543cc
Stepan <oleksiukstepann@gmail.com>
2024-07-30 18:24:20 +0300

    changes added

--- app/knight.py
+++ app/knight.py
@@ -11,53 +11,53 @@ class Knight:
             name: str,
             power: int,
             hp: int,
-            armours: list,
-            weapon: dict,
-            potion: dict
+            **kwargs
     ) -> None:
         self.name = name
         self.power = power
         self.hp = hp
-        self.armour = self.set_armours(armours)
-        self.weapon = Weapon(weapon["name"], weapon["power"])
-        self.potion = self.set_potion(potion)
-        self.protection = 0
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     @staticmethod
-    def set_armours(armours: list) -> list:
-        all_armours = []
-
-        if armours:
-            for armour in armours:
-                all_armours.append(
-                    Armour(armour["part"], armour["protection"])
-                )
-
-        return all_armours
+    def set_armours(armours: list) -> list[Armour] | None:
+        if not armours:
+            return None
+        return [
+            Armour(armour["part"], armour["protection"])
+            for armour in armours
+        ]
 
     @staticmethod
-    def set_potion(potion: dict) -> Potion:
-        if potion:
-            return Potion(potion["name"], potion["effect"])
-        return None
+    def set_weapon(weapon: dict) -> Weapon | None:
+        if not weapon:
+            return None
+        return Weapon(weapon["name"], weapon["power"])
+
+    @staticmethod
+    def set_potion(potion: dict) -> Potion | None:
+        if not potion:
+            return None
+        return Potion(potion["name"], potion["effect"])
 
     def use_potion(self) -> None:
-        if self.potion:
-            for key, value in self.potion.effect.items():
-                if key == "power":
-                    self.power += value
-                elif key == "hp":
-                    self.hp += value
-                elif key == "protection":
-                    self.protection += value
+        if not self.potion:
+            return
+        for key, value in self.potion.effect.items():
+            if key == "power":
+                self.power += value
+            elif key == "hp":
+                self.hp += value
+            elif key == "protection":
+                self.protection += value
 
-    def __repr__(self) -> str:
+    def __str__(self) -> str:
         return (
-            f"Knight: {self.name}; "
-            f"Power: {self.power}; "
-            f"HP: {self.hp}; "
-            f"Armour: {self.armour}; "
-            f"Weapon: {self.weapon}; "
-            f"Potion: {self.potion}; "
+            f"Knight: {self.name} "
+            f"Power: {self.power} "
+            f"HP: {self.hp} "
+            f"Armour: {self.armour} "
+            f"Weapon: {self.weapon.__dict__} "
+            f"Potion: {self.potion} "
             f"Protection: {self.protection}"
         )
--- app/main.py
+++ app/main.py
@@ -1,75 +1,167 @@
 from app.knight import Knight
 
 
-def battle(knights_config: dict) -> str:
-    knights = _create_knights(knights_config)
-    battle_preparations(knights)
-    return start_battle(knights)
+def battle(knights_config: dict) -> dict[str, int]:
+    knights = create_knights(knights_config)
+    for knight in knights:
+        battle_preparations(knight)
+    return start_battle(*knights)
 
 
-def battle_preparations(knights: list) -> None:
-    _apply_armour(knights)
-    _apply_weapon(knights)
-    _use_potion(knights)
+def battle_preparations(knight: Knight) -> None:
+    apply_armour(knight)
+    apply_weapon(knight)
+    use_potion(knight)
 
 
-def start_battle(knights: list) -> None:
+def start_battle(
+        lancelot: Knight,
+        arthur: Knight,
+        mordred: Knight,
+        red_knight: Knight
+) -> dict[str, int]:
     # Lancelot vs Mordred
-    _duel_battle(knights[0], knights[2])
-    _duel_battle(knights[2], knights[0])
+    duel_battle(lancelot, mordred)
+    duel_battle(mordred, lancelot)
 
     # Arthur vs Red Knight
-    _duel_battle(knights[1], knights[3])
-    _duel_battle(knights[3], knights[1])
+    duel_battle(arthur, red_knight)
+    duel_battle(red_knight, arthur)
 
     return {
         knight.name: knight.hp
-        for knight in knights
+        for knight in [lancelot, arthur, mordred, red_knight]
     }
 
 
-def _create_knights(knights_config: dict) -> list:
-    knights = []
-
-    for stats in knights_config.values():
-        knight = Knight(
+def create_knights(knights_config: dict) -> list:
+    return [
+        Knight(
             stats["name"],
             stats["power"],
             stats["hp"],
-            stats["armour"],
-            stats["weapon"],
-            stats["potion"]
+            armour=Knight.set_armours(stats["armour"]),
+            weapon=Knight.set_weapon(stats["weapon"]),
+            potion=Knight.set_potion(stats["potion"])
         )
-        knights.append(knight)
+        for stats in knights_config.values()
+    ]
 
-    return knights
 
-
-def _duel_battle(knight_1: Knight, knight_2: Knight) -> None:
+def duel_battle(knight_1: Knight, knight_2: Knight) -> None:
     knight_1.hp = knight_1.hp - (knight_2.power - knight_1.protection)
-    _check_hp([knight_1, knight_2])
+    check_hp([knight_1, knight_2])
 
 
-def _check_hp(knights: list) -> None:
+def check_hp(knights: list) -> None:
     for knight in knights:
         if knight.hp <= 0:
             knight.hp = 0
 
 
-def _apply_armour(knights: list) -> None:
-    for knight in knights:
-        protection = 0
-        if knight.armour:
-            for armour in knight.armour:
-                protection += armour.protection
-            knight.protection = protection
+def apply_armour(knight: Knight) -> None:
+    protection = 0
+    if not knight.armour:
+        knight.protection = protection
+        return
+    for armour in knight.armour:
+        protection += armour.protection
+    knight.protection = protection
 
 
-def _apply_weapon(knights: list) -> None:
-    for knight in knights:
-        knight.power += knight.weapon.power
+def apply_weapon(knight: Knight) -> None:
+    knight.power += knight.weapon.power
 
 
-def _use_potion(knights: list) -> None:
-    for knight in knights:
-        knight.use_potion()
+def use_potion(knight: Knight) -> None:
+    knight.use_potion()
+
+
+fords = {
+    "lancelot": {
+        "name": "Lancelot",
+        "power": 35,
+        "hp": 100,
+        "armour": [],
+        "weapon": {
+            "name": "Metal Sword",
+            "power": 50,
+        },
+        "potion": None,
+    },
+    "arthur": {
+        "name": "Arthur",
+        "power": 45,
+        "hp": 75,
+        "armour": [
+            {
+                "part": "helmet",
+                "protection": 15,
+            },
+            {
+                "part": "breastplate",
+                "protection": 20,
+            },
+            {
+                "part": "boots",
+                "protection": 10,
+            },
+        ],
+        "weapon": {
+            "name": "Two-handed Sword",
+            "power": 55,
+        },
+        "potion": None,
+    },
+    "mordred": {
+        "name": "Mordred",
+        "power": 30,
+        "hp": 90,
+        "armour": [
+            {
+                "part": "breastplate",
+                "protection": 15,
+            },
+            {
+                "part": "boots",
+                "protection": 10,
+            },
+        ],
+        "weapon": {
+            "name": "Poisoned Sword",
+            "power": 60,
+        },
+        "potion": {
+            "name": "Berserk",
+            "effect": {
+                "power": +15,
+                "hp": -5,
+                "protection": +10,
+            },
+        },
+    },
+    "red_knight": {
+        "name": "Red Knight",
+        "power": 40,
+        "hp": 70,
+        "armour": [
+            {
+                "part": "breastplate",
+                "protection": 25,
+            }
+        ],
+        "weapon": {"name": "Sword", "power": 45},
+        "potion": {
+            "name": "Blessing",
+            "effect": {
+                "hp": +10,
+                "power": +5,
+            },
+        },
+    },
+}
+kniggt = create_knights(fords)
+for knigh in kniggt:
+    battle_preparations(knigh)
+    print(knigh)
+print(battle(fords))
