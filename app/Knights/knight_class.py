c894cdf10dfba2aaf5450ece19442460a69ad288
Serhii-Chubur <serhichubur15@gmail.com>
2024-08-10 16:36:27 +0300

    rewritten

--- app/Knights/knight_class.py
+++ app/Knights/knight_class.py
@@ -5,14 +5,16 @@ class Knight:
     def __init__(self, parameters: dict) -> None:
         self.name = parameters["name"]
         self.hp = parameters["hp"]
-        self.protection = sum([armour["protection"]
-                               for armour in parameters["armour"]])
+        self.protection = sum(
+            armour["protection"] for armour in parameters["armour"]
+        )
         self.power = parameters["power"] + parameters["weapon"]["power"]
 
         if parameters["potion"] is not None:
             self.power += parameters["potion"]["effect"].get("power", 0)
-            self.protection += (parameters["potion"]["effect"]
-                                .get("protection", 0))
+            self.protection += (
+                parameters["potion"]["effect"].get("protection", 0)
+            )
             self.hp += parameters["potion"]["effect"].get("hp", 0)
 
     def knights_buttle(self, other: Knight) -> None:
