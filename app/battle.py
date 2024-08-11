9d6b76736bcd3e1dcd94f567dfbaf57e00844ae6
Ruslan Toister <rtoister.cv@gmail.com>
2024-08-11 23:46:14 +0300

    'Solution2'

---
+++ app/battle.py
@@ -0,0 +1,18 @@
+from typing import Dict, Any
+from app.classes import Knight
+
+
+def battle(knights_config: Dict[str, Any]) -> Dict[str, int]:
+    knights = {
+        name: Knight(**config) for name, config in knights_config.items()
+    }
+
+    knights["lancelot"].receive_damage(knights["mordred"].power)
+    knights["mordred"].receive_damage(knights["lancelot"].power)
+
+    knights["arthur"].receive_damage(knights["red_knight"].power)
+    knights["red_knight"].receive_damage(knights["arthur"].power)
+
+    results = {knight.name: knight.hp for knight in knights.values()}
+
+    return results
