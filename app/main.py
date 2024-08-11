369d8368f48639300e6e6f331c63aed2b9992356
Ruslan Toister <rtoister.cv@gmail.com>
2024-08-11 23:52:05 +0300

    'Solution3'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,21 @@
 from app.knights_config import KNIGHTS
-from app.battle import battle
+from app.realisation import (
+    knight_battle_preparations,
+    update_health_after_battle
+)
 
-if __name__ == "__main__":
-    results = battle(KNIGHTS)
-    print(results)
+
+def battle(knights: dict) -> dict:
+
+    for knight in knights:
+        knight_battle_preparations(knights[knight])
+
+    update_health_after_battle(knights["lancelot"], knights["mordred"])
+    update_health_after_battle(knights["arthur"], knights["red_knight"])
+
+    return {
+        knights[knight]["name"]: knights[knight]["hp"] for knight in knights
+    }
+
+
+print(battle(KNIGHTS))
