c08731810044cb02bc9ae1e5732bf3c253c93a97
Ruslan Toister <rtoister.cv@gmail.com>
2024-08-12 20:01:49 +0300

    'Fixing_loops'

--- app/battle/battle_realisation.py
+++ app/battle/battle_realisation.py
@@ -2,65 +2,41 @@ from app.classes import Knight
 
 
 def battle(knights_config: dict) -> dict:
+    knights = {}
 
-    lancelot = Knight(
-        "Lancelot",
-        knights_config["lancelot"]["power"],
-        knights_config["lancelot"]["hp"],
-        knights_config["lancelot"]["armour"],
-        knights_config["lancelot"]["weapon"],
-        knights_config["lancelot"]["potion"]
-    )
+    for i in knights_config:
+        knights[i] = Knight(
+            knights_config[i]["name"],
+            knights_config[i]["power"],
+            knights_config[i]["hp"],
+            knights_config[i]["armour"],
+            knights_config[i]["weapon"],
+            knights_config[i]["potion"]
+        )
 
-    arthur = Knight(
-        "Arthur",
-        knights_config["arthur"]["power"],
-        knights_config["arthur"]["hp"],
-        knights_config["arthur"]["armour"],
-        knights_config["arthur"]["weapon"],
-        knights_config["arthur"]["potion"]
-    )
+    battles = [
+        ("lancelot", "mordred"),
+        ("arthur", "red_knight")
+    ]
 
-    mordred = Knight(
-        "Mordred",
-        knights_config["mordred"]["power"],
-        knights_config["mordred"]["hp"],
-        knights_config["mordred"]["armour"],
-        knights_config["mordred"]["weapon"],
-        knights_config["mordred"]["potion"]
-    )
+    for first_fiter, second_fiter in battles:
+        power_second = knights[second_fiter].final_power
+        protection_first = knights[first_fiter].final_protection
+        damage_to_first = max(
+            0,
+            power_second - protection_first
+        )
+        knights[first_fiter].final_hp -= damage_to_first
 
-    red_knight = Knight(
-        "Red Knight",
-        knights_config["red_knight"]["power"],
-        knights_config["red_knight"]["hp"],
-        knights_config["red_knight"]["armour"],
-        knights_config["red_knight"]["weapon"],
-        knights_config["red_knight"]["potion"]
-    )
+        power_first = knights[first_fiter].final_power
+        protection_second = knights[second_fiter].final_protection
+        damage_to_second = max(
+            0,
+            power_first - protection_second
+        )
+        knights[second_fiter].final_hp -= damage_to_second
 
-    lancelot.final_hp -= max(
-        0, mordred.final_power - lancelot.final_protection
-    )
-    mordred.final_hp -= max(
-        0, lancelot.final_power - mordred.final_protection
-    )
+    for knight in knights.values():
+        knight.final_hp = max(0, knight.final_hp)
 
-    arthur.final_hp -= max(
-        0, red_knight.final_power - arthur.final_protection
-    )
-    red_knight.final_hp -= max(
-        0, arthur.final_power - red_knight.final_protection
-    )
-
-    lancelot.final_hp = max(0, lancelot.final_hp)
-    mordred.final_hp = max(0, mordred.final_hp)
-    arthur.final_hp = max(0, arthur.final_hp)
-    red_knight.final_hp = max(0, red_knight.final_hp)
-
-    return {
-        lancelot.name: lancelot.final_hp,
-        arthur.name: arthur.final_hp,
-        mordred.name: mordred.final_hp,
-        red_knight.name: red_knight.final_hp,
-    }
+    return {knight.name: knight.final_hp for knight in knights.values()}
