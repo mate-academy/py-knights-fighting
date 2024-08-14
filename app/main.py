64bca08a481f64a86be64ae42d471954a22d6ecc
Ivan Havrylyshyn <jyjuk1@gmail.com>
2024-08-14 10:01:39 +0300

    Solution3

--- app/main.py
+++ app/main.py
@@ -1,45 +1,7 @@
 from typing import Dict
 from app.knight import Knight
 from app.battle import Battle
-
-KNIGHTS = {
-    "lancelot": {
-        "name": "Lancelot",
-        "power": 50,
-        "hp": 80,
-        "armour": [{"part": "helmet", "protection": 10}],
-        "weapon": {"name": "Sword", "power": 30},
-        "potion": None,
-    },
-    "arthur": {
-        "name": "Arthur",
-        "power": 45,
-        "hp": 75,
-        "armour": [
-            {"part": "helmet", "protection": 15},
-            {"part": "breastplate", "protection": 20},
-            {"part": "boots", "protection": 5}
-        ],
-        "weapon": {"name": "Sword", "power": 25},
-        "potion": {"name": "Blessing", "effect": {"hp": 20, "power": 10}},
-    },
-    "mordred": {
-        "name": "Mordred",
-        "power": 55,
-        "hp": 85,
-        "armour": [{"part": "shield", "protection": 30}],
-        "weapon": {"name": "Axe", "power": 35},
-        "potion": None,
-    },
-    "red_knight": {
-        "name": "Red Knight",
-        "power": 40,
-        "hp": 70,
-        "armour": [{"part": "breastplate", "protection": 25}],
-        "weapon": {"name": "Sword", "power": 45},
-        "potion": {"name": "Blessing", "effect": {"hp": 10, "power": 5}},
-    },
-}
+from app.config import KNIGHTS
 
 
 def battle(knights_config: Dict[str, Dict]) -> Dict[str, int]:
