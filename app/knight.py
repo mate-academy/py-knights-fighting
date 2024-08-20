6935bdccbceb6b4f6f1bdc17955f2ae35f7814ca
Arthur <avers.target@gmail.com>
2024-08-20 17:31:53 +0200

    reformatted file

--- app/knight.py
+++ app/knight.py
@@ -20,8 +20,29 @@ class Knight:
                                get("protection", 0)
                                for item in self.armour)
         self.power += self.weapon.get("power", 0)
+
         if self.potion:
             buff = self.potion.get("effect", {})
-            self.power += buff.get("power", 0)
-            self.hp += buff.get("hp", 0)
-            self.protection += buff.get("protection", 0)
+            for attr in ["power", "hp", "protection"]:
+                if attr in buff:
+                    current_value = getattr(self, attr)
+                    setattr(self, attr, current_value + buff[attr])
+
+    @classmethod
+    def battle_preparations(cls, knight_dict: dict) -> list:
+        knights = [cls(**knight_dict[knight]) for knight in knight_dict]
+
+        for knight in knights:
+            knight.get_ready_to_battle()
+
+        return knights
+
+    @staticmethod
+    def result(first: "Knight", second: "Knight") -> None:
+        first.hp -= second.power - first.protection
+        second.hp -= first.power - second.protection
+
+        if first.hp < 0:
+            first.hp = 0
+        if second.hp < 0:
+            second.hp = 0
--- app/main.py
+++ app/main.py
@@ -1,12 +1,20 @@
-from app.knights_config import knights
-from app.battle_preparations import battle_preparations
-from app.battle_results import result
+from app.knight import Knight
+from app.knight_config import knights
 
 
 def battle(dict_of_knight: dict) -> dict:
-    lancelot, arthur, mordred, red_knight = battle_preparations(dict_of_knight)
-    result(lancelot, mordred)
-    result(arthur, red_knight)
+    knights = Knight.battle_preparations(dict_of_knight)
+
+    lancelot = next(knight for knight in knights if knight.name == "Lancelot")
+    arthur = next(knight for knight in knights if knight.name == "Arthur")
+    mordred = next(knight for knight in knights if knight.name == "Mordred")
+    red_knight = next(knight
+                      for knight
+                      in knights
+                      if knight.name == "Red Knight")
+
+    Knight.result(lancelot, mordred)
+    Knight.result(arthur, red_knight)
 
     return {
         lancelot.name: lancelot.hp,
