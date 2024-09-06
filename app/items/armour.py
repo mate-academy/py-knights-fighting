841457b02537fcd5215b08f8eb26a5edb06fe4df
Denys Podobrii <denys.podobrii@gmail.com>
2024-09-06 03:56:51 +0300

    add doc strings to app.items.armour module

--- app/items/armour.py
+++ app/items/armour.py
@@ -1,4 +1,24 @@
 class Armour:
+    """
+    Represents an armour piece worn by a knight. Armour provides protection
+    during battles, reducing the damage taken by the knight.
+
+    Attributes:
+        part (str): The part of the knight's body that the armour protects
+                    (e.g., breastplate, helmet).
+        protection (int): The protection value of the armour piece.
+    """
+
     def __init__(self, part: str, protection: int) -> None:
+        """
+        Initializes an armour piece with the part of the body
+        it protects and the amount of protection it provides.
+
+        Args:
+            part (str): The name of the armour piece
+                        (e.g., breastplate, helmet).
+            protection (int):
+                The protection value that reduces damage in battle.
+        """
         self.part = part
         self.protection = protection
