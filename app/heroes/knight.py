4873e8a8fe09d1ca6ab219a84a6b8b02eca482aa
Denys Podobrii <denys.podobrii@gmail.com>
2024-09-06 03:27:14 +0300

    refactor app.heroes.knight module

--- app/heroes/knight.py
+++ app/heroes/knight.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from app.items.armour import Armour
 from app.items.weapon import Weapon
 from app.items.potion import Potion
@@ -9,15 +11,26 @@ class Knight:
         self.hp = hp
         self.power = power
         self.protection = 0
+        self.armour: list[Armour] = []
+        self.weapon: Weapon = None
+        self.potion: Potion = None
 
     def set_protection(self, armours: list[Armour]) -> None:
-        if armours:
-            self.protection += sum(armour.protection for armour in armours)
+        self.protection += sum(armour.protection for armour in armours)
 
     def increase_power(self, weapon: Weapon) -> None:
         self.power += weapon.power
 
     def set_effect(self, potion: Potion) -> None:
-        self.hp += potion.effect.get("hp", 0)
-        self.power += potion.effect.get("power", 0)
-        self.protection += potion.effect.get("protection", 0)
+        if potion:
+            self.hp += potion.effect.get("hp", 0)
+            self.power += potion.effect.get("power", 0)
+            self.protection += potion.effect.get("protection", 0)
+
+    def fight(self, opponent: Knight) -> None:
+        self.hp = max(self.hp - (opponent.power - self.protection), 0)
+        opponent.hp = max(opponent.hp - (self.power - opponent.protection), 0)
+
+    def check_hp(self) -> None:
+        if self.hp <= 0:
+            self.hp = 0
