a49291cdfa1c9d8fa7017473b1e2b7e708c9a550
vavarvariuk <vasya.varvariuk.27@gmail.com>
2024-09-30 12:41:24 +0300

    Solution knights-flighting

--- app/finish.py
+++ app/finish.py
@@ -1,18 +1,11 @@
 from app.potion import potions
+from app.fight import fights
 
 
 def fight(fighter: dict) -> dict:
     potions(fighter)
-    fighter["lancelot"]["hp"] -= \
-        (fighter["mordred"]["power"] - fighter["lancelot"]["protection"])
-    fighter["mordred"]["hp"] -= \
-        (fighter["lancelot"]["power"] - fighter["mordred"]["protection"])
-
-    fighter["arthur"]["hp"] -=\
-        (fighter["red_knight"]["power"] - fighter["arthur"]["protection"])
-
-    fighter["red_knight"]["hp"] -= \
-        (fighter["arthur"]["power"] - fighter["red_knight"]["protection"])
+    fights(fighter["lancelot"], fighter["mordred"])
+    fights(fighter["red_knight"], fighter["arthur"])
     for person in fighter:
         if fighter[person]["hp"] <= 0:
             fighter[person]["hp"] = 0
