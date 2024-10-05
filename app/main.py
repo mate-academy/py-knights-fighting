6d8d2c4fb3cca36b4ab45b3b98d47b2ff1bb2f6c
Vladyslav Kucherenko <vlados.soc@gmail.com>
2024-10-05 14:25:56 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -12,6 +12,7 @@ def init_knights(knights: dict) -> dict:
 def fight(knight1: Knight, knight2: Knight) -> dict:
     knight1.hp -= knight2.power - knight1.protection
     knight2.hp -= knight1.power - knight2.protection
+
     if knight1.hp <= 0:
         knight1.hp = 0
     if knight2.hp <= 0:
