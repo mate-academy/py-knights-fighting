003df2cfcb578813de48261d4ada99c1554d5dc1
Aleksandr <sasha.syrota15@gmail.com>
2024-10-08 12:21:47 +0300

    solution

--- app/Battle_Preparations/apply_armour.py
+++ app/Battle_Preparations/apply_armour.py
@@ -1,5 +1,5 @@
-def apply_armour(knights: dict) -> None:
-    for stats in knights.values():
+def apply_armour(knight: dict) -> None:
+    for stats in knight.values():
         stats["protection"] = 0
         for armour in stats["armour"]:
             stats["protection"] += armour["protection"]
