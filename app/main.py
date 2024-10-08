9c2e2dbe86467befa31f2fd1c20f69cd32426a64
Fedir <fedor.agureev@gmail.com>
2024-10-08 21:40:24 +0300

    fixes

--- app/main.py
+++ app/main.py
@@ -11,7 +11,6 @@ def battle(knights_config: dict) -> dict:
     knights["mordred"].take_damage(knights["lancelot"])
     knights["arthur"].take_damage(knights["red_knight"])
     knights["red_knight"].take_damage(knights["arthur"])
-
     return {
         knight.name: knight.hp for knight in knights.values()
     }
