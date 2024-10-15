96cbeb8d26a9c7b61267100818ec0fdc5a362788
Radeon <egorka22e40x@gmail.com>
2024-10-15 13:40:13 +0300

    change __init__, __sub__, __isub__, battle

--- app/knight.py
+++ app/knight.py
@@ -4,27 +4,30 @@ from __future__ import annotations
 class Knight:
     def __init__(self, knight: dict) -> None:
         self.knight = knight
-        self.name = knight["name"]
-        self.power = (knight["power"]
-                      + (knight["weapon"].get("power", 0)
-                         if knight["weapon"] else 0)
-                      + (knight["potion"]["effect"].get("power", 0)
-                         if knight["potion"] else 0))
-        self.hp = (knight["hp"]
-                   + (knight["potion"]["effect"].get("hp", 0)
-                      if knight["potion"] else 0))
-        self.protection = ((sum(protec["protection"]
-                                for protec in knight["armour"])
-                            if knight["armour"] else 0)
-                           + (knight["potion"]["effect"].get("protection", 0)
-                              if knight["potion"] else 0))
 
-    def __sub__(self, other: Knight) -> Knight:
+    def preparation(self) -> None:
+        self.name = self.knight["name"]
+        self.power = (self.knight["power"]
+                      + (self.knight["weapon"].get("power", 0)
+                         if self.knight["weapon"] else 0)
+                      + (self.knight["potion"]["effect"].get("power", 0)
+                         if self.knight["potion"] else 0))
+        self.hp = (self.knight["hp"]
+                   + (self.knight["potion"]["effect"].get("hp", 0)
+                      if self.knight["potion"] else 0))
+        self.protection = ((sum(protec["protection"]
+                                for protec in self.knight["armour"])
+                            if self.knight["armour"] else 0)
+                           + (self.knight["potion"]["effect"].get("protection"
+                                                                  , 0)
+                              if self.knight["potion"] else 0))
+
+    def reduce_power(self, other: Knight) -> Knight:
         knight = Knight(self.knight)
         knight.power = self.power - other.protection
         return knight
 
-    def __isub__(self, other: Knight) -> Knight:
+    def fight(self, other: Knight) -> Knight:
         self.hp -= other.power
         self.hp = max(self.hp, 0)
         return self
--- app/main.py
+++ app/main.py
@@ -1,21 +1,26 @@
 from app.data import KNIGHTS
 from app.knight import Knight
 
+battle_table = [
+    ("lancelot", "mordred"),
+    ("arthur", "red_knight")
+]
+
 
 def battle_between(knight1: Knight, knight2: Knight) -> None:
-    knight1 -= knight2 - knight1
-    knight2 -= knight1 - knight2
+    knight1.fight(knight2.reduce_power(knight1))
+    knight2.fight(knight1.reduce_power(knight2))
 
 
 def battle(knights_config: dict) -> dict:
     # BATTLE PREPARATIONS:
     knights = {name: Knight(value) for name, value in knights_config.items()}
+    for knight in knights.values():
+        knight.preparation()
     # -------------------------------------------------------------------------------
     # BATTLE:
-    # 1 Lancelot vs Mordred:
-    battle_between(knights["lancelot"], knights["mordred"])
-    # 2 Arthur vs Red Knight:
-    battle_between(knights["arthur"], knights["red_knight"])
+    for name in battle_table:
+        battle_between(knights[name[0]], knights[name[1]])
     # Return battle results:
     return {value.name: value.hp for value in knights.values()}
 
