afd37d8056fea922ae37eae7dab2dd98a24574a8
Andrii <andrijgordienko67@gmail.com>
2024-10-19 12:04:31 +0300

    Fix

--- app/add_some_abilities/add_potion_abilities.py
+++ app/add_some_abilities/add_potion_abilities.py
@@ -3,7 +3,7 @@ def add_ability_from_potion(knight: dict, ability: str) -> None:
         knight[ability] += knight["potion"]["effect"].get(ability)
 
 
-def check_presence_ability(knight: dict, ability: str):
+def check_presence_ability(knight: dict, ability: str) -> bool:
     if ability in knight["potion"]["effect"]:
         return True
     return False
