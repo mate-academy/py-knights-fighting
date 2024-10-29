33d3c7baed649de6bad01b7c11e886ae46992e20
fanni <fmalevych@zoolatech.com>
2024-10-29 15:32:27 +0200

    Refactioring

---
+++ app/battle/battle.py
@@ -0,0 +1,15 @@
+def knights_battle(knight1: dict, knight2: dict) -> dict:
+    knight1["hp"] -= knight2["power"] - knight1["protection"]
+    knight2["hp"] -= knight1["power"] - knight2["protection"]
+
+    # check if someone fell in battle
+    if knight1["hp"] <= 0:
+        knight1["hp"] = 0
+
+    if knight2["hp"] <= 0:
+        knight2["hp"] = 0
+
+    return {
+        knight1["name"]: knight1["hp"],
+        knight2["name"]: knight2["hp"]
+    }
