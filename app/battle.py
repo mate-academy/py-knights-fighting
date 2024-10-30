bc2059a6ae085cf247bd9724f4122a9fa0f2b86c
kkkkkkkkatya <vasyabarilyuk@gmail.com>
2024-10-30 12:33:54 +0200

    solution 02

--- app/battle.py
+++ app/battle.py
@@ -7,8 +7,13 @@ class Battle:
         self.knight2 = knight2
 
     def conduct_battle(self) -> None:
-        self.knight1.adjust_hp(self.knight2.total_power)
-        self.knight2.adjust_hp(self.knight1.total_power)
+        self.knight1.take_damage(self.knight2.power)
+        self.knight2.take_damage(self.knight1.power)
+
+        if self.knight1.is_defeated():
+            self.knight1.hp = 0
+        if self.knight2.is_defeated():
+            self.knight2.hp = 0
 
     def get_result(self) -> dict:
         return {
--- app/knight.py
+++ app/knight.py
@@ -1,38 +1,31 @@
 class Knight:
     def __init__(self, config: dict) -> None:
         self.name = config["name"]
-        self.base_power = config["power"]
+        self.power = config["power"]
         self.hp = config["hp"]
-        self.armor = config.get("armour", [])
-        self.weapon = config.get("weapon", {})
+        self.armour = config.get("armour", [])
+        self.weapon = config["weapon"]
         self.potion = config.get("potion", None)
-        self.protection = 0
-
-        self.apply_armor()
+        self.protection = self.calculate_protection()
         self.apply_weapon()
         self.apply_potion()
 
-    def apply_armor(self) -> None:
-        self.protection = sum(part["protection"] for part in self.armor)
+    def calculate_protection(self) -> int | float:
+        base_protection = sum(part["protection"] for part in self.armour)
+        return base_protection
 
     def apply_weapon(self) -> None:
-        self.base_power += self.weapon.get("power", 0)
+        self.power += self.weapon["power"]
 
     def apply_potion(self) -> None:
         if self.potion:
             effect = self.potion["effect"]
             self.hp += effect.get("hp", 0)
-            self.base_power += effect.get("power", 0)
+            self.power += effect.get("power", 0)
             self.protection += effect.get("protection", 0)
 
-    @property
-    def total_power(self) -> int | float:
-        return self.base_power
+    def take_damage(self, opponent_power: int | float) -> None:
+        self.hp -= opponent_power - self.protection
 
-    @property
-    def total_protection(self) -> int | float:
-        return self.protection
-
-    def adjust_hp(self, opponent_power: int | float) -> None:
-        damage = max(0, opponent_power - self.total_protection)
-        self.hp = max(0, self.hp - damage)
+    def is_defeated(self) -> bool:
+        return self.hp <= 0
