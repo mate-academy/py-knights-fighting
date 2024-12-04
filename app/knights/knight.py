371be28de447783c9c37587fdb87ee4ac5acd112
Maksym <maximuschampion2002@gmail.com>
2024-12-04 21:19:17 +0200

    knights-fighting

--- app/knights/knight.py
+++ app/knights/knight.py
@@ -27,9 +27,15 @@ class Knight:
         self.apply_weapon()
         self.apply_potion()
 
+    def apply_equipment(self) -> None:
+        self.apply_armour()
+        self.apply_weapon()
+        self.apply_potion()
+
     def apply_armour(self) -> None:
-        for armour_piece in self.armour:
-            self.protection += armour_piece.apply()
+        self.protection = sum(
+            armour_piece.apply() for armour_piece in self.armour
+        )
 
     def apply_weapon(self) -> None:
         self.power += self.weapon.apply()
@@ -37,14 +43,13 @@ class Knight:
     def apply_potion(self) -> None:
         if self.potion:
             effects = self.potion.apply()
-            if "power" in effects:
-                self.power += effects["power"]
-            if "protection" in effects:
-                self.protection += effects["protection"]
-            if "hp" in effects:
-                self.hp += effects["hp"]
+            self.power += effects.get("power", 0)
+            self.protection += effects.get("protection", 0)
+            self.hp += effects.get("hp", 0)
 
     def receive_damage(self, damage: int) -> None:
-        self.hp -= damage
-        if self.hp < 0:
-            self.hp = 0
+        damage_taken = max(0, damage - self.protection)
+        self.hp = max(0, self.hp - damage_taken)
+
+    def is_alive(self) -> bool:
+        return self.hp > 0
