2171b7241f523c208fb981ca4ee15beb27bdc087
Yura Chornyi <chorny.yura@gmail.com>
2025-01-13 22:01:18 +0200

    Solution - knight fighting

---
+++ app/knights/knight.py
@@ -0,0 +1,59 @@
+class Knight:
+
+    def __init__(
+            self,
+            name: str,
+            power: int,
+            hp: int,
+            armour: list,
+            weapon: dict,
+            potion: dict = None
+    ) -> None:
+        self.name = name
+        self.power = power
+        self.hp = hp
+        self.armour = armour
+        self.weapon = weapon
+        self.potion = potion
+        self.protection = 0
+
+    @staticmethod
+    def create_knight(knight: dict) -> "Knight":
+        return Knight(
+            knight["name"],
+            knight["power"],
+            knight["hp"],
+            knight["armour"],
+            knight["weapon"],
+            knight["potion"]
+        )
+
+    def apply_armour(self) -> None:
+        for part in self.armour:
+            self.protection += part["protection"]
+
+    def apply_weapon(self) -> None:
+        self.power += self.weapon["power"]
+
+    def apply_potion(self) -> None:
+        if self.potion is not None:
+            if "power" in self.potion["effect"]:
+                self.power += self.potion["effect"]["power"]
+            if "protection" in self.potion["effect"]:
+                self.protection += self.potion["effect"]["protection"]
+            if "hp" in self.potion["effect"]:
+                self.hp += self.potion["effect"]["hp"]
+
+    def prepare_to_battle(self) -> None:
+        self.apply_armour()
+        self.apply_weapon()
+        self.apply_potion()
+
+    def __str__(self) -> str:
+        return (f"Knight - \"{self.name}\": \n"
+                f"Power - {self.power}\n"
+                f"HP - {self.hp}\n"
+                f"Armour - {", ".join([part["part"]
+                                       for part in self.armour])}\n"
+                f"Weapon - {self.weapon.get("name")}\n"
+                f"Potion - {self.potion.get("name") if self.potion else None}")
