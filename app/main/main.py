c9a41c1e84fbdf468949560fa313c0be342dd51f
Bohdan <khubashvili12@gmail.com>
2025-01-17 22:21:06 +0200

    Done!

---
+++ app/main/main.py
@@ -0,0 +1,118 @@
+from app.main.knight import Knight
+from app.main.battle import Battle
+
+KNIGHTS = {
+    "lancelot": {
+        "name": "Lancelot",
+        "power": 35,
+        "hp": 100,
+        "armour": [],
+        "weapon": {
+            "name": "Metal Sword",
+            "power": 50,
+        },
+        "potion": None,
+    },
+    "arthur": {
+        "name": "Arthur",
+        "power": 45,
+        "hp": 75,
+        "armour": [
+            {
+                "part": "helmet",
+                "protection": 15,
+            },
+            {
+                "part": "breastplate",
+                "protection": 20,
+            },
+            {
+                "part": "boots",
+                "protection": 10,
+            }
+        ],
+        "weapon": {
+            "name": "Two-handed Sword",
+            "power": 55,
+        },
+        "potion": None,
+    },
+    "mordred": {
+        "name": "Mordred",
+        "power": 30,
+        "hp": 90,
+        "armour": [
+            {
+                "part": "breastplate",
+                "protection": 15,
+            },
+            {
+                "part": "boots",
+                "protection": 10,
+            }
+        ],
+        "weapon": {
+            "name": "Poisoned Sword",
+            "power": 60,
+        },
+        "potion": {
+            "name": "Berserk",
+            "effect": {
+                "power": +15,
+                "hp": -5,
+                "protection": +10,
+            }
+        }
+    },
+    "red_knight": {
+        "name": "Red Knight",
+        "power": 40,
+        "hp": 70,
+        "armour": [
+            {
+                "part": "breastplate",
+                "protection": 25,
+            }
+        ],
+        "weapon": {
+            "name": "Sword",
+            "power": 45
+        },
+        "potion": {
+            "name": "Blessing",
+            "effect": {
+                "hp": +10,
+                "power": +5,
+            }
+        }
+    }
+}
+
+
+def battle(knights_config: dict) -> dict:
+    knights = [
+        Knight(**knights_config["lancelot"]),
+        Knight(**knights_config["arthur"]),
+        Knight(**knights_config["mordred"]),
+        Knight(**knights_config["red_knight"]),
+    ]
+
+    for knight in knights:
+        knight.prepare_for_battle()
+
+    battle1 = Battle(knights[0], knights[2])  # Lancelot vs Mordred
+    battle1.fight()
+    battle2 = Battle(knights[1], knights[3])  # Arthur vs Red Knight
+    battle2.fight()
+
+    results = {**battle1.get_result(), **battle2.get_result()}
+    return results
+
+
+def main() -> None:
+    results = battle(KNIGHTS)
+    print("The fight result:", results)
+
+
+if __name__ == "__main__":
+    main()
