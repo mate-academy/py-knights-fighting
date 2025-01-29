e455a99ad06d31c7ce1d5afce980560b542aab7c
Kateryna <kateryna.kotyh@gmail.com>
2025-01-29 21:05:50 +0200

    Edits version 2

--- app/event/opponents.py
+++ app/event/opponents.py
@@ -1,32 +1,27 @@
-# from app.knights.preparations import Knight
-# from app.knights.information import KNIGHTS
-
 from app.event.battle import Battle
 
-# from knights.preparations import Knight
-# from knights.information import KNIGHTS
 
-def sparring(wariers):
-    first_warior = None
-    second_warior = None
-    for man in wariers:
+def sparring(warriors: list) -> dict:
+    first_warrior = None
+    second_warrior = None
+    for man in warriors:
         if man.name == "Lancelot":
-            first_warior = man
+            first_warrior = man
         if man.name == "Mordred":
-            second_warior = man
-    lancelot_vs_mordred = Battle(first_warior, second_warior)
+            second_warrior = man
+    lancelot_vs_mordred = Battle(first_warrior, second_warrior)
     lancelot_vs_mordred.fight()
 
-    for man in wariers:
+    for man in warriors:
         if man.name == "Arthur":
-            first_warior = man
+            first_warrior = man
         if man.name == "Red Knight":
-            second_warior = man
-    arthur_vs_red_knight = Battle(first_warior, second_warior)
+            second_warrior = man
+    arthur_vs_red_knight = Battle(first_warrior, second_warrior)
     arthur_vs_red_knight.fight()
 
     result = {}
 
-    for man in wariers:
+    for man in warriors:
         result[man.name] = man.hp
     return result
--- app/knights/preparations.py
+++ app/knights/preparations.py
@@ -2,23 +2,21 @@ from __future__ import annotations
 
 
 class Knight:
-    def __init__(self, name: str, power: int, hp: int):
+    def __init__(self, name: str, power: int, hp: int) -> None:
         self.name = name
         self.power = power
         self.hp = hp
         self.protection = 0
 
-    def get_armour(self, armour: list):
-        # protection = 0
+    def get_armour(self, armour: list) -> None:
         if armour:
             for part in armour:
                 self.protection += part["protection"]
-        return self.protection
 
-    def get_weapon(self, weapon: dict):
+    def get_weapon(self, weapon: dict) -> None:
         self.power += weapon["power"]
 
-    def get_potion(self, potion: None | dict):
+    def get_potion(self, potion: None | dict) -> None:
         if potion is not None:
             if "power" in potion["effect"]:
                 self.power += potion["effect"]["power"]
