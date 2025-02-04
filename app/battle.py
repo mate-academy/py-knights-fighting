3b77c8c03ded14d95e3f65b9d58514872d453f82
Oleksandr Kozlov <aleksandrkozlov2015@gmail.com>
2025-02-04 13:35:39 +0100

    'Trial_solution'

---
+++ app/battle.py
@@ -0,0 +1,26 @@
+from knight import Knight
+from knights_config import KNIGHTS
+
+
+def battle(knights_config: dict = KNIGHTS) -> list:
+    lancelot, arthur, mordred, red_knight = [
+        Knight(
+            name=data.get("name"),
+            power=data.get("power"),
+            hp=data.get("hp"),
+            armour=data.get("armour"),
+            weapon=data.get("weapon"),
+            potion=data.get("potion"),
+        )
+        for data in knights_config.values()
+    ]
+    lancelot.fight(mordred)
+    arthur.fight(red_knight)
+
+    battle_result_hp = {}
+    for knight in (lancelot, arthur, mordred, red_knight):
+        battle_result_hp[knight.name] = knight.hp
+    return battle_result_hp
+
+
+print(battle(knights_config=KNIGHTS))
---
+++ app/knight.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+
+class Knight:
+
+    def __init__(
+        self, name: str, power: int, hp: int, armour: list, weapon: dict, potion: dict
+    ) -> None:
+        self.name = name
+        self.power = power
+        self.hp = hp
+        self.armour = armour
+        self.weapon = weapon
+        self.potion = potion
+        self.protection = 0
+
+    def prepare_knight(self) -> None:
+        self.protection = sum(armour["protection"] for armour in self.armour)
+
+        self.power += self.weapon["power"]
+
+        if self.potion:
+            for effect_name in ("hp", "power", "protection"):
+                potion = getattr(
+                    self, effect_name
+                )  # Отримання значення атрибута з об'єкту self в рядку що містить назву атрибута "effect name"
+                setattr(
+                    self, effect_name, potion + self.potion["effect"][effect_name]
+                )  # Встановлення значення існуючого атрибута self в рядку effect_name на значення potion + self.potion["effect"][effect_name]
+
+    def repr(self) -> None:
+        return (
+            f"Knight: {self.name}, {self.power}, "
+            f"{self.hp}, {self.armour}, {self.weapon}, "
+            f"{self.potion}, {self.protection}"
+        )
+
+    def fight(self, other: Knight) -> None:
+        self.hp -= other.power - self.protection
+        other.hp -= self.power - other.protection
+        if self.hp <= 0:
+            self.hp == 0
+        elif other.hp <= 0:
+            other.hp == 0
