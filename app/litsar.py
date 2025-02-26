9ab703a1196a89f166d42a6e6519cb908cf08b85
Denys Dikhtiar <dinis.dihtjar08@gmail.com>
2025-02-26 13:23:27 +0200

    Solution

---
+++ app/litsar.py
@@ -0,0 +1,20 @@
+def calculate_stats(knight: dict) -> dict:
+    effective_hp = knight["hp"]
+    effective_power = knight["power"] + knight["weapon"]["power"]
+    effective_protection = sum(
+        item.get("protection", 0) for item in knight.get("armour", [])
+    )
+
+    potion = knight.get("potion")
+    if potion is not None:
+        effect = potion.get("effect", {})
+        effective_hp += effect.get("hp", 0)
+        effective_power += effect.get("power", 0)
+        effective_protection += effect.get("protection", 0)
+
+    return {
+        "name": knight["name"],
+        "hp": effective_hp,
+        "power": effective_power,
+        "protection": effective_protection,
+    }
