f782792307d220597c3476537f6be593ddb8b651
Denys Dikhtiar <dinis.dihtjar08@gmail.com>
2025-03-01 13:00:04 +0200

    Fixed .get and Flake

--- app/battle.py
+++ app/battle.py
@@ -8,31 +8,39 @@ def battle(knights: dict) -> dict:
 
     stats.get("lancelot", {}).update({
         "hp": max(0, stats.get("lancelot", {}).get("hp", 0) - (
-            stats.get("mordred", {}).get("power", 0) - stats.get("lancelot", {}).get("protection", 0)
+            stats.get("mordred", {}).get("power", 0)
+            - stats.get("lancelot", {}).get("protection", 0)
         ))
     })
 
     stats.get("mordred", {}).update({
         "hp": max(0, stats.get("mordred", {}).get("hp", 0) - (
-            stats.get("lancelot", {}).get("power", 0) - stats.get("mordred", {}).get("protection", 0)
+            stats.get("lancelot", {}).get("power", 0)
+            - stats.get("mordred", {}).get("protection", 0)
         ))
     })
 
     stats.get("arthur", {}).update({
         "hp": max(0, stats.get("arthur", {}).get("hp", 0) - (
-            stats.get("red_knight", {}).get("power", 0) - stats.get("arthur", {}).get("protection", 0)
+            stats.get("red_knight", {}).get("power", 0)
+            - stats.get("arthur", {}).get("protection", 0)
         ))
     })
 
     stats.get("red_knight", {}).update({
         "hp": max(0, stats.get("red_knight", {}).get("hp", 0) - (
-            stats.get("arthur", {}).get("power", 0) - stats.get("red_knight", {}).get("protection", 0)
+            stats.get("arthur", {}).get("power", 0)
+            - stats.get("red_knight", {}).get("protection", 0)
         ))
     })
 
     return {
-        stats.get("lancelot", {}).get("name", "unknown"): stats.get("lancelot", {}).get("hp", 0),
-        stats.get("arthur", {}).get("name", "unknown"): stats.get("arthur", {}).get("hp", 0),
-        stats.get("mordred", {}).get("name", "unknown"): stats.get("mordred", {}).get("hp", 0),
-        stats.get("red_knight", {}).get("name", "unknown"): stats.get("red_knight", {}).get("hp", 0),
+        stats.get("lancelot", {}).get("name", "unknown"):
+            stats.get("lancelot", {}).get("hp", 0),
+        stats.get("arthur", {}).get("name", "unknown"):
+            stats.get("arthur", {}).get("hp", 0),
+        stats.get("mordred", {}).get("name", "unknown"):
+            stats.get("mordred", {}).get("hp", 0),
+        stats.get("red_knight", {}).get("name", "unknown"):
+            stats.get("red_knight", {}).get("hp", 0),
     }
