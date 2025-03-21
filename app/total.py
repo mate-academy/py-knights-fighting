32a29d8f50f2cb7734c15acc60d8de389a0fb57e
Oleksandr Movchan <alexdpuaua@gmail.com>
2025-03-21 13:20:13 -0400

    added files

---
+++ app/total.py
@@ -0,0 +1,25 @@
+
+
+class Power:
+
+    def __init__(self, armour: list, weapon: dict, potion: dict) -> None:
+        self.armour = armour
+        self.weapon = weapon
+        self.potion = potion
+
+    def total_power(self) -> tuple:
+        sum_power = 0
+        sum_protection = 0
+        sum_hp = 0
+        if self.armour:
+            for arm in self.armour:
+                sum_protection += arm.get("protection", 0)
+        if self.weapon:
+            sum_power += self.weapon.get("power", 0)
+        if self.potion and "effect" in self.potion:
+            effect = self.potion["effect"]
+            sum_power += effect.get("power", 0)
+            sum_protection += effect.get("protection", 0)
+            sum_hp += effect.get("hp", 0)
+
+        return sum_hp, sum_power, sum_protection
