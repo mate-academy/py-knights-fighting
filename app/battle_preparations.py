4c0646bdb529f947da1adc493a725b9d7d53dfed
Bartek Pietrzyk <bartekpietrzyk@interia.eu>
2025-04-27 10:02:12 +0200

    Solution

--- app/battle_preparations.py
+++ app/battle_preparations.py
@@ -1,11 +1,13 @@
 def prepare_knight(knight: dict) -> dict:
+    knight_copy = knight.copy()
 
-    knight["protection"] = sum(armo["protection"] for armo in knight["armour"])
+    knight_copy["protection"] = sum(
+        armo["protection"] for armo in knight_copy["armour"]
+    )
+    knight_copy["power"] += knight_copy["weapon"]["power"]
 
-    knight["power"] += knight["weapon"]["power"]
+    if knight_copy.get("potion") and knight_copy["potion"].get("effect"):
+        for stat, value in knight_copy["potion"]["effect"].items():
+            knight_copy[stat] = knight_copy.get(stat, 0) + value
 
-    if knight.get("potion") and knight["potion"].get("effect"):
-        for stat, value in knight["potion"]["effect"].items():
-            knight[stat] = knight.get(stat, 0) + value
-
-    return knight
+    return knight_copy
