71ab3d60da6a0d0fe5ff1de0331e0d8e943136d2
Serhii Kermas <serhii.kermas@gmail.com>
2025-05-21 13:17:50 +0300

    Solution

---
+++ app/battle.py
@@ -0,0 +1,20 @@
+from app.knights import Knights
+
+
+def fell_check(knight: Knights) -> None:
+    if knight.hp <= 0:
+        knight.hp = 0
+
+
+def fight(first_knight: Knights, second_knight: Knights) -> None:
+
+    first_knight.fight_preparing()
+    second_knight.fight_preparing()
+    # print(first_knight.power, first_knight.hp, first_knight.protection)
+    # print(second_knight.power, second_knight.hp, second_knight.protection)
+
+    first_knight.hp -= second_knight.power - first_knight.protection
+    second_knight.hp -= first_knight.power - second_knight.protection
+
+    fell_check(first_knight)
+    fell_check(second_knight)
---
+++ app/knights.py
@@ -0,0 +1,71 @@
+class Potion():
+    potion_list = []
+
+    def __init__(self, name: str, power: int, hp: int,
+                 protection: int) -> None:
+        self.name = name
+        self.power = power
+        self.hp = hp
+        self.protection = protection
+        Potion.potion_list.append(self)
+
+
+class Knights():
+    knights_list = []
+
+    def __init__(self, name: str, power: int, hp: int,
+                 armour: list, weapon: dict,
+                 potion: (Potion, None)) -> None:
+        self.name = name
+        self.power = power
+        self.hp = hp
+        self.protection = 0
+        self.armour = armour
+        self.weapon = weapon
+        self.potion = potion
+        Knights.knights_list.append(self)
+
+    def apply_armour(self) -> None:
+        for part in self.armour:
+            self.protection += part["protection"]
+
+    def apply_weapon(self) -> None:
+        self.power += self.weapon["power"]
+
+    def apply_potion(self) -> None:
+        if self.potion is not None:
+            self.power += self.potion.power
+            self.hp += self.potion.hp
+            self.protection += self.potion.protection
+
+    def fight_preparing(self) -> None:
+        self.apply_armour()
+        self.apply_weapon()
+        self.apply_potion()
+
+
+def create_knight(knights_info: dict) -> None:
+
+    for knight in knights_info:
+        if (knights_info[knight]["potion"] is not None):
+            power, hp, protection = 0, 0, 0
+
+            if "power" in knights_info[knight]["potion"]["effect"]:
+                power += knights_info[knight]["potion"]["effect"]["power"]
+            if "hp" in knights_info[knight]["potion"]["effect"]:
+                hp += knights_info[knight]["potion"]["effect"]["hp"]
+            if "protection" in knights_info[knight]["potion"]["effect"]:
+                protection \
+                    = knights_info[knight]["potion"]["effect"]["protection"]
+
+            potion = Potion(knights_info[knight]["potion"]["name"],
+                            power, hp, protection)
+        else:
+            potion = None
+
+        Knights(knights_info[knight]["name"],
+                knights_info[knight]["power"],
+                knights_info[knight]["hp"],
+                knights_info[knight]["armour"],
+                knights_info[knight]["weapon"],
+                potion)
