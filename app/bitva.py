591438a03b494bd89f7bc37c37ab03a9694b70d8
Yevhen Nastoiashchyi <Nastoyashiy@i.ua>
2025-06-01 11:02:22 +0300

    Solution

--- app/bitva.py
+++ app/bitva.py
@@ -6,16 +6,17 @@ def battle(kn_battle: dict) -> dict:
         apply_stats(knight)
     battles = [("lancelot", "mordred"), ("arthur", "red_knight")]
     for knight1, knight2 in battles:
-        kn_battle[knight1]["hp"] = max(
-            0,
-            kn_battle[knight1]["hp"]
-            - kn_battle[knight2]["power"]
-            + kn_battle[knight1]["protection"]
-        )
-        kn_battle[knight2]["hp"] = max(
-            0,
-            kn_battle[knight2]["hp"]
-            - kn_battle[knight1]["power"]
-            + kn_battle[knight2]["protection"]
-        )
+        if knight1 in kn_battle and knight2 in kn_battle:
+            kn_battle[knight1]["hp"] = max(
+                0,
+                kn_battle[knight1]["hp"]
+                - kn_battle[knight2]["power"]
+                + kn_battle[knight1]["protection"]
+            )
+            kn_battle[knight2]["hp"] = max(
+                0,
+                kn_battle[knight2]["hp"]
+                - kn_battle[knight1]["power"]
+                + kn_battle[knight2]["protection"]
+            )
     return {knight["name"]: knight["hp"] for knight in kn_battle.values()}
--- app/pidg.py
+++ app/pidg.py
@@ -2,6 +2,6 @@ def apply_stats(knight: dict) -> None:
     knight["protection"] = sum(arm["protection"] for arm in knight["armour"])
     knight["power"] += knight["weapon"]["power"]
 
-    if knight["potion"]:
+    if knight["potion"] is not None:
         for stat, value in knight["potion"]["effect"].items():
             knight[stat] += value
