1b758366b5c2eb25dc3d9e7c0783df9e73d402ca
helix09825 <maksilczuk@gmail.com>
2025-06-10 15:29:58 +0200

    Solution

---
+++ app/functions_for_battle/battle.py
@@ -0,0 +1,10 @@
+def battle_time(knight_1: dict, knight_2: dict) -> None:
+    knight_1["hp"] -= knight_2["power"] - knight_1["protection"]
+    knight_2["hp"] -= knight_1["power"] - knight_2["protection"]
+
+    # check if someone fell in battle
+    if knight_1["hp"] <= 0:
+        knight_1["hp"] = 0
+
+    if knight_2["hp"] <= 0:
+        knight_2["hp"] = 0
--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
-from app.functions_for_battle.apply_armour import apply_armour
-from app.functions_for_battle.apply_potion import apply_potion
+from app.functions_for_battle.get_ready_to_battle import get_ready_to_battle
+from app.functions_for_battle.battle import battle_time
 
 
 KNIGHTS = {
@@ -96,75 +96,35 @@ def battle(knights_config: dict) -> dict:
     # lancelot
     lancelot = knights_config["lancelot"]
 
-    # apply armour
-    apply_armour(lancelot)
-
-    # apply weapon
-    lancelot["power"] += lancelot["weapon"]["power"]
-
-    # apply potion if exist
-    apply_potion(lancelot)
+    # get ready to battle
+    get_ready_to_battle(lancelot)
 
     # arthur
     arthur = knights_config["arthur"]
 
-    # apply armour
-    apply_armour(arthur)
-
-    # apply weapon
-    arthur["power"] += arthur["weapon"]["power"]
-
-    # apply potion if exist
-    apply_potion(arthur)
+    # get ready to battle
+    get_ready_to_battle(arthur)
 
     # mordred
     mordred = knights_config["mordred"]
 
-    # apply armour
-    apply_armour(mordred)
-
-    # apply weapon
-    mordred["power"] += mordred["weapon"]["power"]
-
-    # apply potion if exist
-    apply_potion(mordred)
+    # get ready to battle
+    get_ready_to_battle(mordred)
 
     # red_knight
     red_knight = knights_config["red_knight"]
 
-    # apply armour
-    apply_armour(red_knight)
-
-    # apply weapon
-    red_knight["power"] += red_knight["weapon"]["power"]
-
-    # apply potion if exist
-    apply_potion(red_knight)
+    # get ready to battle
+    get_ready_to_battle(red_knight)
 
     # -------------------------------------------------------------------------------
     # BATTLE:
 
     # 1 Lancelot vs Mordred:
-    lancelot["hp"] -= mordred["power"] - lancelot["protection"]
-    mordred["hp"] -= lancelot["power"] - mordred["protection"]
-
-    # check if someone fell in battle
-    if lancelot["hp"] <= 0:
-        lancelot["hp"] = 0
-
-    if mordred["hp"] <= 0:
-        mordred["hp"] = 0
+    battle_time(lancelot, mordred)
 
     # 2 Arthur vs Red Knight:
-    arthur["hp"] -= red_knight["power"] - arthur["protection"]
-    red_knight["hp"] -= arthur["power"] - red_knight["protection"]
-
-    # check if someone fell in battle
-    if arthur["hp"] <= 0:
-        arthur["hp"] = 0
-
-    if red_knight["hp"] <= 0:
-        red_knight["hp"] = 0
+    battle_time(arthur, red_knight)
 
     # Return battle results:
     return {
