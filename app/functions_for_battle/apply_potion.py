120bb1326aa37c79298cbd2b92de75ba5fa061c7
helix09825 <maksilczuk@gmail.com>
2025-06-10 15:31:24 +0200

    Solution

--- app/functions_for_battle/apply_potion.py
+++ app/functions_for_battle/apply_potion.py
@@ -4,8 +4,7 @@ def apply_potion(knight: dict) -> None:
             knight["power"] += knight["potion"]["effect"]["power"]
 
         if "protection" in knight["potion"]["effect"]:
-            knight["protection"] += (
-                knight)["potion"]["effect"]["protection"]
+            knight["protection"] += knight["potion"]["effect"]["protection"]
 
         if "hp" in knight["potion"]["effect"]:
             knight["hp"] += knight["potion"]["effect"]["hp"]
