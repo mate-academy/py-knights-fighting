c861f32481585ec8b73880ad97bf250dbcf99d1e
Mirosław Kudłacz <mirek.kudlacz@gmail.com>
2025-06-17 18:02:46 +0200

    Solution_v3

--- app/knights/knight_inst.py
+++ app/knights/knight_inst.py
@@ -1,5 +1,6 @@
 class Knight:
-    def __init__(self, name: str, power: int, hp: int, armour: list[dict], weapon: dict, potion: dict) -> None:
+    def __init__(self, name: str, power: int, hp: int, armour: list[dict],
+                 weapon: dict, potion: dict) -> None:
         self.name = name
         self.base_power = power
         self.base_hp = hp
@@ -11,7 +12,7 @@ class Knight:
         self.current_power = power
         self.current_protection = 0
 
-    def prepare_for_battle(self):
+    def prepare_for_battle(self) -> None:
         for i in self.armour_config:
             self.current_protection += i["protection"]
         self.current_power = self.base_power + self.weapon_config["power"]
@@ -20,12 +21,19 @@ class Knight:
                 self.current_power += self.potion_config["effect"]["power"]
 
             if "protection" in self.potion_config["effect"]:
-                self.current_protection += self.potion_config["effect"]["protection"]
+                self.current_protection +=\
+                    self.potion_config["effect"]["protection"]
 
             if "hp" in self.potion_config["effect"]:
                 self.current_hp += self.potion_config["effect"]["hp"]
 
-    def take_damage(self, opponent_power):
-        self.current_hp -= opponent_power - self.current_protection
+    def take_damage(self, opponent_power: int) -> None:
+        damage = opponent_power - self.current_protection
+        self.current_hp -= damage
         if self.current_hp <= 0:
             self.current_hp = 0
+
+    def reset_stats(self) -> None:
+        self.current_hp = self.base_hp
+        self.current_power = self.base_power
+        self.current_protection = 0
--- app/main.py
+++ app/main.py
@@ -2,21 +2,21 @@ from app.knights.knight_inst import Knight
 
 KNIGHTS = {
     "lancelot": Knight(
-        name= "Lancelot",
-        power =  35,
-        hp = 100,
-        armour = [],
-        weapon = {
+        name="Lancelot",
+        power=35,
+        hp=100,
+        armour=[],
+        weapon={
             "name": "Metal Sword",
             "power": 50,
         },
-        potion = None,
+        potion=None,
     ),
     "arthur": Knight(
-        name = "Arthur",
-        power =  45,
-        hp =  75,
-        armour = [
+        name="Arthur",
+        power=45,
+        hp=75,
+        armour=[
             {
                 "part": "helmet",
                 "protection": 15,
@@ -30,17 +30,17 @@ KNIGHTS = {
                 "protection": 10,
             }
         ],
-        weapon = {
+        weapon={
             "name": "Two-handed Sword",
             "power": 55,
         },
-        potion = None,
+        potion=None,
     ),
     "mordred": Knight(
-        name = "Mordred",
-        power = 30,
-        hp = 90,
-        armour = [
+        name="Mordred",
+        power=30,
+        hp=90,
+        armour=[
             {
                 "part": "breastplate",
                 "protection": 15,
@@ -50,11 +50,11 @@ KNIGHTS = {
                 "protection": 10,
             }
         ],
-        weapon = {
+        weapon={
             "name": "Poisoned Sword",
             "power": 60,
         },
-        potion = {
+        potion={
             "name": "Berserk",
             "effect": {
                 "power": +15,
@@ -62,22 +62,22 @@ KNIGHTS = {
                 "protection": +10,
             }
         }
-),
+    ),
     "red_knight": Knight(
-        name = "Red Knight",
-        power = 40,
-        hp = 70,
-        armour = [
+        name="Red Knight",
+        power=40,
+        hp=70,
+        armour=[
             {
                 "part": "breastplate",
                 "protection": 25,
             }
         ],
-        weapon = {
+        weapon={
             "name": "Sword",
             "power": 45
         },
-        potion = {
+        potion={
             "name": "Blessing",
             "effect": {
                 "hp": +10,
@@ -88,27 +88,32 @@ KNIGHTS = {
 }
 
 
-def battle(knightsConfig):
+def battle(knightsconfig: dict) -> dict:
     lancelot_obj = KNIGHTS["lancelot"]
-    artur_obj = KNIGHTS["arthur"]
+    arthur_obj = KNIGHTS["arthur"]
     mordred_obj = KNIGHTS["mordred"]
     red_knight_obj = KNIGHTS["red_knight"]
 
+    lancelot_obj.reset_stats()
+    arthur_obj.reset_stats()
+    mordred_obj.reset_stats()
+    red_knight_obj.reset_stats()
+
     lancelot_obj.prepare_for_battle()
-    artur_obj.prepare_for_battle()
+    arthur_obj.prepare_for_battle()
     mordred_obj.prepare_for_battle()
     red_knight_obj.prepare_for_battle()
 
     lancelot_obj.take_damage(mordred_obj.current_power)
     mordred_obj.take_damage(lancelot_obj.current_power)
 
-    artur_obj.take_damage(red_knight_obj.current_power)
-    red_knight_obj.take_damage(artur_obj.current_power)
-    return {lancelot_obj.name: lancelot_obj.current_hp,
-            artur_obj.name: artur_obj.current_hp,
+    arthur_obj.take_damage(red_knight_obj.current_power)
+    red_knight_obj.take_damage(arthur_obj.current_power)
+    return {arthur_obj.name: arthur_obj.current_hp,
+            lancelot_obj.name: lancelot_obj.current_hp,
             mordred_obj.name: mordred_obj.current_hp,
             red_knight_obj.name: red_knight_obj.current_hp
-}
+            }
+
+
 print(battle(KNIGHTS))
-for key, value in KNIGHTS.items():
-    print(key, value)
