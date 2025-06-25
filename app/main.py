eede15bea67d7061a23b98c898c566461f232898
Vovas <vovaafon25@gmail.com>
2025-06-25 19:52:15 +0300

    Solution remastered (moved KNIGHTS dict in knights module)

--- app/main.py
+++ app/main.py
@@ -1,99 +1,11 @@
-from modules.knight import Knight
+from modules.knight import Knight, KNIGHTS
 from modules.arsenal import Weapon, Armor, Potion
 
 
-knights = {
-    "lancelot": {
-        "name": "Lancelot",
-        "power": 35,
-        "hp": 100,
-        "armour": [],
-        "weapon": {
-            "name": "Metal Sword",
-            "power": 50,
-        },
-        "potion": None,
-    },
-    "arthur": {
-        "name": "Arthur",
-        "power": 45,
-        "hp": 75,
-        "armour": [
-            {
-                "part": "helmet",
-                "protection": 15,
-            },
-            {
-                "part": "breastplate",
-                "protection": 20,
-            },
-            {
-                "part": "boots",
-                "protection": 10,
-            }
-        ],
-        "weapon": {
-            "name": "Two-handed Sword",
-            "power": 55,
-        },
-        "potion": None,
-    },
-    "mordred": {
-        "name": "Mordred",
-        "power": 30,
-        "hp": 90,
-        "armour": [
-            {
-                "part": "breastplate",
-                "protection": 15,
-            },
-            {
-                "part": "boots",
-                "protection": 10,
-            }
-        ],
-        "weapon": {
-            "name": "Poisoned Sword",
-            "power": 60,
-        },
-        "potion": {
-            "name": "Berserk",
-            "effect": {
-                "power": +15,
-                "hp": -5,
-                "protection": +10,
-            }
-        }
-    },
-    "red_knight": {
-        "name": "Red Knight",
-        "power": 40,
-        "hp": 70,
-        "armour": [
-            {
-                "part": "breastplate",
-                "protection": 25,
-            }
-        ],
-        "weapon": {
-            "name": "Sword",
-            "power": 45
-        },
-        "potion": {
-            "name": "Blessing",
-            "effect": {
-                "hp": +10,
-                "power": +5,
-            }
-        }
-    }
-}
-
-
-def battle(knightsconfig: dict) -> dict:
+def battle(knights_config: dict) -> dict:
     list_of_knights = []
 
-    for knight_data in knightsconfig.values():
+    for knight_data in knights_config.values():
         armor_list = [
             Armor(ar["part"], ar["protection"])
             for ar in knight_data["armour"]
@@ -140,3 +52,7 @@ def battle(knightsconfig: dict) -> dict:
                 "Mordred": mordred.hp, "Red Knight": red_knight.hp}
 
     return res_dict
+
+
+if __name__ == "__main__":
+    print(battle(KNIGHTS))
