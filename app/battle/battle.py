b505a353297aaae85f4c585cad58fbc672759bbd
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-06-29 14:35:10 +0300

    added solution with OOP approach

---
+++ app/battle/battle.py
@@ -0,0 +1,25 @@
+from app.data.knights_data import create_all_knights
+from app.models.knight import Knight
+
+
+def fight(first_knight: Knight, second_knight: Knight) -> None:
+
+    damage_to_first_knight = second_knight.power - first_knight.protection
+    damage_to_second_knight = first_knight.power - second_knight.protection
+    first_knight.take_damage(damage_to_first_knight)
+    second_knight.take_damage(damage_to_second_knight)
+
+
+def tournament(knights_data: dict) -> dict:
+    knights = create_all_knights(knights_data)
+
+    for knight in knights.values():
+        knight.prepare_for_battle()
+
+    fight(knights["lancelot"], knights["mordred"])
+    fight(knights["arthur"], knights["red_knight"])
+
+    return {
+        knight.name: knight.hp
+        for knight in knights.values()
+    }
