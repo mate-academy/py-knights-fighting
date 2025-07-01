b12fc2761a71bc9f007165e158d35ce4cf9cd6df
Anastasiia Skorodied <anastasia.skoroded@gmail.com>
2025-07-01 21:45:15 +0300

    Addressed feedback

--- app/tournament/battle.py
+++ app/tournament/battle.py
@@ -7,10 +7,10 @@ class Battle:
     def update_hp(knight_1: Knight, knight_2: Knight) -> None:
         stats_1 = knight_1.calculate_stats()
         stats_2 = knight_2.calculate_stats()
-        knight_1.hp = max(stats_1["hp"]
-                          - (stats_2["power"] - stats_1["protection"]), 0)
-        knight_2.hp = max(stats_2["hp"]
-                          - (stats_1["power"] - stats_2["protection"]), 0)
+        knight_1.hp = max(0, stats_1["hp"]
+                          - max(0, stats_2["power"] - stats_1["protection"]))
+        knight_2.hp = max(0, stats_2["hp"]
+                          - max(0, stats_1["power"] - stats_2["protection"]))
 
     @staticmethod
     def battle_result(knights: list[Knight]) -> dict:
