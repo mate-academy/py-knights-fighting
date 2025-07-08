eb2e0ce8bf3adc31fb5dc2a49355e2fd82cb5a08
Olha <olaviznak866@gmail.com>
2025-07-08 21:09:59 +0300

    Solution

--- app/EQUIPMENT/potion.py
+++ app/EQUIPMENT/potion.py
@@ -1,4 +1,7 @@
+from typing import Union
+
+
 class Potion:
-    def __init__(self, data: dict[str, dict[str, int]]) -> None:
+    def __init__(self, data: dict[str, Union[str, dict[str, int]]]) -> None:
         self.name = data["name"]
         self.effect = data["effect"]
