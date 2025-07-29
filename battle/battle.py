da856ad93ce87e73e76445a1ac7251d3b3d89bfa
reinvvay <reinvvay@gmail.com>
2025-07-29 22:04:20 +0300

    Refactoring

--- battle/battle.py
+++ battle/battle.py
@@ -1,32 +1,18 @@
 from battle.knight import Knight
 
 
-def create_knight(knight: dict) -> dict:
-    knight = Knight(**knight)
-    return knight.calculate_total_power()
-
-
 def battle(knights: dict) -> dict:
-    lancelot = create_knight(knights["lancelot"])
-    arthur = create_knight(knights["arthur"])
-    mordred = create_knight(knights["mordred"])
-    red_knight = create_knight(knights["red_knight"])
+    lancelot = Knight.from_dict(knights["lancelot"])
+    mordred = Knight.from_dict(knights["mordred"])
+    arthur = Knight.from_dict(knights["arthur"])
+    red_knight = Knight.from_dict(knights["red_knight"])
 
-    # 1 Lancelot vs Mordred
-    lancelot["hp"] -= mordred["power"] - lancelot["protection"]
-    mordred["hp"] -= lancelot["power"] - mordred["protection"]
-
-    # 2 Arthur vs Red Knight
-    arthur["hp"] -= red_knight["power"] - arthur["protection"]
-    red_knight["hp"] -= arthur["power"] - red_knight["protection"]
-
-    for knight in [lancelot, arthur, mordred, red_knight]:
-        if knight["hp"] <= 0:
-            knight["hp"] = 0
+    lancelot.perform_battle(mordred)
+    arthur.perform_battle(red_knight)
 
     return {
-        lancelot["name"]: lancelot["hp"],
-        arthur["name"]: arthur["hp"],
-        mordred["name"]: mordred["hp"],
-        red_knight["name"]: red_knight["hp"],
+        lancelot.name: lancelot.hp,
+        mordred.name: mordred.hp,
+        arthur.name: arthur.hp,
+        red_knight.name: red_knight.hp
     }
--- battle/knight.py
+++ battle/knight.py
@@ -10,30 +10,40 @@ class Knight:
     weapon: dict
     potion: dict | None
 
-    def calculate_total_power(self) -> dict:
-        final_protection = 0
+    @property
+    def stats(self) -> dict:
         final_hp = self.hp
-        final_dmg = self.weapon["power"] + self.power
-
-        if self.armour:
-            for armor in self.armour:
-                final_protection += armor["protection"]
+        final_protection = sum(
+            a["protection"] for a in self.armour
+        ) if self.armour else 0
+        final_power = self.weapon["power"] + self.power
 
         if self.potion:
-            if "power" in self.potion["effect"]:
-                final_dmg += self.potion["effect"]["power"]
-
-            if "protection" in self.potion["effect"]:
-                final_protection += self.potion["effect"]["protection"]
-
-            if "hp" in self.potion["effect"]:
-                final_hp += self.potion["effect"]["hp"]
+            effect = self.potion.get("effect", {})
+            final_power += effect.get("power", 0)
+            final_protection += effect.get("protection", 0)
+            final_hp += effect.get("hp", 0)
 
         result = {
             "name": self.name,
-            "power": final_dmg,
+            "power": final_power,
             "hp": final_hp,
             "protection": final_protection
         }
 
         return result
+
+    def perform_battle(self, other: "Knight") -> None:
+        k1_stats = self.stats
+        k2_stats = other.stats
+
+        self.hp = max(
+            k1_stats["hp"] - (k2_stats["power"] - k1_stats["protection"]), 0
+        )
+        other.hp = max(
+            k2_stats["hp"] - (k1_stats["power"] - k2_stats["protection"]), 0
+        )
+
+    @classmethod
+    def from_dict(cls, data: dict) -> "Knight":
+        return cls(**data)
