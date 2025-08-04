9082f6dd9dcfb0b6567bd15c56cbc74bbc157849
Arseniy Kushnir <arseniykushnir2007@gmail.com>
2025-08-04 19:19:59 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -9,10 +9,10 @@ def battle(knights: dict) -> dict:
     knight_names = list(format_knights.keys())
     result = {}
 
-    for index in range(0, len(knight_names), 2):
-        if index + 1 < len(knight_names):
+    for index in range(len(knight_names)):
+        if index + 2 < len(knight_names):
             knight_one_name = knight_names[index]
-            knight_two_name = knight_names[index + 1]
+            knight_two_name = knight_names[index + 2]
 
             knight_one = format_knights[knight_one_name]
             knight_two = format_knights[knight_two_name]
