a1c7cf90931eeed2fa13938edcfe8897fe9abf3c
Olexii Babii <babiy0617@gmail.com>
2025-08-06 17:57:49 +0300

    Solution2

---
+++ app/battle_preparation/creating_obj.py
@@ -0,0 +1,48 @@
+from app.battle_preparation.armour import Armour
+from app.battle_preparation.knight import Knight
+from app.battle_preparation.weapon import Weapon
+from app.battle_preparation.potion import Potion
+
+
+def creating_knights(dictionary: dict) -> list[Knight]:
+    result = []
+    for value in dictionary.values():
+        obj = Knight(name=value["name"], power=value["power"], hp=value["hp"])
+        obj.__repr__()
+        result.append(obj)
+    return result
+
+
+def creating_armors(dictionary: dict) -> dict[list[Armour]]:
+    result = {}
+    for value in dictionary.values():
+        list_of_armors = []
+        if value["armour"] != []:
+            for armour in value["armour"]:
+                obj = Armour(part=armour["part"],
+                             protection=armour["protection"])
+                obj.__repr__()
+                list_of_armors.append(obj)
+        result[value["name"]] = list_of_armors
+    return result
+
+
+def creating_weapons(dictionary: dict) -> dict[Weapon]:
+    result = {}
+    for value in dictionary.values():
+        obj = Weapon(name=value["weapon"]["name"],
+                     power=value["weapon"]["power"])
+        obj.__repr__()
+        result[value["name"]] = obj
+    return result
+
+
+def creating_potions(dictionary: dict) -> dict[Potion]:
+    result = {}
+    for value in dictionary.values():
+        if value["potion"] is not None:
+            obj = Potion(name=value["potion"]["name"],
+                         effect=value["potion"]["effect"])
+            obj.__repr__()
+            result[value["name"]] = obj
+    return result
---
+++ app/battle_preparation/knight.py
@@ -0,0 +1,8 @@
+class Knight:
+    def __init__(self, name: str, power: int, hp: int,) -> None:
+        self.name = name
+        self.power = power
+        self.hp = hp
+
+    def __repr__(self) -> str:
+        return (self.name)
