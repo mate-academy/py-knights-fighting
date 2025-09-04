3856f30d312824fd9e310600eeca3d4d6675f287
Anna S <my@example.com>
2025-09-04 00:17:40 -0300

    Fixed critical nd minor issues, flake8 pytest run

--- app/battle.py
+++ app/battle.py
@@ -1,10 +1,11 @@
 from typing import Dict, Any
 
+from app.battle_round import battle_round
 from app.participants import KNIGHTS
 from app.preparations import prepare_knight
 
 
-def battle(knights_config: Dict[str, Any]) -> dict:
+def battle(knights_config: Dict[str, Any]) -> Dict[str, int]:
 
     # BATTLE PREPARATIONS:
     lancelot = knights_config["lancelot"]
@@ -19,30 +20,8 @@ def battle(knights_config: Dict[str, Any]) -> dict:
     red_knight = knights_config["red_knight"]
     prepare_knight(red_knight)
 
-    # -------------------------------------------------------------------------------
-    # BATTLE:
-
-    # 1 Lancelot vs Mordred:
-    lancelot["hp"] -= mordred["power"] - lancelot["protection"]
-    mordred["hp"] -= lancelot["power"] - mordred["protection"]
-
-    # check if someone fell in battle
-    if lancelot["hp"] <= 0:
-        lancelot["hp"] = 0
-
-    if mordred["hp"] <= 0:
-        mordred["hp"] = 0
-
-    # 2 Arthur vs Red Knight:
-    arthur["hp"] -= red_knight["power"] - arthur["protection"]
-    red_knight["hp"] -= arthur["power"] - red_knight["protection"]
-
-    # check if someone fell in battle
-    if arthur["hp"] <= 0:
-        arthur["hp"] = 0
-
-    if red_knight["hp"] <= 0:
-        red_knight["hp"] = 0
+    battle_round(lancelot, mordred)
+    battle_round(arthur, red_knight)
 
     # Return battle results:
     return {
--- app/preparations.py
+++ app/preparations.py
@@ -2,22 +2,15 @@ from typing import Dict, Any
 
 
 def prepare_knight(knight: Dict[str, Any]) -> None:
-
     # apply armour
-    knight["protection"] = 0
-    for armour in knight["armour"]:
-        knight["protection"] += armour["protection"]
+    knight["protection"] = sum(
+        armour["protection"] for armour in knight["armour"]
+    )
 
-    # apply weapon
     knight["power"] += knight["weapon"]["power"]
 
-    # apply potion if exist
-    if knight["potion"] is not None:
-        if "power" in knight["potion"]["effect"]:
-            knight["power"] += knight["potion"]["effect"]["power"]
-
-        if "protection" in knight["potion"]["effect"]:
-            knight["protection"] += knight["potion"]["effect"]["protection"]
-
-        if "hp" in knight["potion"]["effect"]:
-            knight["hp"] += knight["potion"]["effect"]["hp"]
+    potion: Any | None = knight.get("potion")
+    if potion:
+        for stat, delta in potion["effect"].items():
+            if stat in knight:
+                knight[stat] += delta
