ced294b692b4c3cf04340d1f38a49ff81e9df2b5
acidcor <deadfoxua1337@gmail.com>
2025-09-10 16:07:07 +0200

    bad code t-t

---
+++ app/action/battle.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from app.entity.knights import Knights
+
+
+class Arena:
+
+    @staticmethod
+    def duel(fighter_one: Knights, fighter_two: Knights) -> None:
+        fighter_one.health -= fighter_two.power - fighter_one.protection
+        fighter_two.health -= fighter_one.power - fighter_two.protection
+
+    @staticmethod
+    def is_lose(fighter: Knights) -> None:
+        if fighter.health < 0:
+            fighter.health = 0
+
+    @staticmethod
+    def fight(fighter_one: Knights, fighter_two: Knights) -> None:
+        Arena.duel(fighter_one, fighter_two)
+        Arena.is_lose(fighter_one)
+        Arena.is_lose(fighter_two)
