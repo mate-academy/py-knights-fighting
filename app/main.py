3ba156b6cae31cb56b8b7f8f90a26e0e9b15bdb5
tomech <109527747+tomech4@users.noreply.github.com>
2025-09-24 22:40:08 +0200

    refactor individual battles between knights and separate into separate module

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from app.knights import KNIGHTS
 import app.preparation as preparation
+import app.fight as fight
 
 
 def battle(knights_config: dict) -> dict:
@@ -12,35 +13,7 @@ def battle(knights_config: dict) -> dict:
     # -------------------------------------------------------------------------------
     # BATTLE:
 
-    # 1 Lancelot vs Mordred:
-    lancelot["hp"] -= mordred["power"] - lancelot["protection"]
-    mordred["hp"] -= lancelot["power"] - mordred["protection"]
-
-    # check if someone fell in battle
-    if lancelot["hp"] <= 0:
-        lancelot["hp"] = 0
-
-    if mordred["hp"] <= 0:
-        mordred["hp"] = 0
-
-    # 2 Arthur vs Red Knight:
-    arthur["hp"] -= red_knight["power"] - arthur["protection"]
-    red_knight["hp"] -= arthur["power"] - red_knight["protection"]
-
-    # check if someone fell in battle
-    if arthur["hp"] <= 0:
-        arthur["hp"] = 0
-
-    if red_knight["hp"] <= 0:
-        red_knight["hp"] = 0
-
-    # Return battle results:
-    return {
-        lancelot["name"]: lancelot["hp"],
-        arthur["name"]: arthur["hp"],
-        mordred["name"]: mordred["hp"],
-        red_knight["name"]: red_knight["hp"],
-    }
+    return fight.rounds(lancelot, mordred, arthur, red_knight)
 
 
 print(battle(KNIGHTS))
