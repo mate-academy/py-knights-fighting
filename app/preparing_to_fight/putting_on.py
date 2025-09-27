9bf64aeb35ae1184af7ec9321f48c9e336ecd30a
Maksym Dazidov <maksymdazidov@gmail.com>
2025-09-27 20:43:25 +0200

    made 1

---
+++ app/preparing_to_fight/putting_on.py
@@ -0,0 +1,16 @@
+def putting_on(knight: dict) -> None:
+    knight["protection"] = 0
+    for i in knight["armour"]:
+        knight["protection"] += i["protection"]
+
+    knight["power"] += knight["weapon"]["power"]
+
+    if knight["potion"] is not None:
+        if "power" in knight["potion"]["effect"]:
+            knight["power"] += knight["potion"]["effect"]["power"]
+
+        if "protection" in knight["potion"]["effect"]:
+            knight["protection"] += knight["potion"]["effect"]["protection"]
+
+        if "hp" in knight["potion"]["effect"]:
+            knight["hp"] += knight["potion"]["effect"]["hp"]
