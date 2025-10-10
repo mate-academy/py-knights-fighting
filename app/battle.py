81b42f00c7db199b90cd2e543c4bacbe79e8e07d
Ertyrten <101129428+Ertyrten@users.noreply.github.com>
2025-10-10 18:49:19 +0300

    Update battle.py

--- app/battle.py
+++ app/battle.py
@@ -1,1 +1,12 @@
+from .knight import Knight
 
+def simulate_fight(knight_a: Knight, knight_b: Knight) -> None:
+    """
+    Симулює одночасний обмін шкодою між двома лицарями.
+    """
+    
+    power_a = knight_a.effective_power
+    power_b = knight_b.effective_power
+    
+    knight_a.take_damage(power_b)
+    knight_b.take_damage(power_a)
