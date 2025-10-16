1c2f96b8f14d23e90ccd01f73c405a83d4ecd7c0
Vladyslav Kozlov <kozlov.vlad@lll.kpi.ua>
2025-10-17 02:50:25 +0300

    Solution

--- app/battle.py
+++ app/battle.py
@@ -9,8 +9,8 @@ class Battle:
     def prepare_for_fight(self) -> None:
         for knight in (self.first_knight, self.second_knight):
             knight.apply_armour()
-            knight.apply_potion()
             knight.apply_weapon()
+            knight.apply_potion()
 
     def check_hp(self, knight: Knight) -> int:
         return 0 if knight.hp <= 0 else knight.hp
@@ -21,13 +21,13 @@ class Battle:
             defender.hp -= damage
 
     def fight(self) -> dict[str, int]:
-        self.perform_attack(self.second_knight, self.first_knight)
-        self.perform_attack(self.first_knight, self.second_knight)
-
-        first_knight_hp = self.check_hp(self.first_knight)
-        second_knight_hp = self.check_hp(self.second_knight)
+        for attacker, defender in (
+                (self.second_knight, self.first_knight),
+                (self.first_knight, self.second_knight),
+        ):
+            self.perform_attack(attacker, defender)
 
         return {
-            self.first_knight.name: first_knight_hp,
-            self.second_knight.name: second_knight_hp,
+            self.first_knight.name: self.check_hp(self.first_knight),
+            self.second_knight.name: self.check_hp(self.second_knight),
         }
