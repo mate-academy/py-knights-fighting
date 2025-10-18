0a7ddd20f603756f365ca355bea55afa246e6396
Tania Kolesnik <kolesnik@gmail.com>
2025-10-18 13:24:42 +0100

    add solution 3

--- app/main.py
+++ app/main.py
@@ -1,36 +1,29 @@
 from app.fighter.fighters import Fighters
+from app.fighter.fighter import Fighter
+
+
+def check_fall(fighter: Fighter) -> None:
+    if fighter.hp <= 0:
+        fighter.hp = 0
+
+
+def fight(fighter_one: Fighter, fighter_two: Fighter) -> None:
+    fighter_one.hp -= fighter_two.power - fighter_one.protection
+    fighter_two.hp -= fighter_one.power - fighter_two.protection
+    check_fall(fighter_one)
+    check_fall(fighter_two)
 
 
 def battle(knights_config: dict) -> dict:
-    # BATTLE PREPARATIONS:
-
     fighters = Fighters(knights_config)
     fighters.create_fighters()
     fighters.initiate_fighters()
+
     lancelot, arthur, mordred, red_knight = fighters.get_fighters()
 
-    lancelot.hp -= mordred.power - lancelot.protection
-    mordred.hp -= lancelot.power - mordred.protection
+    fight(lancelot, mordred)
+    fight(arthur, red_knight)
 
-    # check if someone fell in fighter
-    if lancelot.hp <= 0:
-        lancelot.hp = 0
-
-    if mordred.hp <= 0:
-        mordred.hp = 0
-
-    # 2 Arthur vs Red Knight:
-    arthur.hp -= red_knight.power - arthur.protection
-    red_knight.hp -= arthur.power - red_knight.protection
-
-    # check if someone fell in fighter
-    if arthur.hp <= 0:
-        arthur.hp = 0
-
-    if red_knight.hp <= 0:
-        red_knight.hp = 0
-
-    # Return fighter results:
     return {
         lancelot.name: lancelot.hp,
         arthur.name: arthur.hp,
