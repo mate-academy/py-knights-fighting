8e855f2d5e80ef4fb28052a56c34938dd78debf7
Ievgen German <german.kbrza@gmail.com>
2025-10-23 17:18:39 +0300

    Solution 1

--- app/knights/equipment/weapon.py
+++
@@ -1,9 +0,0 @@
-class Weapon:
-
-    def __init__(self, name: str, power: int):
-        self.name = name
-        self.power = power
-
-    def weapon(self):
-        return {"name": self.name,
-                "power": self.power}
