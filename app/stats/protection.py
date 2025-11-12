6bf348c19413c6436224c8ee9112ad3a68a82ef7
Jakub Bujnowski <jakubuj567@gmail.com>
2025-11-12 13:22:29 +0100

    Solution

---
+++ app/stats/protection.py
@@ -0,0 +1,4 @@
+def protection(name: dict) -> None:
+    name['protection'] = 0
+    for prot in name['armour']:
+        name['protection'] += prot['protection']
---
+++ app/stats/weapon.py
@@ -0,0 +1,2 @@
+def weapon_add(name: dict) -> None:
+    name['power'] += name['weapon']['power']
