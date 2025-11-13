dc475743b3cbd18a274d211829f00f8fbc8b65ef
Vova <vovakucin082@gmail.com>
2025-11-13 11:51:12 +0100

    Code write

--- app/main.py
+++ app/main.py
@@ -1,100 +1,13 @@
-from app.statistics.knights import Knights
-from app.statistics import config
-KNIGHTS = {
-    "lancelot": {
-        "name": "Lancelot",
-        "power": 35,
-        "hp": 100,
-        "armour": [],
-        "weapon": {
-            "name": "Metal Sword",
-            "power": 50,
-        },
-        "potion": None,
-    },
-    "arthur": {
-        "name": "Arthur",
-        "power": 45,
-        "hp": 75,
-        "armour": [
-            {
-                "part": "helmet",
-                "protection": 15,
-            },
-            {
-                "part": "breastplate",
-                "protection": 20,
-            },
-            {
-                "part": "boots",
-                "protection": 10,
-            }
-        ],
-        "weapon": {
-            "name": "Two-handed Sword",
-            "power": 55,
-        },
-        "potion": None,
-    },
-    "mordred": {
-        "name": "Mordred",
-        "power": 30,
-        "hp": 90,
-        "armour": [
-            {
-                "part": "breastplate",
-                "protection": 15,
-            },
-            {
-                "part": "boots",
-                "protection": 10,
-            }
-        ],
-        "weapon": {
-            "name": "Poisoned Sword",
-            "power": 60,
-        },
-        "potion": {
-            "name": "Berserk",
-            "effect": {
-                "power": +15,
-                "hp": -5,
-                "protection": +10,
-            }
-        }
-    },
-    "red_knight": {
-        "name": "Red Knight",
-        "power": 40,
-        "hp": 70,
-        "armour": [
-            {
-                "part": "breastplate",
-                "protection": 25,
-            }
-        ],
-        "weapon": {
-            "name": "Sword",
-            "power": 45
-        },
-        "potion": {
-            "name": "Blessing",
-            "effect": {
-                "hp": +10,
-                "power": +5,
-            }
-        }
-    }
-}
+from app.statistics.knights import Tournament
+from app.data.data import KNIGHTS
 
-stat = Knights(KNIGHTS)
-lister = Knights.list_knights
+stat = Tournament(KNIGHTS)
 
 def battle(knightsConfig):
     # BATTLE PREPARATIONS:
     # apply armour
     for value in knightsConfig.values():
-        config.configurations(value)
+        stat.configurations(value)
 
     # lancelot
     lancelot = knightsConfig["lancelot"]
@@ -113,26 +26,10 @@ def battle(knightsConfig):
     # BATTLE:
 
     # 1 Lancelot vs Mordred:
-    lancelot["hp"] -= mordred["power"] - lancelot["protection"]
-    mordred["hp"] -= lancelot["power"] - mordred["protection"]
-
-    # check if someone fell in battle
-    if lancelot["hp"] <= 0:
-        lancelot["hp"] = 0
-
-    if mordred["hp"] <= 0:
-        mordred["hp"] = 0
+    stat.battle(lancelot, mordred)
 
     # 2 Arthur vs Red Knight:
-    arthur["hp"] -= red_knight["power"] - arthur["protection"]
-    red_knight["hp"] -= arthur["power"] - red_knight["protection"]
-
-    # check if someone fell in battle
-    if arthur["hp"] <= 0:
-        arthur["hp"] = 0
-
-    if red_knight["hp"] <= 0:
-        red_knight["hp"] = 0
+    stat.battle(arthur, red_knight)
 
     # Return battle results:
     return {
@@ -144,6 +41,3 @@ def battle(knightsConfig):
 
 
 print(battle(KNIGHTS))
-print(stat.lancelot)
-print(KNIGHTS["lancelot"])
-print(lister[0])
--- app/statistics/knights.py
+++ app/statistics/knights.py
@@ -1,14 +1,41 @@
-class Knights:
+class Tournament:
     lancelot: dict
     arthur: dict
     mordred: dict
     red_knight: dict
-    list_knights: list["Knights"] = []
+    list_knights: list["Tournament"] = []
     def __init__(self, data: dict, protection: int = 0) -> None:
         self.protection = protection
         for key, value in data.items():
             setattr(self, key, value)
-            Knights.list_knights.append(getattr(self, key))
+            Tournament.list_knights.append(getattr(self, key))
 
-    # def __repr__(self) -> str:
-    #     return f"{self}"
+    @staticmethod
+    def configurations(stat):
+        stat["protection"] = 0
+        for a in stat["armour"]:
+            stat["protection"] += a["protection"]
+
+        # apply weapon
+        stat["power"] += stat["weapon"]["power"]
+
+        # apply potion if exist
+        if stat["potion"] is not None:
+            if "power" in stat["potion"]["effect"]:
+                stat["power"] += stat["potion"]["effect"]["power"]
+
+            if "protection" in stat["potion"]["effect"]:
+                stat["protection"] += stat["potion"]["effect"]["protection"]
+
+            if "hp" in stat["potion"]["effect"]:
+                stat["hp"] += stat["potion"]["effect"]["hp"]
+
+    @staticmethod
+    def battle(knight1, knight2):
+        knight1["hp"] -= knight2["power"] - knight1["protection"]
+        knight2["hp"] -= knight1["power"] - knight2["protection"]
+        if knight1["hp"] <= 0:
+            knight1["hp"] = 0
+
+        if knight2["hp"] <= 0:
+            knight2["hp"] = 0
