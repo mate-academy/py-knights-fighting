042c95d86b831040abd1ee28386144a3bbb98464
Jakub Bujnowski <jakubuj567@gmail.com>
2025-11-13 12:12:26 +0100

    Solution

---
+++ app/stats/potion_effect.py
@@ -0,0 +1,10 @@
+def potion_effect(name: dict) -> None:
+    if name["potion"] is not None:
+        if "power" in name["potion"]["effect"]:
+            name["power"] += name["potion"]["effect"]["power"]
+
+        if "protection" in name["potion"]["effect"]:
+            name["protection"] += name["potion"]["effect"]["protection"]
+
+        if "hp" in name["potion"]["effect"]:
+            name["hp"] += name["potion"]["effect"]["hp"]
