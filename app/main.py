aa03c7f7629e9fd5d478292fa1986b938803b413
Andrii Chychkan <mywork1728@gmail>
2025-11-15 17:52:31 +0200

    fixed

--- app/main.py
+++ app/main.py
@@ -17,7 +17,7 @@ def battle(knights_config: dict[Any]) -> dict[str, int]:
     mordred_prep = BattlePreparation("mordred", knights_config)
     mordred = mordred_prep.create_character()
 
-    red_knight_prep = BattlePreparation("Red Knight", knights_config)
+    red_knight_prep = BattlePreparation("red_knight", knights_config)
     red_knight = red_knight_prep.create_character()
 
     # 1 Lancelot vs Mordred:
@@ -35,7 +35,7 @@ def battle(knights_config: dict[Any]) -> dict[str, int]:
         lancelot.name.capitalize(): lancelot.hp,
         arthur.name.capitalize(): arthur.hp,
         mordred.name.capitalize(): mordred.hp,
-        red_knight.name.capitalize(): red_knight.hp,
+        "Red Knight": red_knight.hp,
     }
 
 
