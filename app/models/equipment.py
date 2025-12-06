3227e5f8967036c56b9c74f76e5cec6736ab1dcb
Andrii <a17drigs@gmail.com>
2025-12-06 22:54:48 +0200

    Solution

---
+++ app/models/equipment.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+
+class ArmourPiece:
+    def __init__(self, part: str, protection: int) -> None:
+        self.part = part
+        self.protection = protection
+
+
+class Weapon:
+    def __init__(self, name: str, power: int) -> None:
+        self.name = name
+        self.power = power
+
+
+class PotionEffect:
+    def __init__(self, hp: int = 0,
+                 power: int = 0, protection: int = 0) -> None:
+        self.hp = hp
+        self.power = power
+        self.protection = protection
+
+    @classmethod
+    def create_from_dict(cls, data: dict) -> PotionEffect:
+        if not data:
+            return cls()
+        return cls(
+            hp=data.get("hp", 0),
+            power=data.get("power", 0),
+            protection=data.get("protection", 0)
+        )
+
+
+class Potion:
+    def __init__(self, name: str, effect: dict) -> None:
+        self.name = name
+        self.effect = effect
+
+    @classmethod
+    def create_from_dict(cls, data: dict) -> Potion | None:
+        if not data:
+            return None
+        effect = PotionEffect.create_from_dict(data.get("effect", {}))
+        return cls(name=data["name"], effect=effect)
