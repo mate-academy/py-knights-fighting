e2ed2b03138ee2c38934b140e5db00ac9e89f908
Steicy <anastasiaosega@gmail.com>
2025-12-08 19:01:57 +0200

    solved tasks

---
+++ app/knight.py
@@ -0,0 +1,28 @@
+class Knight:
+
+    def __init__(self, config: dict) -> None:
+        self.name = config["name"]
+        self.hp = config["hp"]
+        self.power = config["power"]
+        self.armour = config.get("armour", [])
+        self.weapon = config.get("weapon")
+        self.potion = config.get("potion")
+        self.protection = 0
+
+    def apply_armour(self) -> None:
+        self.protection = sum(part["protection"] for part in self.armour)
+
+    def apply_weapon(self) -> None:
+        if self.weapon is not None:
+            self.power += self.weapon["power"]
+
+    def apply_potion(self) -> None:
+        if self.potion is not None:
+            if "power" in self.potion["effect"]:
+                self.power += self.potion["effect"]["power"]
+
+            if "protection" in self.potion["effect"]:
+                self.protection += self.potion["effect"]["protection"]
+
+            if "hp" in self.potion["effect"]:
+                self.hp += self.potion["effect"]["hp"]
