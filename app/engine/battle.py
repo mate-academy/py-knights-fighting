deee931b97d5d8efb9ea3313bd21960375fefd0d
Iryna Mishchenko <djrobentab@gmail.com>
2025-12-10 16:15:15 +0100

    Solution

---
+++ app/engine/battle.py
@@ -0,0 +1,18 @@
+from ..knights.knight import Knight
+
+
+def fight(first_knight: Knight, second_knight: Knight):
+    first_stats = first_knight.prepared_stats()
+    second_stats = second_knight.prepared_stats()
+
+    damage_to_first = max(0, second_stats["power"] - first_stats["protection"])
+    damage_to_second = max(0, first_stats["power"] - second_stats["protection"])
+
+    first_stats["hp"] = max(0, first_stats["hp"] - damage_to_first)
+    second_stats["hp"] = max(0, second_stats["hp"] - damage_to_second)
+
+    battle_result = {
+        first_knight.name: first_stats["hp"],
+        second_knight.name: second_stats["hp"]
+    }
+    return battle_result
