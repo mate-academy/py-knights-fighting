a4d23d477efc3970ca1d7b6eab6a4670e3030613
Yurii <pamkivurij92@gmail.com>
2025-12-12 14:54:43 +0100

    Deleted print debagging

--- app/main.py
+++ app/main.py
@@ -1,9 +1,5 @@
-from app.KnightConfiguration import KNIGHTS
 from app.battle import Battle
 
 
 def battle(knights_config: dict) -> dict:
     return Battle.battle(knights_config)
-
-
-print(battle(KNIGHTS))
