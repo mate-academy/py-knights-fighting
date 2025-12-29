fad2c54e9cdef7eeb3c096e0c231a9ea6b41cb9f
Felipe Pessin <fhpessin@gmail.com>
2025-12-29 15:07:28 -0300

    Update knight.py

--- app/knight.py
+++ app/knight.py
@@ -1,5 +1,4 @@
 class Knight:
-    # Adicionamos 'dict' para o config e 'None' para o retorno
     def __init__(self, config: dict) -> None:
         self.name: str = config["name"]
         self.hp: int = config["hp"]
@@ -18,11 +17,10 @@ class Knight:
             self.power += effect.get("power", 0)
             self.protection += effect.get("protection", 0)
 
-    # Adicionamos 'int' para o dano e 'None' para o retorno
     def receive_damage(self, opponent_power: int) -> None:
         damage = opponent_power - self.protection
         if damage > 0:
             self.hp -= damage
-        
+
         if self.hp < 0:
             self.hp = 0
