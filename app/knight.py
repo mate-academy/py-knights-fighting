03302c2cf1f960a8a7c24858a44b3f2962c17ad4
Felipe Pessin <fhpessin@gmail.com>
2025-12-29 14:49:21 -0300

    Update knight.py

--- app/knight.py
+++ app/knight.py
@@ -1,23 +1,30 @@
 class Knight:
-    def __init__(self, name, hp, power, armour, weapon, potion):
-        self.name = name
-        self.hp = hp
-        self.power = power
+    def __init__(self, config):
+        self.name = config["name"]
+        self.hp = config["hp"]
+        self.power = config["power"]
         self.protection = 0
-        
-        # 1. Aplicar Proteção da Armadura (pode ser mais de uma parte)
-        for part in armour:
-            self.protection += part["protection"]
-            
-        # 2. Aplicar Poder da Arma
-        self.power += weapon["power"]
-        
-        # 3. Aplicar Efeitos da Poção (se existir)
-        if potion:
-            self.hp += potion["effect"].get("hp", 0)
-            self.power += potion["effect"].get("power", 0)
-            self.protection += potion["effect"].get("protection", 0)
 
-    def __repr__(self):
-        """Opcional: ajuda a visualizar o objeto durante o debug"""
-        return f"Knight(name={self.name}, hp={self.hp})"
+        # Aplicar Armadura
+        for armour_part in config["armour"]:
+            self.protection += armour_part["protection"]
+
+        # Aplicar Arma
+        self.power += config["weapon"]["power"]
+
+        # Aplicar Poção (se existir)
+        potion = config.get("potion")
+        if potion:
+            effect = potion["effect"]
+            self.hp += effect.get("hp", 0)
+            self.power += effect.get("power", 0)
+            self.protection += effect.get("protection", 0)
+
+    def receive_damage(self, opponent_power):
+        damage = opponent_power - self.protection
+        if damage > 0:
+            self.hp -= damage
+        
+        # Garante que o HP não seja negativo
+        if self.hp < 0:
+            self.hp = 0
