736af002e6c8f78746a4cdca14dec936f8f0094e
Nik <nikita.pitushkin31@mail.com>
2026-01-18 23:17:26 +0200

    111

--- app/battle/battle.py
+++ app/battle/battle.py
@@ -7,13 +7,11 @@ class Battle:
         self.knight2 = knight2
 
     def start(self) -> dict:
-        if self.knight1.potion and not self.knight1.potion_used:
+        if self.knight1.potion:
             self.knight1.potion.apply(self.knight1)
-            self.knight1.potion_used = True
 
-        if self.knight2.potion and not self.knight2.potion_used:
+        if self.knight2.potion:
             self.knight2.potion.apply(self.knight2)
-            self.knight2.potion_used = True
 
         while self.knight1.is_alive() and self.knight2.is_alive():
             self.knight1.attack(self.knight2)
@@ -21,6 +19,8 @@ class Battle:
                 break
 
             self.knight2.attack(self.knight1)
+            if not self.knight1.is_alive():
+                break
 
         return {
             self.knight1.name: max(self.knight1.hp, 0),
