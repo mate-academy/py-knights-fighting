9609a47a91eb54ebba2d25df2b90a5579885a3ad
Seroga <ilchuk.cergey@gmail.com>
2026-01-19 20:24:24 +0200

    fixed flake8 single quotes

--- app/main.py
+++ app/main.py
@@ -11,12 +11,12 @@ def battle(knights_config: dict) -> dict:
         knights_instance["mordred"]
     )
     battle_between_knights(
-        knights_instance['arthur'],
+        knights_instance["arthur"],
         knights_instance["red_knight"]
     )
     return {
         knights_instance["lancelot"].name: knights_instance["lancelot"].hp,
         knights_instance["mordred"].name: knights_instance["mordred"].hp,
-        knights_instance['arthur'].name: knights_instance['arthur'].hp,
+        knights_instance["arthur"].name: knights_instance["arthur"].hp,
         knights_instance["red_knight"].name: knights_instance["red_knight"].hp,
     }
