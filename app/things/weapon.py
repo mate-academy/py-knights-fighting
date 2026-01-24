9ab7fa5d400692a2194c94b9bfebed58725a330a
py-ovarion <olegk6783@gmail.com>
2026-01-24 22:30:56 +0200

    solved

---
+++ app/things/weapon.py
@@ -0,0 +1,10 @@
+class Weapon:
+    def __init__(self, name: str, power: int) -> None:
+        self.name = name
+        self.power = power
+
+    @classmethod
+    def create_weapon_from_dict(cls,
+                                dictionary: dict) -> Weapon:
+        return cls(name=dictionary["name"],
+                   power=dictionary["power"])
