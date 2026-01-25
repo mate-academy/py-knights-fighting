1bb33f435e1d4b02e36600f80e5e44559ea62c7a
Alexey <alexey.zhuravel2004@gmail.com>
2026-01-25 01:17:29 +0100

    first variant of solution

--- app/Knights/armor.py
+++ app/Knights/armor.py
@@ -1,10 +1,10 @@
 class Armor:
     def __init__(self, parts: list[dict]) -> None:
         self.parts = parts
-    
+
     def total_armor(self) -> int:
         total_armor = 0
         for part in self.parts:
             total_armor += part["protection"]
-        
+
         return total_armor
--- app/Knights/knights.py
+++ app/Knights/knights.py
@@ -2,21 +2,56 @@ from app.Knights.armor import Armor
 
 
 class Knight:
-    def __init__(self, name: str, power: int, hp: int, protection: int):
+    def __init__(
+            self,
+            name: str,
+            power: int,
+            hp: int,
+            protection: int
+    ) -> None:
         self.name = name
         self.power = power
         self.hp = hp
         self.protection = protection
 
+    # this method takes potions effects and normalized it
+    # for the "build_knights_couples_from_config" func
 
+    @staticmethod
+    def potion_normalizer(potion: dict) -> dict:
+        potion_effects = {
+            "power": potion.get("power", 0),
+            "hp": potion.get("hp", 0),
+            "protection": potion.get("protection", 0)
+        }
+        return potion_effects
 
-"""
-ідкя наступна: створити перед боєм лицарів а потім рахувати переможця
-тообто кожен клас предметів просто повертає значення які потім буде використано для створення ліцаря
-потім бій і оголошення результатів згідно вімог тестів
+    # this method builds couples of knights that will fight
 
-увесь бій зводиться до формули hp(red) -= power(blue) - protection(red) 
-а щоб дійти доцього треба рахувати стати лицарів перед боєм
-"""
+    @staticmethod
+    def build_knight_couples_from_cfg(
+        knights: dict
+    ) -> list[tuple["Knight", "Knight"]]:
 
+        fighters_dict = {}
+        for name, stats in knights.items():
+            potion_cfg = stats.get("potion")
+            effects = potion_cfg.get("effect", {}) if potion_cfg else {}
+            potion = Knight.potion_normalizer(effects)
+            hp = stats["hp"] + potion["hp"]
+            power = stats["power"] + stats["weapon"]["power"] + potion["power"]
+            armor = Armor(stats["armour"])
+            protection = armor.total_armor() + potion["protection"]
+            knight = Knight(stats["name"], power, hp, protection)
+            fighters_dict[name] = knight
 
+        return [
+            (
+                fighters_dict.get("lancelot"),
+                fighters_dict.get("mordred")
+            ),
+            (
+                fighters_dict.get("arthur"),
+                fighters_dict.get("red_knight")
+            )
+        ]
